614d82f0984d143f951cf35eb645f73ee6b3273c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-30 21:19:05 +0000

    fern-api: update 3.55.0 bottle.

--- Formula/f/fern-api.rb
+++ Formula/f/fern-api.rb
@@ -10,12 +10,12 @@ class FernApi < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "56136b1de9193f94d3288bf6cb00f67916355e74600b006208b6cbb9f60ecf64"
-    sha256 cellar: :any,                 arm64_sequoia: "f9f1ef235912f2ef824681c11099c037cd12c84074edc892b6a569d0315b0a95"
-    sha256 cellar: :any,                 arm64_sonoma:  "f9f1ef235912f2ef824681c11099c037cd12c84074edc892b6a569d0315b0a95"
-    sha256 cellar: :any,                 sonoma:        "0aea4f3dd6f668ea4001a2db2c7a9e314cd902c874629a9914765c5bb9695724"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3c283184d80c1f2d33644dc70c0d55183c7a2b73e6d1d3dc898918c8494af7f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9590bb645b2259b14c9fef60f374dbb7267e658c714c34ff185ec31621cfaf5a"
+    sha256 cellar: :any,                 arm64_tahoe:   "74ede749713aceb1c543db0dd50e7236b524e007b0831e4bc82fb8a20441e4f7"
+    sha256 cellar: :any,                 arm64_sequoia: "72516ec317ca46397b3e36b6aac87ebb19f600edf11d90d5ce1a9370d79f8f83"
+    sha256 cellar: :any,                 arm64_sonoma:  "72516ec317ca46397b3e36b6aac87ebb19f600edf11d90d5ce1a9370d79f8f83"
+    sha256 cellar: :any,                 sonoma:        "2f619585e7ef6ed6ffd80b4b39e0e89e26f0f15aba23eede6b20cbfb038eceb7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bf7fcf9e5ffc2a57f82651d7c7a81328e2596165190a7b5689a9d2f8920d07dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f54a9519a7d2f18069b7d8b6c4bc60d4947805a6376a96e2d48c0a9ba1cf7881"
   end
 
   depends_on "node"
