7f5888be6f66d1819a709c6d59d4c0d7d66db288
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-30 23:09:28 +0000

    ghostunnel 1.9.1

--- Formula/g/ghostunnel.rb
+++ Formula/g/ghostunnel.rb
@@ -1,8 +1,8 @@
 class Ghostunnel < Formula
   desc "Simple SSL/TLS proxy with mutual authentication"
   homepage "https://github.com/ghostunnel/ghostunnel"
-  url "https://github.com/ghostunnel/ghostunnel/archive/refs/tags/v1.9.0.tar.gz"
-  sha256 "0cc3b2ac8b30aa6d6c2c3df9289436b985c21aed6f986b025c7a4057666bd47c"
+  url "https://github.com/ghostunnel/ghostunnel/archive/refs/tags/v1.9.1.tar.gz"
+  sha256 "264cd681f22138429dbbbf91d8dd06449c8d950880ee0d3f7bacaa48dea8bf65"
   license "Apache-2.0"
   head "https://github.com/ghostunnel/ghostunnel.git", branch: "master"
 
