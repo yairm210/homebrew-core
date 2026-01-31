69064c1c8cbe3a58b02d04269b1cccc4f77d4912
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-31 08:37:23 +0000

    vet: update 1.12.22 bottle.

--- Formula/v/vet.rb
+++ Formula/v/vet.rb
@@ -6,12 +6,12 @@ class Vet < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "09859f8ae597d4ba7bc1ca0838ad513d5c4db90c9d6fa138355f88669b57e66b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c726e8294eb6c6a11d0ab6a11217a0113c6b9785a02cea14e3d4e7fc0607deae"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fb4fb109d1c1c972fb6444eca566dea3cc33092ab4d75394d3aee588c15fc8c0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6804a186d55cc8ac4f236df6b22c968c59241d751b163e674e0a523209c0931a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a57fefdc3f64f1cf200a201cdfd893eb65b000c98738bb062c3e78f01e9895aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92489c33e5fd4a7bc1e1c615b17ad241e97fec533420f4b07e08fe4411265c33"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "242ec4faa1b46219af055bb8957320fa6e38f5c8c57a41b30f6c3f6c7c48028e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c8cf1253f713abd38b770dd4655030b70e5455a365bcf1cd79ac6837d41b1bb2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "695a0c5ab06980938e2edafde6ac0bafd9683b96193e5e246b7ab1fe72efea64"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f6ad48b152bb9d97dc92a0b2c51823db22de56eeb3eb61c1dcd52134fda45c94"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2af15fc5142300d9fcc858886b565c62348bf86cb148eeb8a396a543989cff60"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "de8f73135c8bc65890039c137e6dd2d3440b3fe825868e09ba4087487112fbb8"
   end
 
   depends_on "go"
