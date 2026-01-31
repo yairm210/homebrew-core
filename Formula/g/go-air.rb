6e8f8c2dff3db0a0107e191a46327fc3972d5b53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-31 23:27:28 +0000

    go-air: add 1.64.4 bottle.

--- Formula/g/go-air.rb
+++ Formula/g/go-air.rb
@@ -6,6 +6,15 @@ class GoAir < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/air-verse/air.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b167dfec3d7977dec5c7ef911912aa3f13615c93e9fd0d0338d277bf6bd18466"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b167dfec3d7977dec5c7ef911912aa3f13615c93e9fd0d0338d277bf6bd18466"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b167dfec3d7977dec5c7ef911912aa3f13615c93e9fd0d0338d277bf6bd18466"
+    sha256 cellar: :any_skip_relocation, sonoma:        "eebdb64815e39accaba3827ccfa64362327e9def29125ce77210da2769e1ae99"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b4059eb4d8f78e4869883a06acc135d776702410ffcbb3847ec8d4acd0cb64ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4dcaeb142786456d8854d67b3db9f74444807e15fbc699f8c5e55b19a39549fb"
+  end
+
   depends_on "go"
 
   conflicts_with "air", because: "both install binaries with the same name"
