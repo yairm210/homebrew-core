43dd60b4a97c2c27ac7a4494189b0e099de683ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-01 11:06:06 +0000

    flix 0.68.0

--- Formula/f/flix.rb
+++ Formula/f/flix.rb
@@ -1,8 +1,8 @@
 class Flix < Formula
   desc "Statically typed functional, imperative, and logic programming language"
   homepage "https://flix.dev/"
-  url "https://github.com/flix/flix/archive/refs/tags/v0.67.2.tar.gz"
-  sha256 "3f15b74d7de3c768c9fc8b5a13722341918d727a93cacba6195cfcd998f89067"
+  url "https://github.com/flix/flix/archive/refs/tags/v0.68.0.tar.gz"
+  sha256 "e5cfb63f1d1c180f767e943f1c82c4336f273c6c18a0d63f64149812490c0881"
   license "Apache-2.0"
   head "https://github.com/flix/flix.git", branch: "master"
 
