5ff910befdda8196f655bd282f79371dc00945b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-01 17:40:47 +0000

    x-cmd 0.7.15

--- Formula/x/x-cmd.rb
+++ Formula/x/x-cmd.rb
@@ -1,8 +1,8 @@
 class XCmd < Formula
   desc "Bootstrap 1000+ command-line tools in seconds"
   homepage "https://x-cmd.com"
-  url "https://github.com/x-cmd/x-cmd/archive/refs/tags/v0.7.13.tar.gz"
-  sha256 "b33cdbc509370861009215bafcef68ddc3d9c3090c5f9c407a7015541e6ea334"
+  url "https://github.com/x-cmd/x-cmd/archive/refs/tags/v0.7.15.tar.gz"
+  sha256 "eda4e3d939a072879138f55fb1ad213f1af66414212bda6b484ae88cd2c66537"
   license all_of: ["AGPL-3.0-only", "MIT", "BSD-3-Clause"]
 
   head "https://github.com/x-cmd/x-cmd.git", branch: "X"
