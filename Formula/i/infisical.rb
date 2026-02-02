58248ef98dcb549bd25d466c5a142f1a9424fbfc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-02 17:17:10 +0000

    infisical 0.43.50

--- Formula/i/infisical.rb
+++ Formula/i/infisical.rb
@@ -1,8 +1,8 @@
 class Infisical < Formula
   desc "CLI for Infisical"
   homepage "https://infisical.com/docs/cli/overview"
-  url "https://github.com/Infisical/cli/archive/refs/tags/v0.43.49.tar.gz"
-  sha256 "11ec2ad0ebea65bb2c49d457c8e9816dc16edc4b3635fb02167d580a3158ec45"
+  url "https://github.com/Infisical/cli/archive/refs/tags/v0.43.50.tar.gz"
+  sha256 "cb3ed99a1f36ea438f574b3344e9037622a370e1878b8eb01425b365b29999e1"
   license "MIT"
   head "https://github.com/Infisical/cli.git", branch: "main"
 
