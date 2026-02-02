511f06dfee7c46a497466881ecfd26daf8203dd9
Michael Cho <michael@michaelcho.dev>
2026-02-02 16:39:41 -0500

    zstd: use `zlib-ng-compat` on Linux

--- Formula/z/zstd.rb
+++ Formula/z/zstd.rb
@@ -10,6 +10,7 @@ class Zstd < Formula
     "BSD-2-Clause", # programs/zstdgrep, lib/libzstd.pc.in
     "MIT", # lib/dictBuilder/divsufsort.c
   ]
+  revision 1
   head "https://github.com/facebook/zstd.git", branch: "dev"
 
   # The upstream repository contains old, one-off tags (5.5.5, 6.6.6) that are
@@ -37,7 +38,9 @@ class Zstd < Formula
   depends_on "lz4"
   depends_on "xz"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Legacy support is the default after
