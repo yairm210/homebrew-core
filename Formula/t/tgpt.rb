5f3e2598d6ee460f857abef5a97cc279359c7309
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-02 17:19:19 +0000

    tgpt: update 2.11.1 bottle.

--- Formula/t/tgpt.rb
+++ Formula/t/tgpt.rb
@@ -7,14 +7,12 @@ class Tgpt < Formula
   head "https://github.com/aandrew-me/tgpt.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ff1814a032b50dc938619a0d3beb42ea04bc71e859c74e1bb0788a7195608d4a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0aaf7491e34e5fac755643beb05c215be1c25600e1a3dd92bf5da8585ce49b9c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fa57f2611186a6c6efcc5db8dcfad0d90d398873db067d07db64021160b2f97a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6ca27e2b876508123185c8fb67b65af1aa156c0c3dadcbee75068c9e466f6528"
-    sha256 cellar: :any_skip_relocation, sonoma:        "61e4f46de86ffc8116273f270809e7db38fe1567c496f85519f1895d757568e3"
-    sha256 cellar: :any_skip_relocation, ventura:       "652445f4990a5d03d9c8a20339a28502ba45b2b02081058f6fa425ba491b62d9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b9fb13ae92813f7636af5f1e86da4443ad5dfac176f2380ad637a3e42065555c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d5f05da6f4436deb70610058e858992622bfe504aa815f1b0969529bb6a3fab"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9f170adfc7e221f022662f3890ce7c4e65096a54e89f0e299154a17783ee0666"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3447294392f618efd80f2d311c29cf4a886e31809d1444e60ded6ae2ad153a73"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c9ed0c74f7448afc363ad31c9c12acc7ac39210d70638b93c025319a55c457f6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e53e5ff3d4f95e69cae009c4d8bbfad4b7bc4962146bddd153fea7f49883010a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b806032f331d56a4f7cdf09d9dff1b84940f5f03537e73270ba6519814a55e5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9f5022eceab6be3bfdd87b0a5784b1df7b52de614ce3f78472631fa0f4ba2e0"
   end
 
   depends_on "go" => :build
