c2804481229bc95634641a8f71666cbc5e0374be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-02 18:30:59 +0000

    boring: update 0.12.0 bottle.

--- Formula/b/boring.rb
+++ Formula/b/boring.rb
@@ -9,12 +9,12 @@ class Boring < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4519a4d66c9e2d1bafeef19dfa358e2c56bfda4b2c1070ed251b0fcae9e7fde8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4519a4d66c9e2d1bafeef19dfa358e2c56bfda4b2c1070ed251b0fcae9e7fde8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4519a4d66c9e2d1bafeef19dfa358e2c56bfda4b2c1070ed251b0fcae9e7fde8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4618e05ee175780dca308980515da4f16efc7b62e329f03e1307c67cec99d094"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "bc712b056d95434baa12289884298df928e7c4394be8b8c156c196fdf5dd9f43"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2611d4e6ac1ee88db608c4decf132871021d86a933814f54b7550169ca1eec19"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "82d7a181b1bcb7115556412c386c6618cd0008c9d0f29d57797765651544e2c9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "82d7a181b1bcb7115556412c386c6618cd0008c9d0f29d57797765651544e2c9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "82d7a181b1bcb7115556412c386c6618cd0008c9d0f29d57797765651544e2c9"
+    sha256 cellar: :any_skip_relocation, sonoma:        "46b81c339e326c67b686284c304b3813783cef478e7f17ef3d5fdf90cbff8d25"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6a76d714a17e63ca3cd1edc43ddad69dc984a419d4eb4809f8bfd3cd55283b0c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7aca59fda3df3f2d38b1e6b3d790abaa95aefe4476235929a29717ebb0f29cf2"
   end
 
   depends_on "go" => :build
