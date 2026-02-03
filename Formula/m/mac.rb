4dcdf596528f8a45c53285171ff06b15f7c5bb9a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-03 13:06:54 +0000

    mac: update 12.10 bottle.

--- Formula/m/mac.rb
+++ Formula/m/mac.rb
@@ -14,12 +14,12 @@ class Mac < Formula
   no_autobump! because: :incompatible_version_format
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "3b00bef2620eb48b4b69543bd1dc336aa7fa9b69fde107ebe49071cdee1143e6"
-    sha256 cellar: :any,                 arm64_sequoia: "a93e49a9477e2547765948f7a3058fcd36180b6544f5d0b7488b002b3390479e"
-    sha256 cellar: :any,                 arm64_sonoma:  "9a191e04c8e05b5bafe3705e97245979aba317ba4812cc83c91c916d6a420b1c"
-    sha256 cellar: :any,                 sonoma:        "3ffd611bfa8ed53067c57aee5f7a69dd9297f781290d23c347be0e67601d4796"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b8ae9f3c5a6cd64ffde6f9e28ed667e40aa623eae76f44c903417704cc015092"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "35405a4e03725564d84c721102f42dc146ac581458019333407b763315e3b670"
+    sha256 cellar: :any,                 arm64_tahoe:   "025039c141d4020aee52cf4dafeb0571642edd7abf0c58d2a4f9b4092d98b025"
+    sha256 cellar: :any,                 arm64_sequoia: "7710fb6d10b15ac98991c3e7163e5395a388f40da4c56bddedf69f27dc24bf1c"
+    sha256 cellar: :any,                 arm64_sonoma:  "1fb5a574f4db257eb3acb467c4e9286aace563f793ca90de668a00f5ae99534a"
+    sha256 cellar: :any,                 sonoma:        "c2e48606fa9cdb815e6847cd2baceb4b01b71d0b136abbe91ba9996be8d0c098"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3d2b921acc2421086b865a3d357cb70e7a22823a549f5f7a5b0baf9cbfd757d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d10220c5ace1cfdbae96f44e1a0aa9266ddd1d4491170167dd253f76c6118774"
   end
 
   depends_on "cmake" => :build
