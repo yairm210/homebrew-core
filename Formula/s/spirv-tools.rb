5168252ea0a5496ed3a9eac5d9be8e6a18327719
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-03 03:28:12 +0000

    spirv-tools 1.4.341.0

--- Formula/s/spirv-tools.rb
+++ Formula/s/spirv-tools.rb
@@ -1,8 +1,8 @@
 class SpirvTools < Formula
   desc "API and commands for processing SPIR-V modules"
   homepage "https://github.com/KhronosGroup/SPIRV-Tools"
-  url "https://github.com/KhronosGroup/SPIRV-Tools/archive/refs/tags/vulkan-sdk-1.4.335.0.tar.gz"
-  sha256 "8b3d5637061b52675e506ffa1100740031e38bdd96b8177978acfd898a705da2"
+  url "https://github.com/KhronosGroup/SPIRV-Tools/archive/refs/tags/vulkan-sdk-1.4.341.0.tar.gz"
+  sha256 "15bfb678138cdf9cd1480dfb952547bbb66b763a735b6d5582578572f5c2e6f9"
   license "Apache-2.0"
   version_scheme 1
   head "https://github.com/KhronosGroup/SPIRV-Tools.git", branch: "main"
