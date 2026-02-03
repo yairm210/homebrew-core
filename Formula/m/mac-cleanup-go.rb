8ff1865d1f751473f35dba975aa505cc744d6f87
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-03 20:59:51 +0000

    mac-cleanup-go: update 1.3.10 bottle.

--- Formula/m/mac-cleanup-go.rb
+++ Formula/m/mac-cleanup-go.rb
@@ -7,10 +7,10 @@ class MacCleanupGo < Formula
   head "https://github.com/2ykwang/mac-cleanup-go.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c988a43d66ee6da46c4441d4f3f9c147324b8a7a2375f1df436808aa07507839"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c988a43d66ee6da46c4441d4f3f9c147324b8a7a2375f1df436808aa07507839"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c988a43d66ee6da46c4441d4f3f9c147324b8a7a2375f1df436808aa07507839"
-    sha256 cellar: :any_skip_relocation, sonoma:        "81acfdd8422dfe2cb8ebd1e10c96b887ed7df5fcc140c99deec72bb4d1d3df7d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7034ce5de250f38fe1184baf787caeab0c6ab4d599a1d8264d254cf7f7353cde"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7034ce5de250f38fe1184baf787caeab0c6ab4d599a1d8264d254cf7f7353cde"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7034ce5de250f38fe1184baf787caeab0c6ab4d599a1d8264d254cf7f7353cde"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6d39918c2e47b69a1458f9e8ceb5d91e5277bfc0e4f7f33a2a18ca6b664b98d7"
   end
 
   depends_on "go" => :build
