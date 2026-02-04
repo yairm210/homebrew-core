dc546311906c77e29356f453618b28143bbacb9c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-04 03:20:56 +0000

    libnetfilter_conntrack: update 1.1.1 bottle.

--- Formula/lib/libnetfilter_conntrack.rb
+++ Formula/lib/libnetfilter_conntrack.rb
@@ -11,8 +11,8 @@ class LibnetfilterConntrack < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "e1c3d0f17c63a78eae6ba07fdf7cff0414dc883c3dc78495d5c1dddc3ca66c92"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ae07370b1802d6089195cb605b4010635126ca6f6f37d20023c5f19ff9b833e6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "0c7acb47bfd4c7e21ee0d3d6454b387d8b74d13bd6fbbaf375ad85b405d3d132"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "af64ee07b344180676533bdd4b495741cf69afd3fd483c166e8d672cc414692e"
   end
 
   depends_on "pkgconf" => [:build, :test]
