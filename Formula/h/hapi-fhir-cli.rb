115db1143863262ec04ec1dff4da449a93f88cd0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-04 23:07:52 +0000

    hapi-fhir-cli 8.6.3

--- Formula/h/hapi-fhir-cli.rb
+++ Formula/h/hapi-fhir-cli.rb
@@ -1,8 +1,8 @@
 class HapiFhirCli < Formula
   desc "Command-line interface for the HAPI FHIR library"
   homepage "https://hapifhir.io/"
-  url "https://github.com/hapifhir/hapi-fhir/releases/download/v8.6.1/hapi-fhir-8.6.1-cli.zip"
-  sha256 "affb9442a6d7f8e11e7a7a2014a802be40275fa13b88850bd703c01e6d6f0e19"
+  url "https://github.com/hapifhir/hapi-fhir/releases/download/v8.6.3/hapi-fhir-8.6.3-cli.zip"
+  sha256 "43c19cd81db22f1201f824dbbe453516ec12c3fd8fefc8eedf6fa00c65dec8a1"
   license "Apache-2.0"
 
   # The "latest" release on GitHub is sometimes for an older major/minor, so we
