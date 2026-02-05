32b6718945e57ec637c64ca4503ca50060381164
Rui Chen <rui@chenrui.dev>
2026-02-05 22:42:03 +0000

    pdftoipe 7.2.29.2
    pdftoipe: remove build patches
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pdftoipe.rb
+++ Formula/p/pdftoipe.rb
@@ -1,10 +1,9 @@
 class Pdftoipe < Formula
   desc "Reads arbitrary PDF files and generates an XML file readable by Ipe"
   homepage "https://github.com/otfried/ipe-tools"
-  url "https://github.com/otfried/ipe-tools/archive/refs/tags/v7.2.29.1.tar.gz"
-  sha256 "604ef6e83ad8648fa09c41a788549db28193bb3638033d69cac2b0b3f33bd69b"
+  url "https://github.com/otfried/ipe-tools/archive/refs/tags/v7.2.29.2.tar.gz"
+  sha256 "c8de0dc7eb8fa959c96539fb19ebfb8e16f459e9b4ef9259aeb30b76072cd083"
   license "GPL-2.0-or-later"
-  revision 14
 
   no_autobump! because: :requires_manual_review
 
@@ -20,22 +19,6 @@ class Pdftoipe < Formula
   depends_on "pkgconf" => :build
   depends_on "poppler"
 
-  # Backport fix for `poppler` 25+ compatibility
-  # PR ref: https://github.com/otfried/ipe-tools/pull/72
-  # PR ref: https://github.com/otfried/ipe-tools/pull/77
-  patch do
-    url "https://github.com/otfried/ipe-tools/commit/0da954e50fbdedf43796291853890fe36248bc16.patch?full_index=1"
-    sha256 "65f7010897fa4dd94cfa933d986cae6978ddd4e33e2aa1479ec7c11786e100c3"
-  end
-  patch do
-    url "https://github.com/otfried/ipe-tools/commit/2f59d3b747a23cd4b13b09ebee9f703b8129116c.patch?full_index=1"
-    sha256 "b1b48088c9dd4067d862d788643c750fc6981102cd85f62a85f898948ca33771"
-  end
-
-  # Backport fix for poppler 26.02+ compatibility
-  # Upstream PR ref: https://github.com/otfried/ipe-tools/pull/79
-  patch :DATA
-
   def install
     cd "pdftoipe" do
       system "make"
@@ -50,59 +33,3 @@ class Pdftoipe < Formula
     assert_match "<ipestyle>", File.read("test.ipe")
   end
 end
-
-__END__
-diff --git a/pdftoipe/xmloutputdev.cpp b/pdftoipe/xmloutputdev.cpp
-index 506b16d..bb0b37d 100644
---- a/pdftoipe/xmloutputdev.cpp
-+++ b/pdftoipe/xmloutputdev.cpp
-@@ -290,21 +290,28 @@ void XmlOutputDev::startText(GfxState *state, double x, double y)
-   double xt, yt;
-   state->transform(x, y, &xt, &yt);
- 
--  const double *T = state->getTextMat();
--  const double *C = state->getCTM();
-+#if POPPLER_VERSION_AT_LEAST(26, 2, 0)
-+  const auto &T = state->getTextMat();
-+  const auto &C = state->getCTM();
-+  const double *Tp = T.data();
-+  const double *Cp = C.data();
-+#else
-+  const double *Tp = state->getTextMat();
-+  const double *Cp = state->getCTM();
-+#endif
- 
-   /*
-   fprintf(stderr, "TextMatrix = %g %g %g %g %g %g\n", 
--	  T[0], T[1], T[2], T[3], T[4], T[5]);
-+	  Tp[0], Tp[1], Tp[2], Tp[3], Tp[4], Tp[5]);
-   fprintf(stderr, "CTM = %g %g %g %g %g %g\n", 
--	  C[0], C[1], C[2], C[3], C[4], C[5]);
-+	  Cp[0], Cp[1], Cp[2], Cp[3], Cp[4], Cp[5]);
-   */
- 
-   double M[4];
--  M[0] = C[0] * T[0] + C[2] * T[1];
--  M[1] = C[1] * T[0] + C[3] * T[1];
--  M[2] = C[0] * T[2] + C[2] * T[3];
--  M[3] = C[1] * T[2] + C[3] * T[3];
-+  M[0] = Cp[0] * Tp[0] + Cp[2] * Tp[1];
-+  M[1] = Cp[1] * Tp[0] + Cp[3] * Tp[1];
-+  M[2] = Cp[0] * Tp[2] + Cp[2] * Tp[3];
-+  M[3] = Cp[1] * Tp[2] + Cp[3] * Tp[3];
- 
-  GfxRGB rgb;
-  state->getFillRGB(&rgb);
-@@ -348,7 +355,12 @@ void XmlOutputDev::drawImage(GfxState *state, Object *ref, Stream *str,
- 
-   writePSFmt("<image width=\"%d\" height=\"%d\"", width, height);
- 
-+#if POPPLER_VERSION_AT_LEAST(26, 2, 0)
-+  const auto &matArr = state->getCTM();
-+  const double *mat = matArr.data();
-+#else
-   const double *mat = state->getCTM();
-+#endif
-   double tx = mat[0] + mat[2] + mat[4];
-   double ty = mat[1] + mat[3] + mat[5];
-   writePSFmt(" rect=\"%g %g %g %g\"", mat[4], mat[5], tx, ty);
