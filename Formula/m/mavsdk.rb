bbf38cdc0314e43d1d17e584f6c39ff57e5b13f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-05 20:14:05 +0000

    mavsdk 3.15.0

--- Formula/m/mavsdk.rb
+++ Formula/m/mavsdk.rb
@@ -2,10 +2,9 @@ class Mavsdk < Formula
   desc "API and library for MAVLink compatible systems written in C++17"
   homepage "https://mavsdk.mavlink.io/main/en/index.html"
   url "https://github.com/mavlink/MAVSDK.git",
-      tag:      "v3.14.0",
-      revision: "7cbd8e5af40892bbb62f6025ee3ff14a70765829"
+      tag:      "v3.15.0",
+      revision: "721efdc45eedfe8761ceb7280dedca6004b1ea92"
   license "BSD-3-Clause"
-  revision 3
 
   livecheck do
     url :stable
