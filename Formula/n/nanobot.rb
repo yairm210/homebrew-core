036b5729c88dad6d8a5e87c4b905dac15f01496f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-05 18:29:30 +0000

    nanobot: update 0.0.54 bottle.

--- Formula/n/nanobot.rb
+++ Formula/n/nanobot.rb
@@ -7,12 +7,12 @@ class Nanobot < Formula
   head "https://github.com/nanobot-ai/nanobot.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "396abb7ea8685d50561a1abcc1a0415a1398b98a8fe61bbc1606e44d6e8ed133"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "396abb7ea8685d50561a1abcc1a0415a1398b98a8fe61bbc1606e44d6e8ed133"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "396abb7ea8685d50561a1abcc1a0415a1398b98a8fe61bbc1606e44d6e8ed133"
-    sha256 cellar: :any_skip_relocation, sonoma:        "093adac712024d07e68fbb4c7ab6fa3664413e6c24ce172880eacb9c73c0cd41"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4bb10683ba84d7967fde4d5b0192cf38c23f4d1a5b594f9e88cbe15653ef74e8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c86b2cb763e60e19dbfd8c715b947ec726f1d1585f1295c01a3bd1e095e460f3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2b125d668ca252514b0b0a4ac40bf13892af6a8392c0de84309febab2a3255cb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2b125d668ca252514b0b0a4ac40bf13892af6a8392c0de84309febab2a3255cb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2b125d668ca252514b0b0a4ac40bf13892af6a8392c0de84309febab2a3255cb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6c3541a5f4d8b0dcd557b38c719e9981b98098611250651f8f1deb5dfab43c8b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4c86df83387b281edc68c39adb6dc93b9fee53ee2fa13ebeda868e194f411339"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3416a132aa55654ba62225c6331f34bce92b8720a9468c877f771cb6e2293a3f"
   end
 
   depends_on "go" => :build
