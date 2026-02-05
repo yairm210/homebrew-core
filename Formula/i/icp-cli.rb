954b28764b4cfc675bdeb8a1baf1563913e3f215
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-05 03:29:06 +0000

    icp-cli: add 0.1.0 bottle.

--- Formula/i/icp-cli.rb
+++ Formula/i/icp-cli.rb
@@ -5,6 +5,15 @@ class IcpCli < Formula
   sha256 "98e79229b3b89b77aaa6237a5540ca719586e17501c7886dbfd5faf0a32d2364"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "38162080409c67940fcbcd5ed44bf36b3e0c80fc9c5506548323ed73cc9e128f"
+    sha256 cellar: :any,                 arm64_sequoia: "03cf035c80f3d8488ebf46dbc56e3861c2e3cf1ebf1fae51152722a801636b83"
+    sha256 cellar: :any,                 arm64_sonoma:  "41fbb44b38ae38c3fe55fc14a9b71ed1abd7b48fe90512d0e68865ec130b6aa7"
+    sha256 cellar: :any,                 sonoma:        "74fdf71c244ca69247d31a33735b776aeebb9b9dc7dee4ea301750d325fb76a9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "071dd13cb61fb4b8f33ed5dc9383e873ab92c5496db841d82faa4d82c1a9d014"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a59c2b898944eeccf14ee21da8d6f45ca2e3d93abde0d080c94936af1820af1"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3"
 
