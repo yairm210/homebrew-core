90b741295c733f6a6322a7264216699fcdee3c0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 00:18:32 +0000

    lazyjournal: update 0.8.5 bottle.

--- Formula/l/lazyjournal.rb
+++ Formula/l/lazyjournal.rb
@@ -7,12 +7,12 @@ class Lazyjournal < Formula
   head "https://github.com/Lifailon/lazyjournal.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2d60ad451b8301fbb64cdcece0b8da356daa25c1459ef21edce05caf5c25a17f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2d60ad451b8301fbb64cdcece0b8da356daa25c1459ef21edce05caf5c25a17f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2d60ad451b8301fbb64cdcece0b8da356daa25c1459ef21edce05caf5c25a17f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "03c4d1a90a6ffbeb4ba00df4e07f6d25a3ab8240f633a78c1a53b632c9595d6a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5db94f08841d06a917139e9244b6fb057c7699b836b71fcd2a782ac0fc282264"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ae0d2f165d0cdd65d0c6d134daad5fe9367fc371f87579aff343c9824d8cf28e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6bde455e98f3fe65e534813a5ed7bf17a0063083e970e1f44ef40a8ac62acdb7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6bde455e98f3fe65e534813a5ed7bf17a0063083e970e1f44ef40a8ac62acdb7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6bde455e98f3fe65e534813a5ed7bf17a0063083e970e1f44ef40a8ac62acdb7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5e0ae947a159934998b1440161ddb4ac72f3faf78566bb2c7e3eec28eeb9e58f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "30fea8709af118108b6f233e3fbcee3b6909bb045ec73825362af2a73ba645e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "555196fa2e59b70b597c09329b6a123bdcb60f544603eee363b7df53be4d783a"
   end
 
   depends_on "go" => :build
