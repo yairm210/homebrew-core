767bd48f73b6557b0896ffaa8071ababb7368804
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 17:51:19 +0000

    systemd 259.1

--- Formula/s/systemd.rb
+++ Formula/s/systemd.rb
@@ -3,8 +3,8 @@ class Systemd < Formula
 
   desc "System and service manager"
   homepage "https://systemd.io"
-  url "https://github.com/systemd/systemd/archive/refs/tags/v259.tar.gz"
-  sha256 "a84123692d1add7f9c48fd11cdf5f901393008c2d2ade667c18f25a20bf1290d"
+  url "https://github.com/systemd/systemd/archive/refs/tags/v259.1.tar.gz"
+  sha256 "7af4f36db512ad2f0f749a0f9886370edeb2bb5128014fc47cdf73702c7e1911"
   license all_of: [
     # Main license is LGPL-2.1-or-later while systemd-udevd is GPL-2.0-or-later
     "LGPL-2.1-or-later",
