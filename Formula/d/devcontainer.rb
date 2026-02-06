d2510a07f295b4bafbda400a90cc7c260d8a14f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 23:01:33 +0000

    devcontainer 0.83.0

--- Formula/d/devcontainer.rb
+++ Formula/d/devcontainer.rb
@@ -1,8 +1,8 @@
 class Devcontainer < Formula
   desc "Reference implementation for the Development Containers specification"
   homepage "https://containers.dev"
-  url "https://registry.npmjs.org/@devcontainers/cli/-/cli-0.82.0.tgz"
-  sha256 "a5508f00ee0d478a74c72671e2489bc3281ec63b1af2eb0601664de57e56f8ed"
+  url "https://registry.npmjs.org/@devcontainers/cli/-/cli-0.83.0.tgz"
+  sha256 "dc96b3e1fa48e9009948d25e4669e43b350037cdb65f18732b2de2bbf092a594"
   license "MIT"
 
   bottle do
