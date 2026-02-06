98d33a876b85cd7189123f504b9e4eda975d172a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 11:21:27 +0000

    mariadb@11.4 11.4.10
    mariadb@11.4: declare indirect deps with linkage for linux build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mariadb@11.4.rb
+++ Formula/m/mariadb@11.4.rb
@@ -1,8 +1,8 @@
 class MariadbAT114 < Formula
   desc "Drop-in replacement for MySQL"
   homepage "https://mariadb.org/"
-  url "https://archive.mariadb.org/mariadb-11.4.9/source/mariadb-11.4.9.tar.gz"
-  sha256 "8e481ca29b5a740444d45451c8ea2d93711cf525d6fa5d27bc9512cf8973b075"
+  url "https://archive.mariadb.org/mariadb-11.4.10/source/mariadb-11.4.10.tar.gz"
+  sha256 "14783ddc5edd966ff05aa0efd5ed6d3d369ed5b9e4080a448f00f87a9f0a4a6b"
   license "GPL-2.0-only"
 
   livecheck do
@@ -50,7 +50,6 @@ class MariadbAT114 < Formula
   uses_from_macos "libxcrypt"
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "openjdk" => :build
@@ -58,6 +57,7 @@ class MariadbAT114 < Formula
 
   on_linux do
     depends_on "linux-pam"
+    depends_on "zlib-ng-compat"
   end
 
   def install
