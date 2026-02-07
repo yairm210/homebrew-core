4920a9a57edb636e5a7225cc230f9543dc7fb806
Michael Cho <michael@michaelcho.dev>
2026-02-06 21:31:33 -0500

    pangene: use `zlib-ng-compat` on Linux

--- Formula/p/pangene.rb
+++ Formula/p/pangene.rb
@@ -19,7 +19,9 @@ class Pangene < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "487052e67934b027898914dcfec64d1805b0ab9f9457704c1bb5e2aed204d5a7"
   end
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "make"
