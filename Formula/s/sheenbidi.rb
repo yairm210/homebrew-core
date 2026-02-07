9cc5a5b085a695b971253790ddcecef50cd80ae1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-07 16:35:59 +0000

    sheenbidi: add 2.9.0 bottle.

--- Formula/s/sheenbidi.rb
+++ Formula/s/sheenbidi.rb
@@ -5,6 +5,15 @@ class Sheenbidi < Formula
   sha256 "e90ae142c6fc8b94366f3526f84b349a2c10137f87093db402fe51f6eace6d13"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "937f494d35e17ea7329000a4b2a65e44c40687b86f7116c7e4fd6841ffecb18f"
+    sha256 cellar: :any,                 arm64_sequoia: "8118d771952be49ca04efaaabbecae4b9b7abd5c8fd6d8a8581d4f4effe48f37"
+    sha256 cellar: :any,                 arm64_sonoma:  "254ff65fa1488865559fea8e76b2fb11f7fdf0053523f5fcc05314f6901a43a7"
+    sha256 cellar: :any,                 sonoma:        "2ae51b809fb3cccce63f59055a15ad85fa7abb6b509f05f697878fae2af4aa0d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6a7beddd0917b9af373f2d6ddfc581497551d8558cd04a5d401ed6ffbb9eea71"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed7320cea30ff43f03a54904ed3c4df20ae5f098da6bfe6c8bc42e55a92f858c"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
 
