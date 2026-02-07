6612aa7d19448714371daadef035e420ef217d22
Michael Cho <michael@michaelcho.dev>
2026-02-06 21:15:09 -0500

    binkd: use `zlib-ng-compat` on Linux

--- Formula/b/binkd.rb
+++ Formula/b/binkd.rb
@@ -28,16 +28,16 @@ class Binkd < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "d99d97e67e7079053da1c408b6064abff7ff447a116e2044e4a39d82ed654bc2"
   end
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     cp Dir["mkfls/unix/*"].select { |f| File.file? f }, "."
     inreplace "binkd.conf", "/var/", "#{var}/" if build.stable?
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}"
+    system "./configure", "--disable-silent-rules",
+                          "--mandir=#{man}",
+                          *std_configure_args
     system "make", "install"
   end
 
