996d540dade530ff2cad83d25c2e11c8f782acba
Michael Cho <michael@michaelcho.dev>
2026-02-06 21:34:57 -0500

    pigz: use `zlib-ng-compat` on Linux

--- Formula/p/pigz.rb
+++ Formula/p/pigz.rb
@@ -23,7 +23,9 @@ class Pigz < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6f8eefa10c0d4704e5ab22e5211d683972f25e1573618191e6d29342a838c44"
   end
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
