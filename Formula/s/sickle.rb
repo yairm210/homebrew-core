8b8db9a2e36e115b644bde210681334fae38bf59
Michael Cho <michael@michaelcho.dev>
2026-02-06 21:38:51 -0500

    sickle: use `zlib-ng-compat` on Linux

--- Formula/s/sickle.rb
+++ Formula/s/sickle.rb
@@ -23,7 +23,9 @@ class Sickle < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d084fefaf95e4433a64685c9c332964389911bbbdbb038aeeb87c2d16f06f2ab"
   end
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "make"
