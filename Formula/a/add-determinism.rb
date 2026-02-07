4cbe5b5cc3c3df8cbbbc57075145feece37ffd87
Michael Cho <michael@michaelcho.dev>
2026-02-07 16:10:34 -0500

    add-determinism: use `zlib-ng-compat` on Linux

--- Formula/a/add-determinism.rb
+++ Formula/a/add-determinism.rb
@@ -18,10 +18,10 @@ class AddDeterminism < Formula
   depends_on "rust" => :build
 
   uses_from_macos "llvm" => :build
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "libselinux"
+    depends_on "zlib-ng-compat"
   end
 
   def install
