a9db75fea03828e11776986f188f7faa79f2dea3
Michael Cho <michael@michaelcho.dev>
2026-02-06 21:35:25 -0500

    potrace: use `zlib-ng-compat` on Linux

--- Formula/p/potrace.rb
+++ Formula/p/potrace.rb
@@ -28,7 +28,9 @@ class Potrace < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d47c98611b15465f29e55972018135927d42929e5901153ad99c6579de32863c"
   end
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   resource "head.pbm" do
     url "https://potrace.sourceforge.net/img/head.pbm"
@@ -36,10 +38,9 @@ class Potrace < Formula
   end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}",
-                          "--with-libpotrace"
+    system "./configure", "--mandir=#{man}",
+                          "--with-libpotrace",
+                          *std_configure_args
     system "make", "install"
   end
 
