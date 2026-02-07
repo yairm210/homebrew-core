66713c7d73a841bf9d87f0ab311b52e814cda7aa
Michael Cho <michael@michaelcho.dev>
2026-02-07 16:12:12 -0500

    aide: use `zlib-ng-compat` on Linux

--- Formula/a/aide.rb
+++ Formula/a/aide.rb
@@ -31,7 +31,10 @@ class Aide < Formula
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
   uses_from_macos "curl"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with cask: "aide-app"
 
