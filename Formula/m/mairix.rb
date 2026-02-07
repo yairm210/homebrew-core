48b6cf0f5825152d330ec1886dec13a0b787e62e
Michael Cho <michael@michaelcho.dev>
2026-02-06 21:28:54 -0500

    mairix: use `zlib-ng-compat` on Linux

--- Formula/m/mairix.rb
+++ Formula/m/mairix.rb
@@ -31,7 +31,10 @@ class Mairix < Formula
 
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV.deparallelize
