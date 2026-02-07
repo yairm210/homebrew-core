6e03a218bc8f79cc4f0668385164d03651f84f24
Michael Cho <michael@michaelcho.dev>
2026-02-07 16:11:26 -0500

    afflib: use `zlib-ng-compat` on Linux

--- Formula/a/afflib.rb
+++ Formula/a/afflib.rb
@@ -27,7 +27,10 @@ class Afflib < Formula
 
   uses_from_macos "curl"
   uses_from_macos "expat"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def python3
     which("python3.14")
