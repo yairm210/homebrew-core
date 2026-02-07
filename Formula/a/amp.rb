ccd3cb31f1568160f9c6ea2a67638797c83168db
Michael Cho <michael@michaelcho.dev>
2026-02-07 16:18:40 -0500

    amp: use `zlib-ng-compat` on Linux

--- Formula/a/amp.rb
+++ Formula/a/amp.rb
@@ -21,10 +21,10 @@ class Amp < Formula
   depends_on "rust" => :build
 
   uses_from_macos "python" => :build
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "libxcb"
+    depends_on "zlib-ng-compat"
   end
 
   def install
