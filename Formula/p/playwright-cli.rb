c4d6fc74b13a5df340266546980006abfc54eba4
Sergey Kochetkov <skoch13@pm.me>
2026-02-07 22:34:53 +0100

    playwright-cli 0.1.0 (new formula)

---
+++ Formula/p/playwright-cli.rb
@@ -0,0 +1,22 @@
+class PlaywrightCli < Formula
+  desc "CLI for Playwright: record/generate code, inspect selectors, take screenshots"
+  homepage "https://playwright.dev"
+  url "https://registry.npmjs.org/@playwright/cli/-/cli-0.1.0.tgz"
+  sha256 "83cc21857b4e76645b49b8cee4663cbc49b0c549ae3ead0f76cad17937f2df5e"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+
+    node_modules = libexec/"lib/node_modules/@playwright/cli/node_modules"
+    deuniversalize_machos node_modules/"fsevents/fsevents.node" if OS.mac?
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/playwright-cli --version")
+    assert_match "no browsers", shell_output("#{bin}/playwright-cli list")
+  end
+end
