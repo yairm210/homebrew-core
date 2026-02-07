192921cf9c2e03a816f903804190e817e201dd43
Michael Cho <michael@michaelcho.dev>
2026-02-06 21:10:23 -0500

    adapterremoval: use `zlib-ng-compat` on Linux

--- Formula/a/adapterremoval.rb
+++ Formula/a/adapterremoval.rb
@@ -17,7 +17,10 @@ class Adapterremoval < Formula
   end
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV.deparallelize
