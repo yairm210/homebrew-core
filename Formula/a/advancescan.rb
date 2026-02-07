aa6e03e7c7ba66d1c24d91354ad13cc0712da9a9
Michael Cho <michael@michaelcho.dev>
2026-02-06 21:12:23 -0500

    advancescan: use `zlib-ng-compat` on Linux

--- Formula/a/advancescan.rb
+++ Formula/a/advancescan.rb
@@ -23,7 +23,9 @@ class Advancescan < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "3a42de6a209af1dd02c8544621f7da76afabd415d46265b548b3e35b7667b45d"
   end
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV.cxx11
