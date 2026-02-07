76537c2b27112aa1dfb0d5f49e507e5ed0871a47
Michael Cho <michael@michaelcho.dev>
2026-02-06 21:34:42 -0500

    pgpdump: use `zlib-ng-compat` on Linux

--- Formula/p/pgpdump.rb
+++ Formula/p/pgpdump.rb
@@ -22,7 +22,10 @@ class Pgpdump < Formula
   end
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
