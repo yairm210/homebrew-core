e0041267b68399ff82185970ba2c96e6aceda5eb
Michael Cho <michael@michaelcho.dev>
2026-02-06 21:41:31 -0500

    ucon64: use `zlib-ng-compat` on Linux

--- Formula/u/ucon64.rb
+++ Formula/u/ucon64.rb
@@ -30,7 +30,10 @@ class Ucon64 < Formula
   end
 
   uses_from_macos "unzip" => [:build, :test]
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # ucon64's normal install process installs the discmage library in
