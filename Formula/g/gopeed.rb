d0a43556d05e5c5414c90d27cc3c2a4f9e823355
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-08 14:45:53 +0000

    gopeed: update 1.9.1 bottle.

--- Formula/g/gopeed.rb
+++ Formula/g/gopeed.rb
@@ -7,12 +7,12 @@ class Gopeed < Formula
   head "https://github.com/GopeedLab/gopeed.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7faf00b1e70ffa23dcb7d61d33c18568aa73f1230d9275382a6ece610f024501"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "087ed4941de0968e8f5582c64569ecfca4df1185f64696551376582e652d4e6d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5c025be4dcf4912dea3ea711082508549d431a7c4082189f571542462620d9fa"
-    sha256 cellar: :any_skip_relocation, sonoma:        "664a67391f4125dc962cc9d07984b2acb622bc8e62bd09f182ae2964e45e8880"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dcf0798783a2a8e127913a7839b203bfc8c7a9a5b9625aeac1eb9992b0a55836"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ad2a2b9f75ffb6c78723fcb0331d5ce36ed1441efc54e7b35dbf686fd6eadbd7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eb2603abe43db5588aabf5917f8644f72e5e28970b3de6db3962ed43e776c69e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "74c7c8dd529326775e8df35af9538d0ad9ba582bcd669ab3a4378f388e380785"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d9b8b8169c13168c6ee2bbca3c62a8e0ba8694b646d4215e449aee52eaf7fd8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6b5f7c025b7584a43655942abbee5760a95b637ba3685164bf6364f8f191851f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "22772f0e15a20aec1cc2a24c269e0c11adea5cca2804db99db12592c3ecaf706"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "debb1dfe749930277304410cc2e34610aa1526e12fc1cef84403f94e425b5ce2"
   end
 
   depends_on "go" => :build
