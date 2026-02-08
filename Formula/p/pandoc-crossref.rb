3666654f0b2a9e5b3f9665aa025381ef1583b20c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-08 08:45:08 +0000

    pandoc-crossref: update 0.3.23a bottle.

--- Formula/p/pandoc-crossref.rb
+++ Formula/p/pandoc-crossref.rb
@@ -7,12 +7,12 @@ class PandocCrossref < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "142dc37ee88f270e5b74d3a8eacd46c46c22954dfc276856d3869f0991e45df5"
-    sha256 cellar: :any,                 arm64_sequoia: "accd50ffef51a871b9d9d5b37098776aa8278621ddd5a683670855bd0adba916"
-    sha256 cellar: :any,                 arm64_sonoma:  "d6ea32accdc7592b7f8d26d53a8fff4c4785976f4780b1aea8a5be763179a9f1"
-    sha256 cellar: :any,                 sonoma:        "084f98f78bb4d9799f2bb0aa55a37276b5d052073abdc9105a0512899e29c402"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "899087e66b519e9d91b8d5dd73da6517b9aab9aac7a6f39579cb2baf99c73049"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a056548c445e9ccf662e90310ca955481868997103af9c125a07adfe9ba9d920"
+    sha256 cellar: :any,                 arm64_tahoe:   "dcc0c04cee89e8b64dfc6a375435e827a6662c0ea34f902f2869bdb7acff4864"
+    sha256 cellar: :any,                 arm64_sequoia: "f04c16c8ce3486e544f8cf71c63ad6249fb49297de84f4c3423782a6432712dc"
+    sha256 cellar: :any,                 arm64_sonoma:  "80837bc0d92ed327217a4fd932fce76b457dd088fedf98c1830dfc4f3af9e7e5"
+    sha256 cellar: :any,                 sonoma:        "8f453b22c706eff1e653c859d7c62bb7c403606043d849cd65295e237b49df0d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b57232e3b86978936a6ef517d2c8273008028010e0f34ceac211be5ebf3eb382"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "81427608e02bc74cfe4d81525d4a5f0aa3a6ff06f088a082611b73af9578d4d7"
   end
 
   depends_on "cabal-install" => :build
