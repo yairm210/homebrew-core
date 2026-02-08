6aa038d27534ec30ae762e1e661ddb477d18bd86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-08 05:47:56 +0000

    pandoc-crossref 0.3.23a
    pandoc-crossref: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pandoc-crossref.rb
+++ Formula/p/pandoc-crossref.rb
@@ -1,8 +1,9 @@
 class PandocCrossref < Formula
   desc "Pandoc filter for numbering and cross-referencing"
   homepage "https://github.com/lierdakil/pandoc-crossref"
-  url "https://github.com/lierdakil/pandoc-crossref/archive/refs/tags/v0.3.23.tar.gz"
-  sha256 "c4104b5cd2fe8fa214475a332cf5c0120f801f09d6b4cef0659fba908675b296"
+  url "https://github.com/lierdakil/pandoc-crossref/archive/refs/tags/v0.3.23a.tar.gz"
+  version "0.3.23a"
+  sha256 "7b3638c8b8d416f28e950cf650c52d3e961f53ce6cc640133caf8ee99b2efade"
   license "GPL-2.0-or-later"
 
   bottle do
@@ -22,9 +23,6 @@ class PandocCrossref < Formula
   uses_from_macos "unzip" => :build
   uses_from_macos "libffi"
   uses_from_macos "zlib"
-
-  # Fix pandoc upper bound to support pandoc 3.9, upstream bug report, https://github.com/lierdakil/pandoc-crossref/issues/503
-  patch :DATA
 
   def install
     rm("cabal.project.freeze")
@@ -49,76 +47,3 @@ class PandocCrossref < Formula
     refute_match "WARNING: pandoc-crossref was compiled", output
   end
 end
-
-__END__
-diff --git a/package.yaml b/package.yaml
-index 3b8d7b6..c851445 100644
---- a/package.yaml
-+++ b/package.yaml
-@@ -30,7 +30,7 @@ data-files:
- dependencies:
-   base: ">=4.19 && <5"
-   text: ">=1.2.2 && <2.2"
--  pandoc: ">=3.8.2 && <3.9"
-+  pandoc: ">=3.8.2 && <3.10"
-   pandoc-types: ">= 1.23 && < 1.24"
- _deps:
-   containers: &containers { containers: ">=0.1 && <0.9" }
-diff --git a/pandoc-crossref.cabal b/pandoc-crossref.cabal
-index ec6b1cf..18a6584 100644
---- a/pandoc-crossref.cabal
-+++ b/pandoc-crossref.cabal
-@@ -175,7 +175,7 @@ library
-     , microlens >=0.4.12.0 && <0.5.0.0
-     , microlens-mtl >=0.2.0.1 && <0.3.0.0
-     , mtl >=1.1 && <2.4
--    , pandoc >=3.8.2 && <3.9
-+    , pandoc >=3.8.2 && <3.10
-     , pandoc-crossref-internal
-     , pandoc-types ==1.23.*
-     , text >=1.2.2 && <2.2
-@@ -229,7 +229,7 @@ library pandoc-crossref-internal
-     , microlens-mtl >=0.2.0.1 && <0.3.0.0
-     , microlens-th >=0.4.3.10 && <0.5.0.0
-     , mtl >=1.1 && <2.4
--    , pandoc >=3.8.2 && <3.9
-+    , pandoc >=3.8.2 && <3.10
-     , pandoc-types ==1.23.*
-     , syb >=0.4 && <0.8
-     , template-haskell >=2.7.0.0 && <3.0.0.0
-@@ -259,7 +259,7 @@ executable pandoc-crossref
-     , gitrev >=1.3.1 && <1.4
-     , open-browser >=0.2 && <0.4
-     , optparse-applicative >=0.13 && <0.20
--    , pandoc >=3.8.2 && <3.9
-+    , pandoc >=3.8.2 && <3.10
-     , pandoc-crossref
-     , pandoc-types ==1.23.*
-     , template-haskell >=2.7.0.0 && <3.0.0.0
-@@ -289,7 +289,7 @@ test-suite test-integrative
-     , directory >=1 && <1.4
-     , filepath >=1.1 && <1.6
-     , hspec >=2.4.4 && <3
--    , pandoc >=3.8.2 && <3.9
-+    , pandoc >=3.8.2 && <3.10
-     , pandoc-crossref
-     , pandoc-types ==1.23.*
-     , text >=1.2.2 && <2.2
-@@ -325,7 +325,7 @@ test-suite test-pandoc-crossref
-     , microlens >=0.4.12.0 && <0.5.0.0
-     , microlens-mtl >=0.2.0.1 && <0.3.0.0
-     , mtl >=1.1 && <2.4
--    , pandoc >=3.8.2 && <3.9
-+    , pandoc >=3.8.2 && <3.10
-     , pandoc-crossref
-     , pandoc-crossref-internal
-     , pandoc-types ==1.23.*
-@@ -354,7 +354,7 @@ benchmark simple
-   build-depends:
-       base >=4.19 && <5
-     , criterion >=1.5.9.0 && <1.7
--    , pandoc >=3.8.2 && <3.9
-+    , pandoc >=3.8.2 && <3.10
-     , pandoc-crossref
-     , pandoc-types ==1.23.*
-     , text >=1.2.2 && <2.2
