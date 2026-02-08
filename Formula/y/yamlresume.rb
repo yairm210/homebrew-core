4832c6fc4e5890e4b40f21f99098004a9886ef15
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-08 11:47:19 +0000

    yamlresume: add 0.11.1 bottle.

--- Formula/y/yamlresume.rb
+++ Formula/y/yamlresume.rb
@@ -5,6 +5,15 @@ class Yamlresume < Formula
   sha256 "eecf19120041bedfa4027bf3d95e98cc6cd82b8822816907db991286c4e8c490"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f3526e6790c2f04d77fe11d51388cc48f0fa802c8746203548368f1838a1fa24"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa77e1c051270179890c7a9b623b3308cd8cabcaf1bd3e39a1a9c12a36a416bb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fa77e1c051270179890c7a9b623b3308cd8cabcaf1bd3e39a1a9c12a36a416bb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "69536254f372ffc76d7d67e7491260e734cfda95a1e7e1e4b68fb4786c0e8798"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e0be2248bf3e1254c6f3fc055bb131c94a6cdc7c96ec365bc6a7360dcdbe117c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e0be2248bf3e1254c6f3fc055bb131c94a6cdc7c96ec365bc6a7360dcdbe117c"
+  end
+
   depends_on "node"
 
   def install
