fab7f7345d83439b2f388711084150484496be74
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-08 11:39:31 +0000

    try-rs: add 1.0.0 bottle.

--- Formula/t/try-rs.rb
+++ Formula/t/try-rs.rb
@@ -6,6 +6,15 @@ class TryRs < Formula
   license "MIT"
   head "https://github.com/tassiovirginio/try-rs.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "15f0a84f822694949a236b063b384373746bd76c7657d1ddc6edd32ad0efbbd5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "785e287943cbb52c66e7ba6909570eeeb083b40fd9b2a2af81438dc783029899"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "42e0ec2fa5c83c5718d9f54b82e9b35774f16950ce05809632bc127e9058c23d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d992ed7c6528cb63de8e58a5e573e9c268123294353a95d8cd502078ec8171a9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7f9956152b2f3bb25db4ab8d2926b17887aee9eff6fa6c131c1407949584faf7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6d97a3fcbb46a46a3f9ba592c0afc6972e94a1e61c28f3106eb6e8c68ab32bbc"
+  end
+
   depends_on "rust" => :build
 
   def install
