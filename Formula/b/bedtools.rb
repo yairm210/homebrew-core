9b435dfd22628c7c5001cf78b083398e420ee462
Michael Cho <michael@michaelcho.dev>
2026-02-07 20:53:07 -0500

    bedtools: use `zlib-ng-compat` on Linux

--- Formula/b/bedtools.rb
+++ Formula/b/bedtools.rb
@@ -22,7 +22,10 @@ class Bedtools < Formula
 
   uses_from_macos "python" => :build
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "make"
