262ba296180b12878b7c524a5f3cdb1b6b4c30b6
Michael Cho <michael@michaelcho.dev>
2026-02-07 20:53:48 -0500

    bgpdump: use `zlib-ng-compat` on Linux

--- Formula/b/bgpdump.rb
+++ Formula/b/bgpdump.rb
@@ -31,14 +31,14 @@ class Bgpdump < Formula
   depends_on "autoconf" => :build
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./bootstrap.sh"
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    system "./configure", "--disable-silent-rules", *std_configure_args
     system "make"
     system "make", "install"
   end
