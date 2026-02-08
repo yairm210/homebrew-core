cf2a97281ffe3fcbdd37f8926855bae65119a2b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-08 23:17:23 +0000

    numbat 1.23.0

--- Formula/n/numbat.rb
+++ Formula/n/numbat.rb
@@ -1,8 +1,8 @@
 class Numbat < Formula
   desc "Statically typed programming language for scientific computations"
   homepage "https://numbat.dev/"
-  url "https://github.com/sharkdp/numbat/archive/refs/tags/v1.22.0.tar.gz"
-  sha256 "db5d9c2c0ec113f6773c22e7c09c51c08a62a196ca28a2008ff11546c77297bb"
+  url "https://github.com/sharkdp/numbat/archive/refs/tags/v1.23.0.tar.gz"
+  sha256 "578c0ff6cb80098baca84042a104644758907002e4fc7ba839a1266be7f4af28"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sharkdp/numbat.git", branch: "main"
 
