d1b16fea09ccbe8a842a5fed5a46ba98f99ff1f4
Michael Cho <michael@michaelcho.dev>
2026-02-07 21:01:08 -0500

    bulk_extractor: use `zlib-ng-compat` on Linux

--- Formula/b/bulk_extractor.rb
+++ Formula/b/bulk_extractor.rb
@@ -37,10 +37,10 @@ class BulkExtractor < Formula
   uses_from_macos "flex" => :build
   uses_from_macos "expat"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "openssl@3" # uses CommonCrypto on macOS
+    depends_on "zlib-ng-compat"
   end
 
   def install
