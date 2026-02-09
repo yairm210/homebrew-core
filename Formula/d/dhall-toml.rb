37b658c9fda5e7f369b724d7f847a7f14274aa79
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:55:28 -0500

    dhall-toml: use `zlib-ng-compat` on Linux

--- Formula/d/dhall-toml.rb
+++ Formula/d/dhall-toml.rb
@@ -24,7 +24,10 @@ class DhallToml < Formula
 
   uses_from_macos "libffi"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Workaround to build aeson with GHC 9.14, https://github.com/haskell/aeson/issues/1155
