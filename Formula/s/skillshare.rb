e40cc7014a36680d4074fd40b264b4585a6b0eeb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-09 08:53:14 +0000

    skillshare: add 0.10.2 bottle.

--- Formula/s/skillshare.rb
+++ Formula/s/skillshare.rb
@@ -6,6 +6,15 @@ class Skillshare < Formula
   license "MIT"
   head "https://github.com/runkids/skillshare.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b017fc2ad53a203b116498681f6dd6e375a01e98ede5255898dcbab57496a2dd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b017fc2ad53a203b116498681f6dd6e375a01e98ede5255898dcbab57496a2dd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b017fc2ad53a203b116498681f6dd6e375a01e98ede5255898dcbab57496a2dd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b8e5f9e281480abb534a67f138aa9f403f6182bf01e4f620f8a980b0acf0ca3c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "26b30739400c5126eec87413c9c2f4aa6d9887ad499bb0f6f08b148a565cf079"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2add5462f82f1df8c1da2ad1d519c8d531b23b6a4e6e44c8c2ba0c3fe3b8deb"
+  end
+
   depends_on "go" => :build
 
   def install
