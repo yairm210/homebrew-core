8adc886112090d110507fd0f21032f243aa55650
Michael Cho <michael@michaelcho.dev>
2026-02-08 20:16:34 -0500

    x86_64-elf-gcc: use `zlib-ng-compat` on Linux

--- Formula/x/x86_64-elf-gcc.rb
+++ Formula/x/x86_64-elf-gcc.rb
@@ -27,7 +27,9 @@ class X8664ElfGcc < Formula
   depends_on "x86_64-elf-binutils"
   depends_on "zstd"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     target = "x86_64-elf"
