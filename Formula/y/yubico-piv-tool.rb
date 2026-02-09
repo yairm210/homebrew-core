9d7da83ba65e7e18040d8edb5e60edb95e915cd8
Michael Cho <michael@michaelcho.dev>
2026-02-08 20:14:10 -0500

    yubico-piv-tool: use `zlib-ng-compat` on Linux

--- Formula/y/yubico-piv-tool.rb
+++ Formula/y/yubico-piv-tool.rb
@@ -27,7 +27,10 @@ class YubicoPivTool < Formula
   depends_on "openssl@3"
 
   uses_from_macos "pcsc-lite"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV.append_to_cflags "-I#{Formula["pcsc-lite"].opt_include}/PCSC" unless OS.mac?
