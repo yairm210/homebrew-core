e0e45edd67f007cdd7c469efa44a8f788668f882
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:37:17 -0500

    core-lightning: use `zlib-ng-compat` on Linux

--- Formula/c/core-lightning.rb
+++ Formula/c/core-lightning.rb
@@ -38,10 +38,13 @@ class CoreLightning < Formula
   uses_from_macos "jq" => :build, since: :sequoia
   uses_from_macos "python"
   uses_from_macos "sqlite"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gnu-sed" => :build
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   pypi_packages package_name:   "",
