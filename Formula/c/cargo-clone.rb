d47f323e2bbdd0789160d2ee53367de510bd1e73
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:30:47 -0500

    cargo-clone: use `zlib-ng-compat` on Linux

--- Formula/c/cargo-clone.rb
+++ Formula/c/cargo-clone.rb
@@ -24,7 +24,10 @@ class CargoClone < Formula
   depends_on "openssl@3"
 
   uses_from_macos "curl"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Ensure that the `openssl` crate picks up the intended library.
