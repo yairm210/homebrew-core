0f5b5c70772b82372b9603bd8186f8a4c6f04012
Michael Cho <michael@michaelcho.dev>
2026-02-09 18:34:12 -0500

    dillo: use `zlib-ng-compat` on Linux

--- Formula/d/dillo.rb
+++ Formula/d/dillo.rb
@@ -30,10 +30,9 @@ class Dillo < Formula
   depends_on "libpng"
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "libx11"
+    depends_on "zlib-ng-compat"
   end
 
   def install
