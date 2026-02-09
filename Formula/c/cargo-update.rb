ff37cf9426e67e0024c676d450ccce2fa19eed81
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:31:45 -0500

    cargo-update: use `zlib-ng-compat` on Linux

--- Formula/c/cargo-update.rb
+++ Formula/c/cargo-update.rb
@@ -26,7 +26,10 @@ class CargoUpdate < Formula
   depends_on "openssl@3"
 
   uses_from_macos "curl"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV["LIBGIT2_NO_VENDOR"] = "1"
