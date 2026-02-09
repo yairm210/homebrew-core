5d6bbbab3991b05db32e4ea0b00db30fc43dd526
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:31:13 -0500

    cargo-geiger: use `zlib-ng-compat` on Linux

--- Formula/c/cargo-geiger.rb
+++ Formula/c/cargo-geiger.rb
@@ -22,7 +22,9 @@ class CargoGeiger < Formula
   depends_on "rustup" => :test
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Ensure that the `openssl` crate picks up the intended library.
