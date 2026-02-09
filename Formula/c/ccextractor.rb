ca0455d9494f362c71a678bea7394c107ce2836c
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:34:29 -0500

    ccextractor: use `zlib-ng-compat` on Linux

--- Formula/c/ccextractor.rb
+++ Formula/c/ccextractor.rb
@@ -23,12 +23,11 @@ class Ccextractor < Formula
   depends_on "protobuf-c"
   depends_on "utf8proc"
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "llvm" => :build
     depends_on "leptonica"
     depends_on "tesseract"
+    depends_on "zlib-ng-compat"
   end
 
   def install
