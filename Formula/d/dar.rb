b7714d7762da7971d92dc6283c2260f87bdfd7bc
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:52:17 -0500

    dar: use `zlib-ng-compat` on Linux

--- Formula/d/dar.rb
+++ Formula/d/dar.rb
@@ -27,10 +27,13 @@ class Dar < Formula
   depends_on "xz"
   depends_on "zstd"
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   def install
