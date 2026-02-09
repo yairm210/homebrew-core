a5a0ea42bf9adf7eb4ac49ff0320863644e24ae9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-09 20:57:00 +0000

    lnk: update 0.9.0 bottle.

--- Formula/l/lnk.rb
+++ Formula/l/lnk.rb
@@ -6,13 +6,12 @@ class Lnk < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "20085bbd583ce52186cbec36c213bc4ae6aae60fc66ea4a24e0af4e01b0ed8ed"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "20085bbd583ce52186cbec36c213bc4ae6aae60fc66ea4a24e0af4e01b0ed8ed"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "20085bbd583ce52186cbec36c213bc4ae6aae60fc66ea4a24e0af4e01b0ed8ed"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d6a2b0a711fbddb9a493a4dee9f6b82bf13ff199fa106b2e307c00a703b64ef6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3dbb89d967f4233593f2fe396038b5f50f247a4c995ba793234ca32ec3e85e63"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f9ce81bb0758298bb85d26cec1b34eda869dc263d36d554944dced3a40aa467"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "055a54409a0a2a53aa4b0d3f2579eb99f73b0594d071ab9d5491c32237bd9af0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "055a54409a0a2a53aa4b0d3f2579eb99f73b0594d071ab9d5491c32237bd9af0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "055a54409a0a2a53aa4b0d3f2579eb99f73b0594d071ab9d5491c32237bd9af0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d326f2fb6241ca83a3b15574abfb8947968a5ef017e220ddc4c51d210c9c423d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "78c8e6195d4b0d0e97deb873aafcb92b85867a0bceba10f91c87960acdc3fc37"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "20920e2cd0a28753716343bb44158f1113d446346bdf54ac2387af64d6b77dc7"
   end
 
   depends_on "go" => :build
