4e9a840affccd2bd7d8d8d00a5a2290fcb29368c
Michael Cho <michael@michaelcho.dev>
2026-02-09 18:35:43 -0500

    dovecot: use `zlib-ng-compat` on Linux

--- Formula/d/dovecot.rb
+++ Formula/d/dovecot.rb
@@ -42,13 +42,13 @@ class Dovecot < Formula
   uses_from_macos "bzip2"
   uses_from_macos "libxcrypt"
   uses_from_macos "sqlite"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "libtirpc"
     depends_on "linux-pam"
     depends_on "lz4"
     depends_on "xz"
+    depends_on "zlib-ng-compat"
     depends_on "zstd"
   end
 
