dba353f3a797d17298c4b7bdf6fd559a68f0c230
Michael Cho <michael@michaelcho.dev>
2026-02-08 21:55:31 -0500

    freetype: use `zlib-ng-compat` on Linux

--- Formula/f/freetype.rb
+++ Formula/f/freetype.rb
@@ -5,7 +5,7 @@ class Freetype < Formula
   mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.14.1.tar.xz"
   sha256 "32427e8c471ac095853212a37aef816c60b42052d4d9e48230bab3bdf2936ccc"
   license "FTL"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable
@@ -21,16 +21,22 @@ class Freetype < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "1ac27d046ced62aa0b41ed4087e91b56fcd01299f7a760a944ec0260b9001983"
   end
 
-  depends_on "gnu-sed" => :build
   depends_on "pkgconf" => :build
   depends_on "libpng"
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "gnu-sed" => :build
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # https://gitlab.freedesktop.org/freetype/freetype/-/issues/1358
-    ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin"
+    ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin" if OS.mac?
 
     # This file will be installed to bindir, so we want to avoid embedding the
     # absolute path to the pkg-config shim.
