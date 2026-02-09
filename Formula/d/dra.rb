c2e69bf3a47d4fa1793e458c19fbb5e9fa132bc9
Michael Cho <michael@michaelcho.dev>
2026-02-09 18:36:00 -0500

    dra: use `zlib-ng-compat` on Linux

--- Formula/d/dra.rb
+++ Formula/d/dra.rb
@@ -17,7 +17,9 @@ class Dra < Formula
 
   depends_on "rust" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
