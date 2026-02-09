4a6d6fea42568845aea6ffd03a7b5f650350f16c
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:36:15 -0500

    code-cli: use `zlib-ng-compat` on Linux

--- Formula/c/code-cli.rb
+++ Formula/c/code-cli.rb
@@ -24,7 +24,9 @@ class CodeCli < Formula
   depends_on "rust" => :build
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with cask: "visual-studio-code"
 
