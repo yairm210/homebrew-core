46ba9e50eb9ad782d1dfc0adfc7185eb7a4e1520
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:55:17 -0500

    dhall-lsp-server: use `zlib-ng-compat` on Linux

--- Formula/d/dhall-lsp-server.rb
+++ Formula/d/dhall-lsp-server.rb
@@ -39,7 +39,10 @@ class DhallLspServer < Formula
 
   uses_from_macos "libffi"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = if build.head?
