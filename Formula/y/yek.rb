9265b4adb0d6575c590ce4f617c067be194b3a1b
Michael Cho <michael@michaelcho.dev>
2026-02-08 20:13:29 -0500

    yek: use `zlib-ng-compat` on Linux

--- Formula/y/yek.rb
+++ Formula/y/yek.rb
@@ -24,7 +24,10 @@ class Yek < Formula
   depends_on "rust" => :build
 
   depends_on "openssl@3"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
