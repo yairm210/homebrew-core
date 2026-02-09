3f9cd060b102196b966de727c5d2bddf2b91f6f6
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:36:01 -0500

    codanna: use `zlib-ng-compat` on Linux

--- Formula/c/codanna.rb
+++ Formula/c/codanna.rb
@@ -18,7 +18,9 @@ class Codanna < Formula
   depends_on "rust" => :build
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args, "--all-features"
