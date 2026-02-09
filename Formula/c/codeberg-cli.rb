4ed3914cbeb00e356a501dcff46dcd72d59eff69
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:36:38 -0500

    codeberg-cli: use `zlib-ng-compat` on Linux

--- Formula/c/codeberg-cli.rb
+++ Formula/c/codeberg-cli.rb
@@ -19,7 +19,9 @@ class CodebergCli < Formula
   depends_on "rust" => :build
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
