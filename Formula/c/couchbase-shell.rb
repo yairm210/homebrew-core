2aaadd83de176f47a1f0d1dd950e7afd8bb3509c
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:38:04 -0500

    couchbase-shell: use `zlib-ng-compat` on Linux

--- Formula/c/couchbase-shell.rb
+++ Formula/c/couchbase-shell.rb
@@ -20,12 +20,11 @@ class CouchbaseShell < Formula
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "libx11"
     depends_on "libxcb"
     depends_on "openssl@3"
+    depends_on "zlib-ng-compat"
   end
 
   def install
