eaf2a22228c49ee0f18e1ea41eaf5e016b41885a
Michael Cho <michael@michaelcho.dev>
2026-02-09 16:36:27 -0500

    code2prompt: use `zlib-ng-compat` on Linux

--- Formula/c/code2prompt.rb
+++ Formula/c/code2prompt.rb
@@ -19,7 +19,9 @@ class Code2prompt < Formula
   depends_on "rust" => :build
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Ensure the correct `openssl` will be picked up.
