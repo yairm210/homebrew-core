3e368d633ab74ecd699679e0aafce91488a49f84
Patrick Linnane <patrick@linnane.io>
2026-02-08 22:11:02 -0800

    skillshare 0.10.2 (new formula)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Formula/s/skillshare.rb
@@ -0,0 +1,29 @@
+class Skillshare < Formula
+  desc "Sync skills across AI CLI tools"
+  homepage "https://skillshare.runkids.cc"
+  url "https://github.com/runkids/skillshare/archive/refs/tags/v0.10.2.tar.gz"
+  sha256 "b631d891bfcef0989479841ed55464eb2756ce559ed6419ee60e6e6721436b28"
+  license "MIT"
+  head "https://github.com/runkids/skillshare.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    # Avoid building web UI
+    ui_path = "internal/server/dist"
+    mkdir_p ui_path
+    (buildpath/"#{ui_path}/index.html").write "<!DOCTYPE html><html><body><h1>UI not built</h1></body></html>"
+
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/skillshare"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/skillshare version")
+
+    assert_match "config not found", shell_output("#{bin}/skillshare sync 2>&1", 1)
+  end
+end
