447ed893050b24749cf13e63dc7337a85533a3b6
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:39:29 -0500

    karchive: use `zlib-ng-compat` on Linux

--- Formula/k/karchive.rb
+++ Formula/k/karchive.rb
@@ -36,7 +36,10 @@ class Karchive < Formula
   depends_on "zstd"
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cmake", "-S", ".", "-B", "build", "-DBUILD_QCH=ON", *std_cmake_args
