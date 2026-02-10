429b8367d21a4da29eb0df0d50f9cb9df9288a46
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:46:02 -0500

    links: use `zlib-ng-compat` on Linux

--- Formula/l/links.rb
+++ Formula/l/links.rb
@@ -27,7 +27,10 @@ class Links < Formula
   depends_on "openssl@3"
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./configure", "--mandir=#{man}",
