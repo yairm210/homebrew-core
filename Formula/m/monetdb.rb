dfede4914fb41a90bd347b1f6df5091fdf72eadf
Michael Cho <michael@michaelcho.dev>
2026-02-10 18:38:29 -0500

    monetdb: use `zlib-ng-compat` on Linux

--- Formula/m/monetdb.rb
+++ Formula/m/monetdb.rb
@@ -34,7 +34,10 @@ class Monetdb < Formula
 
   uses_from_macos "python" => :build
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cmake", "-S", ".", "-B", "build",
