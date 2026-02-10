ecb0fad5d1e021b244b73488decf2c804dd959f6
Michael Cho <michael@michaelcho.dev>
2026-02-09 19:49:26 -0500

    frum: use `zlib-ng-compat` on Linux

--- Formula/f/frum.rb
+++ Formula/f/frum.rb
@@ -32,7 +32,9 @@ class Frum < Formula
 
   depends_on "rust" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
