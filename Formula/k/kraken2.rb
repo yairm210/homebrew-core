0d6964dcc51e54f5a960b31183e69a4a72bc6484
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:41:22 -0500

    kraken2: use `zlib-ng-compat` on Linux

--- Formula/k/kraken2.rb
+++ Formula/k/kraken2.rb
@@ -26,10 +26,13 @@ class Kraken2 < Formula
 
   uses_from_macos "perl"
   uses_from_macos "rsync"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "libomp"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   def install
