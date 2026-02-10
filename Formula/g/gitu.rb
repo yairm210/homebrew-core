30561dd10cecb1ea40cccd9fbddf98340d2b8414
Michael Cho <michael@michaelcho.dev>
2026-02-10 03:03:01 -0500

    gitu: use `zlib-ng-compat` on Linux

--- Formula/g/gitu.rb
+++ Formula/g/gitu.rb
@@ -17,7 +17,9 @@ class Gitu < Formula
 
   depends_on "rust" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
