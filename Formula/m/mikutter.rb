da05f7173230505c035b8c01ed66adbfaaec7ad4
Michael Cho <michael@michaelcho.dev>
2026-02-10 18:13:14 -0500

    mikutter: use `zlib-ng-compat` on Linux

--- Formula/m/mikutter.rb
+++ Formula/m/mikutter.rb
@@ -33,11 +33,14 @@ class Mikutter < Formula
   uses_from_macos "libffi"
   uses_from_macos "libxml2" # for nokogiri
   uses_from_macos "libxslt" # for nokogiri
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gettext"
     depends_on "terminal-notifier"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   # check `plugin/gtk3/Gemfile` for `gtk3` gem version
