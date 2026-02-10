ac48bd202b500f7907c29f4bb2cfb6a79db666a6
Michael Cho <michael@michaelcho.dev>
2026-02-09 19:31:35 -0500

    ecflow-ui: use `zlib-ng-compat` on Linux

--- Formula/e/ecflow-ui.rb
+++ Formula/e/ecflow-ui.rb
@@ -28,7 +28,10 @@ class EcflowUi < Formula
   depends_on "qtsvg"
 
   uses_from_macos "libxcrypt"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = %w[
