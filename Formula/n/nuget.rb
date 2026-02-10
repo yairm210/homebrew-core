c6673fcc9c07e611d590db8c098bd7328f1c657d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-10 23:37:16 +0000

    nuget 7.3.0

--- Formula/n/nuget.rb
+++ Formula/n/nuget.rb
@@ -1,8 +1,8 @@
 class Nuget < Formula
   desc "Package manager for Microsoft development platform including .NET"
   homepage "https://www.nuget.org/"
-  url "https://dist.nuget.org/win-x86-commandline/v7.0.1/nuget.exe"
-  sha256 "8ddc8cc04298fa08277efdca35373eb158f0c95f5bb1b15efcab2b62952028f6"
+  url "https://dist.nuget.org/win-x86-commandline/v7.3.0/nuget.exe"
+  sha256 "39b6309e76c832e4de2ac7f86da9a8afaa12bc5b4307ea335e9d69e2d6d1a94a"
   license "MIT"
 
   livecheck do
