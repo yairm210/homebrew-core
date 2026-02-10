7bb8976b0ee266575cbcefe50d33ab490f59d0b5
Michael Cho <michael@michaelcho.dev>
2026-02-10 03:22:13 -0500

    haproxy: use `zlib-ng-compat` on Linux

--- Formula/h/haproxy.rb
+++ Formula/h/haproxy.rb
@@ -23,7 +23,10 @@ class Haproxy < Formula
   depends_on "pcre2"
 
   uses_from_macos "libxcrypt"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = %w[
