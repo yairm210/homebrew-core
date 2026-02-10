1c3cc030f41bc4a519e6c7570311267ae13bf8b5
Michael Cho <michael@michaelcho.dev>
2026-02-10 09:51:05 -0500

    hopenpgp-tools: use `zlib-ng-compat` on Linux

--- Formula/h/hopenpgp-tools.rb
+++ Formula/h/hopenpgp-tools.rb
@@ -27,7 +27,10 @@ class HopenpgpTools < Formula
   depends_on "nettle"
 
   uses_from_macos "libffi"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   # TODO: Remove resource once new release ixset-typed release is available
   resource "ixset-typed" do
