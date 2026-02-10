88b5cec83cd7dcbd2515018fe2d3e61e92526cd6
Michael Cho <michael@michaelcho.dev>
2026-02-09 20:50:25 -0500

    gfold: use `zlib-ng-compat` on Linux

--- Formula/g/gfold.rb
+++ Formula/g/gfold.rb
@@ -24,7 +24,9 @@ class Gfold < Formula
   depends_on "rust" => :build
   depends_on "libgit2"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with "coreutils", because: "both install `gfold` binaries"
 
