85162b06664f95372b4a46a82ea735bc0cdaf8bd
Michael Cho <michael@michaelcho.dev>
2026-02-10 18:14:04 -0500

    minimal-racket: use `zlib-ng-compat` on Linux

--- Formula/m/minimal-racket.rb
+++ Formula/m/minimal-racket.rb
@@ -26,7 +26,10 @@ class MinimalRacket < Formula
   depends_on "openssl@3"
 
   uses_from_macos "libffi"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   # these two files are amended when (un)installing packages
   skip_clean "lib/racket/launchers.rktd", "lib/racket/mans.rktd"
