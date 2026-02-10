0446828fc3656e70148db909cd097b8e0b30bd8d
Michael Cho <michael@michaelcho.dev>
2026-02-10 18:47:07 -0500

    ncdc: use `zlib-ng-compat` on Linux

--- Formula/n/ncdc.rb
+++ Formula/n/ncdc.rb
@@ -35,10 +35,13 @@ class Ncdc < Formula
   depends_on "sqlite"
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   def install
