bede94eac3f99072632068187d04250fb4446bfd
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:41:08 -0500

    komac: use `zlib-ng-compat` on Linux

--- Formula/k/komac.rb
+++ Formula/k/komac.rb
@@ -18,10 +18,9 @@ class Komac < Formula
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "openssl@3"
+    depends_on "zlib-ng-compat"
   end
 
   def install
