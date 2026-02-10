54a3182ccdba8c9aead0757b01f41cac73c08f2d
Michael Cho <michael@michaelcho.dev>
2026-02-10 03:01:16 -0500

    git-graph: use `zlib-ng-compat` on Linux

--- Formula/g/git-graph.rb
+++ Formula/g/git-graph.rb
@@ -19,7 +19,9 @@ class GitGraph < Formula
 
   depends_on "rust" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
