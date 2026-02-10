c1f9f8f83d2d5b3e0e11a6ac05541c106f3483ee
Michael Cho <michael@michaelcho.dev>
2026-02-09 19:34:24 -0500

    erofs-utils: use `zlib-ng-compat` on Linux

--- Formula/e/erofs-utils.rb
+++ Formula/e/erofs-utils.rb
@@ -29,11 +29,10 @@ class ErofsUtils < Formula
   depends_on "lz4"
   depends_on "xz"
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "libfuse"
     depends_on "util-linux" # for libuuid
+    depends_on "zlib-ng-compat"
   end
 
   def install
