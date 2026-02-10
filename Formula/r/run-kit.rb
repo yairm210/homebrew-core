441420a315a9a63c4eaf17b9fa4665d0d4c77bb5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-10 07:51:11 +0000

    run-kit: add 0.6.1 bottle.

--- Formula/r/run-kit.rb
+++ Formula/r/run-kit.rb
@@ -6,6 +6,15 @@ class RunKit < Formula
   license "Apache-2.0"
   head "https://github.com/Esubaalew/run.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "59425600697d42df0cbe1fcc84ee8659886fd1daff82d1e09bedcad6eb0a888c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "472c93de740952aab9c27ee423f85b5fac0a9a3ea5dc48ab4a3efe4f9119a167"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e3dd508261528a010e99a5505c1bc259d63ac1e4bfc766548574dbde8e3a47f8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8c2d201cda7b3eccad0e5d30a7f1d55ad39b75ee25e522461a185561d6a316b9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "94ba7426f72a4f53ac293cf474d16a77130f34352b35cbeca9214c50bc9e44cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cbc305018fadd7dc6c785902373763f8e6ebffaf2e64b7ba41128b9abe322bc1"
+  end
+
   depends_on "rust" => :build
 
   conflicts_with "run", because: "both install a `run` binary"
