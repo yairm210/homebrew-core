9cef392fcc34a2937a023856deff89e0e5f60632
Michael Cho <michael@michaelcho.dev>
2026-02-10 18:51:00 -0500

    nmap: use `zlib-ng-compat` on Linux

--- Formula/n/nmap.rb
+++ Formula/n/nmap.rb
@@ -33,7 +33,10 @@ class Nmap < Formula
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
   uses_from_macos "libpcap"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with "cern-ndiff", "ndiff", because: "both install `ndiff` binaries"
   conflicts_with cask: "zenmap", because: "both install `nmap` binaries"
