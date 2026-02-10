e5bca132e4206b9e33fe054fbb1f71154aab1507
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:41:38 -0500

    kyoto-tycoon: use `zlib-ng-compat` on Linux

--- Formula/k/kyoto-tycoon.rb
+++ Formula/k/kyoto-tycoon.rb
@@ -33,7 +33,9 @@ class KyotoTycoon < Formula
   depends_on "pkgconf" => :build
   depends_on "kyoto-cabinet"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   # Build patch (submitted upstream)
   patch do
