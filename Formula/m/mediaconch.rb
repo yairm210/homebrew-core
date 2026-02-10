ba5f669b8e82d146b6b32d53573abb9188b290e0
Michael Cho <michael@michaelcho.dev>
2026-02-10 17:25:55 -0500

    mediaconch: use `zlib-ng-compat` on Linux

--- Formula/m/mediaconch.rb
+++ Formula/m/mediaconch.rb
@@ -30,7 +30,10 @@ class Mediaconch < Formula
   uses_from_macos "curl"
   uses_from_macos "libxml2"
   uses_from_macos "libxslt"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     cd "MediaConch/Project/GNU/CLI" do
