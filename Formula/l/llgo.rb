e65acc2a8959421797a36c12ee6f22d7f4347249
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:46:15 -0500

    llgo: use `zlib-ng-compat` on Linux

--- Formula/l/llgo.rb
+++ Formula/l/llgo.rb
@@ -29,10 +29,10 @@ class Llgo < Formula
   depends_on "pkgconf"
 
   uses_from_macos "libffi"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "libunwind"
+    depends_on "zlib-ng-compat"
   end
 
   def find_dep(name)
