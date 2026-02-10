fd68d9f6a86472ff7f9e8991d857721a253b6de7
Michael Cho <michael@michaelcho.dev>
2026-02-09 19:42:17 -0500

    ffms2: use `zlib-ng-compat` on Linux

--- Formula/f/ffms2.rb
+++ Formula/f/ffms2.rb
@@ -32,7 +32,9 @@ class Ffms2 < Formula
   depends_on "pkgconf" => :build
   depends_on "ffmpeg"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./autogen.sh", "--enable-avresample", *std_configure_args
