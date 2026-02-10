5ca5f7d917feaeffa8737c6000f5292b60eaeb15
Michael Cho <michael@michaelcho.dev>
2026-02-10 18:50:35 -0500

    ngircd: use `zlib-ng-compat` on Linux

--- Formula/n/ngircd.rb
+++ Formula/n/ngircd.rb
@@ -29,15 +29,17 @@ class Ngircd < Formula
   depends_on "libident"
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
-    system "./configure", *std_configure_args,
-                          "--disable-silent-rules",
+    system "./configure", "--disable-silent-rules",
                           "--sysconfdir=#{etc}",
                           "--enable-ipv6",
                           "--with-ident",
-                          "--with-openssl"
+                          "--with-openssl",
+                          *std_configure_args
     system "make", "install"
 
     if OS.mac?
