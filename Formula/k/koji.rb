c7bc87e5861e5ebe84b138eb940a70a03b3685ee
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:40:52 -0500

    koji: use `zlib-ng-compat` on Linux

--- Formula/k/koji.rb
+++ Formula/k/koji.rb
@@ -19,7 +19,9 @@ class Koji < Formula
   depends_on "rust" => :build
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV["OPENSSL_NO_VENDOR"] = "1"
