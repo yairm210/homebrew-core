5d24a08cf71177686426de0acfb82c5b9b1b061d
Michael Cho <michael@michaelcho.dev>
2026-02-09 19:37:40 -0500

    exult: use `zlib-ng-compat` on Linux

--- Formula/e/exult.rb
+++ Formula/e/exult.rb
@@ -30,10 +30,9 @@ class Exult < Formula
   depends_on "libvorbis"
   depends_on "sdl2"
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "alsa-lib"
+    depends_on "zlib-ng-compat"
   end
 
   def install
