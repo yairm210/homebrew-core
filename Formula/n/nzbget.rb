d9495fd156c988db15f2651d6bd917192e83ca87
Michael Cho <michael@michaelcho.dev>
2026-02-10 18:52:40 -0500

    nzbget: use `zlib-ng-compat` on Linux

--- Formula/n/nzbget.rb
+++ Formula/n/nzbget.rb
@@ -22,7 +22,10 @@ class Nzbget < Formula
 
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
