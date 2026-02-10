8f9247b9e24d8bf3cb729a637bf865143f4b0642
Michael Cho <michael@michaelcho.dev>
2026-02-10 03:23:58 -0500

    hck: use `zlib-ng-compat` on Linux

--- Formula/h/hck.rb
+++ Formula/h/hck.rb
@@ -23,7 +23,9 @@ class Hck < Formula
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
