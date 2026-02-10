5d634bfefd5a3f604a8b44cc3bc78c5b1d862fd3
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:40:18 -0500

    kcov: use `zlib-ng-compat` on Linux

--- Formula/k/kcov.rb
+++ Formula/k/kcov.rb
@@ -33,10 +33,10 @@ class Kcov < Formula
 
   uses_from_macos "python" => :build
   uses_from_macos "curl"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "elfutils"
+    depends_on "zlib-ng-compat"
   end
 
   def install
