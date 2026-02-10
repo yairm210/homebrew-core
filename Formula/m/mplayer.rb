6d6073bddeb4d32f088e6936f5f3ac0cabef8442
Michael Cho <michael@michaelcho.dev>
2026-02-10 18:41:58 -0500

    mplayer: use `zlib-ng-compat` on Linux

--- Formula/m/mplayer.rb
+++ Formula/m/mplayer.rb
@@ -41,7 +41,10 @@ class Mplayer < Formula
   uses_from_macos "bzip2"
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Work around build failure with newer Clang
