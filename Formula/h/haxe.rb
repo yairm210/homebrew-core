4c10e84f5a220f4c2410f317d5061f063311a73e
Michael Cho <michael@michaelcho.dev>
2026-02-10 03:23:42 -0500

    haxe: use `zlib-ng-compat` on Linux

--- Formula/h/haxe.rb
+++ Formula/h/haxe.rb
@@ -30,10 +30,9 @@ class Haxe < Formula
   depends_on "neko"
   depends_on "pcre2"
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "node" => :test
+    depends_on "zlib-ng-compat"
   end
 
   def install
