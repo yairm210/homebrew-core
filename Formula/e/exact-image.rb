9318dfaf913ac7e63f22fcac2e45567acb83f19b
Michael Cho <michael@michaelcho.dev>
2026-02-09 19:36:57 -0500

    exact-image: use `zlib-ng-compat` on Linux

--- Formula/e/exact-image.rb
+++ Formula/e/exact-image.rb
@@ -27,7 +27,10 @@ class ExactImage < Formula
   depends_on "libagg"
 
   uses_from_macos "expat"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV.cxx11
