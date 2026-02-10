dc9486aab34dcb777564a96df50ac3fe83507e07
Michael Cho <michael@michaelcho.dev>
2026-02-10 18:38:42 -0500

    monit: use `zlib-ng-compat` on Linux

--- Formula/m/monit.rb
+++ Formula/m/monit.rb
@@ -24,10 +24,10 @@ class Monit < Formula
   depends_on "openssl@3"
 
   uses_from_macos "libxcrypt"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "linux-pam"
+    depends_on "zlib-ng-compat"
   end
 
   def install
