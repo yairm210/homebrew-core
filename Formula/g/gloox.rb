ec160ad0f0071e1bf05018fb4b47d9aef4170cbe
Michael Cho <michael@michaelcho.dev>
2026-02-10 03:07:02 -0500

    gloox: use `zlib-ng-compat` on Linux

--- Formula/g/gloox.rb
+++ Formula/g/gloox.rb
@@ -29,7 +29,9 @@ class Gloox < Formula
   depends_on "libidn"
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   # Fix build issue with `{ 0 }`, build patch sent to upstream author
   patch :DATA
