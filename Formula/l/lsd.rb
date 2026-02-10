5b037d1c340c1e8015b41bd39d74b6810555cfa7
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:46:40 -0500

    lsd: use `zlib-ng-compat` on Linux

--- Formula/l/lsd.rb
+++ Formula/l/lsd.rb
@@ -18,7 +18,9 @@ class Lsd < Formula
   depends_on "pandoc" => :build
   depends_on "rust" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV["SHELL_COMPLETIONS_DIR"] = buildpath
