1064e725f72f3a8b6d604426c419be75844f96af
Michael Cho <michael@michaelcho.dev>
2026-02-09 19:38:08 -0500

    erlang: use `zlib-ng-compat` on Linux

--- Formula/e/erlang.rb
+++ Formula/e/erlang.rb
@@ -34,10 +34,10 @@ class Erlang < Formula
 
   uses_from_macos "libxslt" => :build
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "mesa-glu"
+    depends_on "zlib-ng-compat"
   end
 
   resource "html" do
