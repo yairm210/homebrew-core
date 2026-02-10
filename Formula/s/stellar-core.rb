a3fadf24ec713c0736998d7cb47c8edab927349f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-10 23:48:10 +0000

    stellar-core 25.1.1

--- Formula/s/stellar-core.rb
+++ Formula/s/stellar-core.rb
@@ -2,8 +2,8 @@ class StellarCore < Formula
   desc "Backbone of the Stellar (XLM) network"
   homepage "https://www.stellar.org/"
   url "https://github.com/stellar/stellar-core.git",
-      tag:      "v25.0.1",
-      revision: "ac5427a148203e8269294cf50866200cbe4ec1d3"
+      tag:      "v25.1.1",
+      revision: "a5393933f5e8c0d5be2d493b6624a6589bec155a"
   license "Apache-2.0"
   head "https://github.com/stellar/stellar-core.git", branch: "master"
 
@@ -65,11 +65,7 @@ class StellarCore < Formula
   test do
     test_categories = %w[
       accountsubentriescount
-      bucketlistconsistent
     ]
-    # Reduce tests on Intel macOS as runner is too slow and times out
-    test_categories << "topology" if !OS.mac? || !Hardware::CPU.intel?
-
     system bin/"stellar-core", "test", test_categories.map { |category| "[#{category}]" }.join(",")
   end
 end
