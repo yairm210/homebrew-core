4a84bfa6fa2a178f5273d06df71e81da9e4ab914
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:43:44 -0500

    lasso: use `zlib-ng-compat` on Linux

--- Formula/l/lasso.rb
+++ Formula/l/lasso.rb
@@ -30,10 +30,13 @@ class Lasso < Formula
 
   uses_from_macos "python" => :build
   uses_from_macos "libxslt"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   resource "six" do
