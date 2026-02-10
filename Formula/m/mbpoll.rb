a0083959d89fa68eafeb2f2352cfbfcbadd11db7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-10 11:25:51 +0000

    mbpoll: update 1.5.4 bottle.

--- Formula/m/mbpoll.rb
+++ Formula/m/mbpoll.rb
@@ -7,14 +7,12 @@ class Mbpoll < Formula
   head "https://github.com/epsilonrt/mbpoll.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "e9a8e9a61b04c4c4b18a0af7c86006612b40949b381fba4cd0342cb0cf073245"
-    sha256 cellar: :any,                 arm64_sequoia: "f2e1b7cc1e13adec4be27c0786d6d58d8452981695981111b9fb5a27b4209672"
-    sha256 cellar: :any,                 arm64_sonoma:  "38fed0cebc17a0f5b56f57c877ba9aedb7740f88ad49ec8b7ab20b75d46d451a"
-    sha256 cellar: :any,                 arm64_ventura: "11ad727f1304188c6973eeb682de71cf60275cda1cc2055564454dcfa03eb264"
-    sha256 cellar: :any,                 sonoma:        "448753a6babe4e91ec293111c781deca8456740af65c153c09b3a7f334d7ef8f"
-    sha256 cellar: :any,                 ventura:       "18d5251c8aa33916193ca6023807ba57a335f0abc5c2109fc7ff9921a3b69cd6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "13e6297dc717b70f1c464440d12ca264392b9b87bfee6a33e61f14d1579c80fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dffd6e9a6cbca3f5950ea1fd49d0db7ca5cf0d7a117faac986a1c2bbe43cff49"
+    sha256 cellar: :any,                 arm64_tahoe:   "10b1218b9b816f0931d893ce7cc81f3e1af5a0cd16933c7bb299122281a409ca"
+    sha256 cellar: :any,                 arm64_sequoia: "475ec1a589ff0454a78f47daa221cde466f658eb94b0317a9b136f990fd17f1e"
+    sha256 cellar: :any,                 arm64_sonoma:  "e9013365ba0618e900fd66bc89b9a44f0ada967d149edd8a5286ac2d8bc75b6d"
+    sha256 cellar: :any,                 sonoma:        "be89f71b83662d272b2fe11a1cda6a2d666839014aea1f0cd97b09e006b72f37"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "325eeaf4fd666c7208fc4eb2c0094c9842331144663ef03200ebd8d8f783511c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bdca044ed55118e25a83d39bc6e463f614fffc7f2aff84d484855eaa8714b36b"
   end
 
   depends_on "cmake" => :build
