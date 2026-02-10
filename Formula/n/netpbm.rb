8bdb1cce7fdc8de9361074ce986c39a843d0db94
Michael Cho <michael@michaelcho.dev>
2026-02-10 18:49:00 -0500

    netpbm: use `zlib-ng-compat` on Linux

--- Formula/n/netpbm.rb
+++ Formula/n/netpbm.rb
@@ -35,7 +35,10 @@ class Netpbm < Formula
   uses_from_macos "flex" => :build
   uses_from_macos "python" => :build
   uses_from_macos "libxml2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with "jbigkit", because: "both install `pbm.5` and `pgm.5` files"
 
