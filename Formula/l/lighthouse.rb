237601a7685e58e1d15889cc284aaa6bfdd8f7ae
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:45:29 -0500

    lighthouse: use `zlib-ng-compat` on Linux

--- Formula/l/lighthouse.rb
+++ Formula/l/lighthouse.rb
@@ -24,11 +24,11 @@ class Lighthouse < Formula
   depends_on "rust" => :build
 
   uses_from_macos "llvm" => :build
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "pkgconf" => :build
     depends_on "openssl@3"
+    depends_on "zlib-ng-compat"
   end
 
   def install
