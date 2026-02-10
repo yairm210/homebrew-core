8f58f47c08efa1421c81cfbeb85ab5116ff80d05
Michael Cho <michael@michaelcho.dev>
2026-02-10 03:08:35 -0500

    gnu-smalltalk: use `zlib-ng-compat` on Linux

--- Formula/g/gnu-smalltalk.rb
+++ Formula/g/gnu-smalltalk.rb
@@ -53,7 +53,10 @@ class GnuSmalltalk < Formula
   uses_from_macos "zip" => :build
   uses_from_macos "expat"
   uses_from_macos "libffi"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Fix compile with newer Clang
