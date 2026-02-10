906c7b3b042853261763b10b5a75fce2318dd91f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-10 18:59:39 +0000

    scalingo: update 1.43.1 bottle.

--- Formula/s/scalingo.rb
+++ Formula/s/scalingo.rb
@@ -6,12 +6,12 @@ class Scalingo < Formula
   license "BSD-4-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "aa3eb1a9d81b345d46c2a8bf8c5f246e861fda665a528286ad0aa19c4b70bb67"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aa3eb1a9d81b345d46c2a8bf8c5f246e861fda665a528286ad0aa19c4b70bb67"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aa3eb1a9d81b345d46c2a8bf8c5f246e861fda665a528286ad0aa19c4b70bb67"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6492921cd6b7f6a8138114f2e6d3bba3e0a9d33e94516a5ce6ed042112143fd7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e9ba7758593b73b8343d3a102c6c41c1d8704d543a19cf62353676b01bc5fec3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eebcae820e63cd5b14d5a5aeeb80119a7ed169c6b4cf055c2f7a13123bf6aa63"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8fb4a52e2d8dcb260847f551f79202eb4b59a7581997a18c2731bb44e54705a8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8fb4a52e2d8dcb260847f551f79202eb4b59a7581997a18c2731bb44e54705a8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8fb4a52e2d8dcb260847f551f79202eb4b59a7581997a18c2731bb44e54705a8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "070490520d67ba213e8d0e6ed57e54f420e075eba61e808f2178a1bafa6c0d39"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "599e44d66d2629aca98425cbd49f1fbdd0521695e83d9d4fadf26324f9e47f9d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4d4f0251f1893cb6c636d53ed206b1851f3d338935402015110be85959768c68"
   end
 
   depends_on "go" => :build
