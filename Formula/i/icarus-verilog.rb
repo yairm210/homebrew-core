297141e4a4668b4219891ede4c9eac46a45d8270
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:10:27 -0500

    icarus-verilog: use `zlib-ng-compat` on Linux

--- Formula/i/icarus-verilog.rb
+++ Formula/i/icarus-verilog.rb
@@ -36,10 +36,10 @@ class IcarusVerilog < Formula
   uses_from_macos "flex" => :build
   uses_from_macos "gperf" => :build
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "readline"
+    depends_on "zlib-ng-compat"
   end
 
   def install
