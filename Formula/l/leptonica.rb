ec5776690297399948ee98024964935b97032f4b
Michael Cho <michael@michaelcho.dev>
2026-02-10 10:44:34 -0500

    leptonica: use `zlib-ng-compat` on Linux

--- Formula/l/leptonica.rb
+++ Formula/l/leptonica.rb
@@ -27,7 +27,9 @@ class Leptonica < Formula
   depends_on "openjpeg"
   depends_on "webp"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./configure", "--with-libwebp", "--with-libopenjpeg", *std_configure_args
