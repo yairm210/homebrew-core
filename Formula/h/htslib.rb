3384bc7fc01cedf01051be79348155bfada667c6
Michael Cho <michael@michaelcho.dev>
2026-02-10 09:52:17 -0500

    htslib: use `zlib-ng-compat` on Linux

--- Formula/h/htslib.rb
+++ Formula/h/htslib.rb
@@ -24,10 +24,10 @@ class Htslib < Formula
 
   uses_from_macos "bzip2"
   uses_from_macos "curl"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "openssl@3"
+    depends_on "zlib-ng-compat"
   end
 
   def install
