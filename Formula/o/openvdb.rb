25fa70a3dffd0d73b954d319362f1f4dadf5ef7b
Michael Cho <michael@michaelcho.dev>
2026-02-11 10:48:24 -0500

    openvdb: use `zlib-ng-compat` on Linux

--- Formula/o/openvdb.rb
+++ Formula/o/openvdb.rb
@@ -25,7 +25,9 @@ class Openvdb < Formula
   depends_on "openexr"
   depends_on "tbb"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = [
