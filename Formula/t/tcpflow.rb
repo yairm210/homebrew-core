1e1bdec4ac75a8c2cbc09498c1e20430801087ef
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:13:53 -0500

    tcpflow: use `zlib-ng-compat` on Linux

--- Formula/t/tcpflow.rb
+++ Formula/t/tcpflow.rb
@@ -46,13 +46,16 @@ class Tcpflow < Formula
 
   uses_from_macos "bzip2"
   uses_from_macos "libpcap"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "bash", "./bootstrap.sh" if build.head?
-    system "./configure", *std_configure_args,
-                          "--disable-silent-rules",
-                          "--mandir=#{man}"
+    system "./configure", "--disable-silent-rules",
+                          "--mandir=#{man}",
+                          *std_configure_args
     system "make", "install"
   end
 
