6168e2115248bbb805afe4298af3d53129236f2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-11 14:14:48 +0000

    bareos-client 25.0.2

--- Formula/b/bareos-client.rb
+++ Formula/b/bareos-client.rb
@@ -1,8 +1,8 @@
 class BareosClient < Formula
   desc "Client for Bareos (Backup Archiving REcovery Open Sourced)"
   homepage "https://www.bareos.com/"
-  url "https://github.com/bareos/bareos/archive/refs/tags/Release/25.0.1.tar.gz"
-  sha256 "b44c481ddf1cfdf603e1b1fc1c4e4a120d5a0e9f34f781c90b1c487fc4716f27"
+  url "https://github.com/bareos/bareos/archive/refs/tags/Release/25.0.2.tar.gz"
+  sha256 "39c300ea4886c5c949d61034eff5990e046a130ac4d0e295a92745acd30516a9"
   license "AGPL-3.0-only"
 
   livecheck do
