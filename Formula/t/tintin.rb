0637bd0b4af987e33529538bc5464a49d53cfcd1
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:17:43 -0500

    tintin: use `zlib-ng-compat` on Linux

--- Formula/t/tintin.rb
+++ Formula/t/tintin.rb
@@ -22,7 +22,9 @@ class Tintin < Formula
   depends_on "gnutls"
   depends_on "pcre2"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # find Homebrew's libpcre
