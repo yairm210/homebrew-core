1ae492cb3bf42eea0ebbe911a60be6693cd680da
Michael Cho <michael@michaelcho.dev>
2026-02-11 10:52:46 -0500

    pandoc: use `zlib-ng-compat` on Linux

--- Formula/p/pandoc.rb
+++ Formula/p/pandoc.rb
@@ -22,7 +22,10 @@ class Pandoc < Formula
 
   uses_from_macos "unzip" => :build # for cabal install
   uses_from_macos "libffi"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Workaround to build aeson with GHC 9.14, https://github.com/haskell/aeson/issues/1155
