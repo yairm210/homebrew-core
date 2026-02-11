bac526ebdca041e9344e52ef3d8f486b53932c35
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:16:25 -0500

    thors-anvil: use `zlib-ng-compat` on Linux

--- Formula/t/thors-anvil.rb
+++ Formula/t/thors-anvil.rb
@@ -23,7 +23,10 @@ class ThorsAnvil < Formula
   depends_on "openssl@3"
   depends_on "snappy"
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV["COV"] = "gcov"
