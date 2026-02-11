60415d11dd548f005a1077122f15af805856a957
Michael Cho <michael@michaelcho.dev>
2026-02-11 11:03:14 -0500

    ppsspp: use `zlib-ng-compat` on Linux

--- Formula/p/ppsspp.rb
+++ Formula/p/ppsspp.rb
@@ -27,7 +27,6 @@ class Ppsspp < Formula
   depends_on "zstd"
 
   uses_from_macos "python" => :build
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "molten-vk"
@@ -36,6 +35,7 @@ class Ppsspp < Formula
   on_linux do
     depends_on "glew"
     depends_on "mesa"
+    depends_on "zlib-ng-compat"
   end
 
   on_intel do
