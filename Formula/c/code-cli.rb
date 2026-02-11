c7b6e90445247aefb1de5d0c13f01de66bc55f22
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-11 05:27:33 +0000

    code-cli 1.109.2

--- Formula/c/code-cli.rb
+++ Formula/c/code-cli.rb
@@ -1,8 +1,8 @@
 class CodeCli < Formula
   desc "Command-line interface built-in Visual Studio Code"
   homepage "https://github.com/microsoft/vscode"
-  url "https://github.com/microsoft/vscode/archive/refs/tags/1.109.1.tar.gz"
-  sha256 "fd473ee592b541f4f953e9accb42e71368ca1853bd965ba98a94c3f94e11a7c3"
+  url "https://github.com/microsoft/vscode/archive/refs/tags/1.109.2.tar.gz"
+  sha256 "73e42ad18c090123a4e535aa79e3f66d7590bff50bc8cfdfa4a7a04af4746039"
   license "MIT"
   head "https://github.com/microsoft/vscode.git", branch: "main"
 
