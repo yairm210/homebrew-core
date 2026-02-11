664e556d369a361948adb1c1233121750dd2d6f0
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:45:20 -0500

    sofia-sip: use `zlib-ng-compat` on Linux

--- Formula/s/sofia-sip.rb
+++ Formula/s/sofia-sip.rb
@@ -28,10 +28,12 @@ class SofiaSip < Formula
   depends_on "glib"
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
-
   on_macos do
     depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   def install
