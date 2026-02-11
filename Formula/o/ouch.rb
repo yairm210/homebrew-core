d8d8376387e2b6a663f013e16e758dec603d372c
Michael Cho <michael@michaelcho.dev>
2026-02-11 10:50:41 -0500

    ouch: use `zlib-ng-compat` on Linux

--- Formula/o/ouch.rb
+++ Formula/o/ouch.rb
@@ -27,12 +27,12 @@ class Ouch < Formula
 
   depends_on "rust" => :build
 
+  uses_from_macos "llvm" => :build
   uses_from_macos "bzip2"
   uses_from_macos "xz"
-  uses_from_macos "zlib"
 
   on_linux do
-    depends_on "llvm" => :build
+    depends_on "zlib-ng-compat"
   end
 
   def install
