5c65fbc179f42dd876e86350ec515df8c28393f4
Michael Cho <michael@michaelcho.dev>
2026-02-11 12:02:59 -0500

    readsb: use `zlib-ng-compat` on Linux

--- Formula/r/readsb.rb
+++ Formula/r/readsb.rb
@@ -19,10 +19,13 @@ class Readsb < Formula
   depends_on "zstd"
 
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "libusb"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   def install
