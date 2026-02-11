1c9f32dcc1372313a443db9fa1a768c0ebbe1ba5
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:32:03 -0500

    sbom-tool: use `zlib-ng-compat` on Linux

--- Formula/s/sbom-tool.rb
+++ Formula/s/sbom-tool.rb
@@ -24,7 +24,9 @@ class SbomTool < Formula
 
   depends_on "dotnet@8"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV["DOTNET_CLI_TELEMETRY_OPTOUT"] = "true"
