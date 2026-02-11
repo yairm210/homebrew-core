4a543704724683bccfd54a94badef9469d662c91
Michael Cho <michael@michaelcho.dev>
2026-02-11 10:58:14 -0500

    pkl: use `zlib-ng-compat` on Linux

--- Formula/p/pkl.rb
+++ Formula/p/pkl.rb
@@ -17,7 +17,9 @@ class Pkl < Formula
   depends_on "gradle" => :build
   depends_on "openjdk@21" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV["JAVA_HOME"] = Formula["openjdk@21"].opt_prefix
