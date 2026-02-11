b3916fc48e7077669e9e77e0980d1a435d5f4110
Michael Cho <michael@michaelcho.dev>
2026-02-11 12:03:27 -0500

    recc: use `zlib-ng-compat` on Linux

--- Formula/r/recc.rb
+++ Formula/r/recc.rb
@@ -31,7 +31,6 @@ class Recc < Formula
   depends_on "re2"
 
   uses_from_macos "curl"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gflags"
@@ -40,6 +39,7 @@ class Recc < Formula
   on_linux do
     depends_on "pkgconf" => :build
     depends_on "util-linux"
+    depends_on "zlib-ng-compat"
   end
 
   def install
