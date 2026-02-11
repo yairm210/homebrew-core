bc6a2b7defb9373c7075a8ff6fe76cfd84ff1f3c
Michael Cho <michael@michaelcho.dev>
2026-02-11 10:44:07 -0500

    open-simh: use `zlib-ng-compat` on Linux

--- Formula/o/open-simh.rb
+++ Formula/o/open-simh.rb
@@ -28,7 +28,10 @@ class OpenSimh < Formula
 
   uses_from_macos "libedit"
   uses_from_macos "libpcap"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with "nova-fairwinds", because: "both install `nova` binaries"
   conflicts_with "sigma-cli", because: "both install `sigma` binaries"
