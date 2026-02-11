cf84bedfc08b8818cb3cf1ab4444e8093364387a
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:16:38 -0500

    tiff2png: use `zlib-ng-compat` on Linux

--- Formula/t/tiff2png.rb
+++ Formula/t/tiff2png.rb
@@ -27,7 +27,9 @@ class Tiff2png < Formula
   depends_on "libpng"
   depends_on "libtiff"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     bin.mkpath
