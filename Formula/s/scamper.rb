3d26c4ac044b0b17c78c519945a8ddd46d73ea1d
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:32:31 -0500

    scamper: use `zlib-ng-compat` on Linux

--- Formula/s/scamper.rb
+++ Formula/s/scamper.rb
@@ -23,7 +23,9 @@ class Scamper < Formula
   depends_on "openssl@3"
   depends_on "xz" # for LZMA
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./configure", *std_configure_args
