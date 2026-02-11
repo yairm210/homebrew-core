4e23ca27f09344cedad85fe4d265b9cf2e4e117a
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:18:58 -0500

    tor: use `zlib-ng-compat` on Linux

--- Formula/t/tor.rb
+++ Formula/t/tor.rb
@@ -33,7 +33,9 @@ class Tor < Formula
   depends_on "libscrypt"
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = %W[
