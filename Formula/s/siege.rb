0f04f60abd388eaaad69433c0deb2982b7c9ac83
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:43:18 -0500

    siege: use `zlib-ng-compat` on Linux

--- Formula/s/siege.rb
+++ Formula/s/siege.rb
@@ -23,7 +23,9 @@ class Siege < Formula
 
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # workaround for newer clang
