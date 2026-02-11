01b27a369aa9968d41a16cb162258906c6d66672
Michael Cho <michael@michaelcho.dev>
2026-02-11 13:40:18 -0500

    vmdktool: use `zlib-ng-compat` on Linux

--- Formula/v/vmdktool.rb
+++ Formula/v/vmdktool.rb
@@ -30,7 +30,9 @@ class Vmdktool < Formula
 
   depends_on "groff" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Fixes "call to undeclared library function 'tolower' with type 'int (int)'".
