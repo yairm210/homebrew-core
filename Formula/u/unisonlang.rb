4c8bd3673b7baad79be5f3d0c0c9d580abde9fdb
Michael Cho <michael@michaelcho.dev>
2026-02-11 13:44:12 -0500

    unisonlang: use `zlib-ng-compat` on Linux

--- Formula/u/unisonlang.rb
+++ Formula/u/unisonlang.rb
@@ -47,7 +47,10 @@ class Unisonlang < Formula
 
   uses_from_macos "python" => :build
   uses_from_macos "xz" => :build
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     odie "local-ui resource needs to be updated" if build.stable? && version != resource("local-ui").version
