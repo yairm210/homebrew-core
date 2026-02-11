09e9c84e20ac00407ea7f003398ace245dba44c1
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:19:12 -0500

    transmission-cli: use `zlib-ng-compat` on Linux

--- Formula/t/transmission-cli.rb
+++ Formula/t/transmission-cli.rb
@@ -27,10 +27,10 @@ class TransmissionCli < Formula
 
   uses_from_macos "python" => :build
   uses_from_macos "curl"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "openssl@3" # Uses CommonCrypto on macOS
+    depends_on "zlib-ng-compat"
   end
 
   def install
