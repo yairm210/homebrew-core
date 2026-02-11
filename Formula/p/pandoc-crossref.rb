5cf592da0dd102ed493be726a2b41c5363c8fca6
Michael Cho <michael@michaelcho.dev>
2026-02-11 10:52:22 -0500

    pandoc-crossref: use `zlib-ng-compat` on Linux

--- Formula/p/pandoc-crossref.rb
+++ Formula/p/pandoc-crossref.rb
@@ -22,7 +22,10 @@ class PandocCrossref < Formula
 
   uses_from_macos "unzip" => :build
   uses_from_macos "libffi"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     rm("cabal.project.freeze")
