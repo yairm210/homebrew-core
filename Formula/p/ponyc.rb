2453c6138652809aead5fbf45aafc8dd7a298b27
Michael Cho <michael@michaelcho.dev>
2026-02-11 11:02:36 -0500

    ponyc: use `zlib-ng-compat` on Linux

--- Formula/p/ponyc.rb
+++ Formula/p/ponyc.rb
@@ -18,7 +18,10 @@ class Ponyc < Formula
   depends_on "cmake" => :build
 
   uses_from_macos "python" => :build
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     if OS.linux?
