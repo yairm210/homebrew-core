0d97624b932bd1f9055fcecc575e60c39263fa1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-11 11:18:10 +0000

    ghidra 12.0.3

--- Formula/g/ghidra.rb
+++ Formula/g/ghidra.rb
@@ -1,8 +1,8 @@
 class Ghidra < Formula
   desc "Multi-platform software reverse engineering framework"
   homepage "https://github.com/NationalSecurityAgency/ghidra"
-  url "https://github.com/NationalSecurityAgency/ghidra/archive/refs/tags/Ghidra_12.0.2_build.tar.gz"
-  sha256 "1d803a87debac6908bc8a638baecb1de53ec9e0e6ac1fecc7a690492cc8b13ed"
+  url "https://github.com/NationalSecurityAgency/ghidra/archive/refs/tags/Ghidra_12.0.3_build.tar.gz"
+  sha256 "39e5d160fafa544c8b1858e2df869728d18aa3c9c5490f47a2d0db776f3b4d4d"
   license "Apache-2.0"
 
   livecheck do
