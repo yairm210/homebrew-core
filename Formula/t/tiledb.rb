495c8f0a259c6006efaf7a519faa109a826f5a06
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:17:03 -0500

    tiledb: use `zlib-ng-compat` on Linux

--- Formula/t/tiledb.rb
+++ Formula/t/tiledb.rb
@@ -32,7 +32,10 @@ class Tiledb < Formula
   depends_on "zstd"
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = %w[
