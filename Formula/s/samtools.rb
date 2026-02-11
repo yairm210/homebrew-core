58fb8ea2af9adf8f94f50deb39ceb8e42365cc57
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:31:28 -0500

    samtools: use `zlib-ng-compat` on Linux

--- Formula/s/samtools.rb
+++ Formula/s/samtools.rb
@@ -17,7 +17,10 @@ class Samtools < Formula
   depends_on "htslib"
 
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}",
