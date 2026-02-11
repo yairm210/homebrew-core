40e783b2ffec9408014933e6db40d693acc96049
Michael Cho <michael@michaelcho.dev>
2026-02-11 10:49:51 -0500

    osrm-backend: use `zlib-ng-compat` on Linux

--- Formula/o/osrm-backend.rb
+++ Formula/o/osrm-backend.rb
@@ -32,7 +32,10 @@ class OsrmBackend < Formula
 
   uses_from_macos "bzip2"
   uses_from_macos "expat"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with "flatbuffers", because: "both install flatbuffers headers"
 
