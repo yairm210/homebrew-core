93495a458d322d19fa98b99becb2ac287bc546d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-11 14:55:19 +0000

    redocly-cli 2.18.0

--- Formula/r/redocly-cli.rb
+++ Formula/r/redocly-cli.rb
@@ -1,8 +1,8 @@
 class RedoclyCli < Formula
   desc "Your all-in-one OpenAPI utility"
   homepage "https://redocly.com/docs/cli"
-  url "https://registry.npmjs.org/@redocly/cli/-/cli-2.17.0.tgz"
-  sha256 "d8036faca5c4140c03be2d39920e3f65345c4d9169e76568caa341a9273a0d6e"
+  url "https://registry.npmjs.org/@redocly/cli/-/cli-2.18.0.tgz"
+  sha256 "0453bf018ba7e82f0c503bc777a0474f350712f7d1031459401a2da3149cbe90"
   license "MIT"
 
   bottle do
