878cd34c81df7d60259470fe1caa48ea2d11045e
Michael Cho <michael@michaelcho.dev>
2026-02-11 11:03:25 -0500

    privoxy: use `zlib-ng-compat` on Linux

--- Formula/p/privoxy.rb
+++ Formula/p/privoxy.rb
@@ -24,7 +24,9 @@ class Privoxy < Formula
   depends_on "libtool" => :build
   depends_on "pcre2"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "autoreconf", "--force", "--install", "--verbose"
