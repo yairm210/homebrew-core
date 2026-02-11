0044adbf42c9c9a0614a6e2b5e362431899476c0
Michael Cho <michael@michaelcho.dev>
2026-02-11 10:53:26 -0500

    passenger: use `zlib-ng-compat` on Linux

--- Formula/p/passenger.rb
+++ Formula/p/passenger.rb
@@ -27,7 +27,10 @@ class Passenger < Formula
   uses_from_macos "curl"
   uses_from_macos "libxcrypt"
   uses_from_macos "ruby"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     if OS.mac? && MacOS::CLT.installed?
