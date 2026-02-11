dc7997c73dc1af5b9831040ddde19fbd1cf0f048
Michael Cho <michael@michaelcho.dev>
2026-02-11 17:18:12 -0500

    tmux-sessionizer: use `zlib-ng-compat` on Linux

--- Formula/t/tmux-sessionizer.rb
+++ Formula/t/tmux-sessionizer.rb
@@ -18,7 +18,9 @@ class TmuxSessionizer < Formula
 
   depends_on "rust" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
