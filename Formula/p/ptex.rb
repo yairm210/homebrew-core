cb1db5376a497e1430f65d327f5e7394a72d821d
Michael Cho <michael@michaelcho.dev>
2026-02-11 11:12:02 -0500

    ptex: use `zlib-ng-compat` on Linux

--- Formula/p/ptex.rb
+++ Formula/p/ptex.rb
@@ -23,7 +23,9 @@ class Ptex < Formula
   depends_on "pkgconf" => :build
   depends_on "libdeflate"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cmake", "-S", ".", "-B", "build", "-DCMAKE_CXX_STANDARD=17", *std_cmake_args
