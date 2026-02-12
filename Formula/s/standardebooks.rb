e2aa9e8cecf573cae811934861954dae605e2fd1
Michael Cho <michael@michaelcho.dev>
2026-02-12 16:26:30 -0500

    standardebooks: use `zlib-ng-compat` on Linux

--- Formula/s/standardebooks.rb
+++ Formula/s/standardebooks.rb
@@ -28,7 +28,10 @@ class Standardebooks < Formula
 
   uses_from_macos "libxml2"
   uses_from_macos "libxslt"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   pypi_packages exclude_packages: %w[certifi cffi pillow pycairo]
 
