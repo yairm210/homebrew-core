2e9d779fc294f9af11e8de01d2c7e8d68274a880
Michael Cho <michael@michaelcho.dev>
2026-02-12 16:20:25 -0500

    solargraph: use `zlib-ng-compat` on Linux

--- Formula/s/solargraph.rb
+++ Formula/s/solargraph.rb
@@ -19,7 +19,9 @@ class Solargraph < Formula
   depends_on "ruby"
   depends_on "xz"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV["GEM_HOME"] = libexec
