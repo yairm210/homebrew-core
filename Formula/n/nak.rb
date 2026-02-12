056b95cdcd7a2b90b324f6642a00d53119d60625
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-12 18:09:44 +0000

    nak: update 0.18.4 bottle.

--- Formula/n/nak.rb
+++ Formula/n/nak.rb
@@ -12,12 +12,12 @@ class Nak < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d9c0ead931f445aabdaddb6d6e87188ec6d48122ef6c455406390444c0644512"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d9c0ead931f445aabdaddb6d6e87188ec6d48122ef6c455406390444c0644512"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d9c0ead931f445aabdaddb6d6e87188ec6d48122ef6c455406390444c0644512"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0e4408b6f940c355dfcf6ddc8b4d13c298a964ed60213572c3d4198e5ccd8c6d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cffc076930050a739eef5b6c0c7b96ce46e09fb9af303070b51c2b13336e76f8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0e1a2e82a461c5390b69ff97af0188ce9853541878673d9ec6f86ac28d5241fd"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3e557be1204fb3d132bd6275f38862b097f7ee00faef62c2b3d748191b84a68c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3e557be1204fb3d132bd6275f38862b097f7ee00faef62c2b3d748191b84a68c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3e557be1204fb3d132bd6275f38862b097f7ee00faef62c2b3d748191b84a68c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e3c52db3cd04f00fca39a40f62c21104f233ee218f4d86779679b2a780f8570b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7621bd793daca7d188c42a1f186708822483a31e2484fa21cab085345249f1f0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6c509b9bb3c0dc3d4fb02da70fdc43b206bdfce8443fe36a4d7a77d7a13578e1"
   end
 
   depends_on "go" => :build
