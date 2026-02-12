e6c091047f0e7294a0d7abe6449f9b68b970de73
François Proulx <francois@boostsecurity.io>
2026-02-12 09:16:09 -0500

    bagel 0.2.0 (new formula)
    Co-authored-by: Sean Molenaar <sean@seanmolenaar.eu>

---
+++ Formula/b/bagel.rb
@@ -0,0 +1,44 @@
+class Bagel < Formula
+  desc "CLI to audit posture and evaluate compromise blast radius"
+  homepage "https://boostsecurityio.github.io/bagel/"
+  url "https://github.com/boostsecurityio/bagel/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "b7317200cfc6d7556c6c1fbeb244c8397f68fc2998348bf07dbdc4f9bed46506"
+  license "GPL-3.0-or-later"
+  head "https://github.com/boostsecurityio/bagel.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.Version=#{version}
+      -X main.Commit=#{tap.user}
+      -X main.Date=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/bagel"
+
+    generate_completions_from_executable(bin/"bagel", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bagel version")
+
+    (testpath/"bagel.yaml").write <<~YAML
+      version: 1
+      file_index:
+        base_dirs: ["#{testpath}"]
+    YAML
+
+    (testpath/".aws").mkpath
+    (testpath/".aws/credentials").write <<~INI
+      [default]
+      aws_access_key_id = AKIAIOSFODNN7EXAMPLE
+      aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+    INI
+
+    # Removed the explicit '0' to satisfy brew audit
+    output = shell_output("#{bin}/bagel scan --config #{testpath}/bagel.yaml")
+    assert_match "AWS", output
+  end
+end
