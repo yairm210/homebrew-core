df0cf3fe65841ac8ce824b62fac595ae6e69aee5
Michael Cho <michael@michaelcho.dev>
2026-02-12 16:22:41 -0500

    spr: use `zlib-ng-compat` on Linux

--- Formula/s/spr.rb
+++ Formula/s/spr.rb
@@ -20,10 +20,9 @@ class Spr < Formula
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "openssl@3"
+    depends_on "zlib-ng-compat"
   end
 
   def install
