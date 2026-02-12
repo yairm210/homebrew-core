5ce00559b1552fdcea40da2d6830e86e9d02f66c
Anton Melnikov <antonsm21@gmail.com>
2026-02-12 16:57:57 +0000

    jsonschema2pojo: add 1.3.2 bottle.

--- Formula/j/jsonschema2pojo.rb
+++ Formula/j/jsonschema2pojo.rb
@@ -11,6 +11,15 @@ class Jsonschema2pojo < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "edb99d1265ef939c0501a99a4a0a957fdad8bd6be1e113af05e91a3a92c55a14"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "edb99d1265ef939c0501a99a4a0a957fdad8bd6be1e113af05e91a3a92c55a14"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "edb99d1265ef939c0501a99a4a0a957fdad8bd6be1e113af05e91a3a92c55a14"
+    sha256 cellar: :any_skip_relocation, sonoma:        "edb99d1265ef939c0501a99a4a0a957fdad8bd6be1e113af05e91a3a92c55a14"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d59cbeb207969062c0e70ad795e40caa8c0cd218dc7cec8737d31b5a66ddda38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d59cbeb207969062c0e70ad795e40caa8c0cd218dc7cec8737d31b5a66ddda38"
+  end
+
   depends_on "openjdk"
 
   def install
