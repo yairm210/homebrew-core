fa10f40c156534253a55ab932beb6c626c2d77c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-12 17:50:52 +0000

    sec 2.9.4

--- Formula/s/sec.rb
+++ Formula/s/sec.rb
@@ -1,8 +1,8 @@
 class Sec < Formula
   desc "Event correlation tool for event processing of various kinds"
   homepage "https://simple-evcorr.sourceforge.net/"
-  url "https://github.com/simple-evcorr/sec/releases/download/2.9.3/sec-2.9.3.tar.gz"
-  sha256 "280f5b94eebbf7efbf5a7d7e417beae75415a7dc5103a2d0fdb3008568fb9f30"
+  url "https://github.com/simple-evcorr/sec/releases/download/2.9.4/sec-2.9.4.tar.gz"
+  sha256 "77fd945980d15ca07f94a9cad6484677f5d3fe8ded5da12ec2c0c444ae7b0994"
   license "GPL-2.0-or-later"
 
   bottle do
