02b2a62a65e4dd259ea9481a9c776c7645dce5af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-12 15:48:31 +0000

    tuckr: add 0.12.0 bottle.

--- Formula/t/tuckr.rb
+++ Formula/t/tuckr.rb
@@ -6,6 +6,15 @@ class Tuckr < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/RaphGL/Tuckr.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0ba342c8c41b3672c46158a076ecd403e59bb1fbee8707a068075682ea6cecdf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e6f27026639472a51c3e6533cc6990ee5315f0682e3f43bf410003ae884799fb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "43f756de8a7db26e34c797177757952def7f9123532c14b11130c9933559e145"
+    sha256 cellar: :any_skip_relocation, sonoma:        "309d59bb8359e672fdeecc5dd441349a151a7f33dbcefaa2dc9b7129bdb2bd58"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f297c0b32effc6a51133c8d6f17fcf98f2af5ff070b9ed284459e5606e01a1c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "12abef9b77e858758c9fc3d630571013cb891cbe7ad17faf8df364adbf9f0fd2"
+  end
+
   depends_on "rust" => :build
 
   def install
