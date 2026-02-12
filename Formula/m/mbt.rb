3213afbe8144bc5fb47bdd85338038bf2df10f0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-12 21:23:31 +0000

    mbt: update 1.2.43 bottle.

--- Formula/m/mbt.rb
+++ Formula/m/mbt.rb
@@ -7,12 +7,12 @@ class Mbt < Formula
   head "https://github.com/SAP/cloud-mta-build-tool.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "19f997ad2d500ac0250aad8215f714d2ffde0d7c2c6132760498ce034e724d7e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "19f997ad2d500ac0250aad8215f714d2ffde0d7c2c6132760498ce034e724d7e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "19f997ad2d500ac0250aad8215f714d2ffde0d7c2c6132760498ce034e724d7e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "496fd538562837f1d8d751a53770f9ee27d990633b53eecf9fb4c94897f1ac12"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b9d17bab82cfba2920244b822c14c227a7e1911bea60c0dfdb05fffd06360f53"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a5a1ae103f77ba25969984de42a43a195be11d3ccd64de5b6baf94118533706"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "311d8eb5123a3aea5fbdf32976abfa895b646419d2eca32684b744aab5559c81"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "311d8eb5123a3aea5fbdf32976abfa895b646419d2eca32684b744aab5559c81"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "311d8eb5123a3aea5fbdf32976abfa895b646419d2eca32684b744aab5559c81"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2fcfad38f3da1b077e469a44b13a93c7851f20c06e8ec5fc9ee77cb3b4f949b7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d6dbf3a587e8d21a6777f0798e523950c7285208927a147da9cbc7a0615ffc8f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc414a4dc5df5fd9f1929201bb3ab02c4cf36faee99919bbffcfd2df1fb21c6c"
   end
 
   depends_on "go" => :build
