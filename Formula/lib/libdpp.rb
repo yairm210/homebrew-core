a1b6b4a46624b5c59be7903953ad42beee189baf
Michael Cho <michael@michaelcho.dev>
2026-02-13 00:58:44 -0500

    libdpp: use `zlib-ng-compat` on Linux

--- Formula/lib/libdpp.rb
+++ Formula/lib/libdpp.rb
@@ -20,7 +20,9 @@ class Libdpp < Formula
   depends_on "opus"
   depends_on "pkgconf"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cmake", "-S", ".", "-B", "build",
