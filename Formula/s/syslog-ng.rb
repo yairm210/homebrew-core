2abfcfc82a31c86d0917f6cb16e5f4be22d98eae
Michael Cho <michael@michaelcho.dev>
2026-02-12 23:04:05 -0500

    syslog-ng: use `zlib-ng-compat` on Linux

--- Formula/s/syslog-ng.rb
+++ Formula/s/syslog-ng.rb
@@ -59,13 +59,16 @@ class SyslogNg < Formula
   depends_on "riemann-client"
 
   uses_from_macos "curl"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
     depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   def install
