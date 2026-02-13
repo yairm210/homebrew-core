1245ab3e44a248a17f8db002e029a7cc929d67d5
Michael Cho <michael@michaelcho.dev>
2026-02-13 18:09:04 -0500

    haskell-language-server: use `zlib-ng-compat` on Linux

--- Formula/h/haskell-language-server.rb
+++ Formula/h/haskell-language-server.rb
@@ -29,7 +29,10 @@ class HaskellLanguageServer < Formula
 
   uses_from_macos "libffi"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def ghcs
     deps.filter_map { |dep| dep.to_formula if dep.name.match? "ghc" }
