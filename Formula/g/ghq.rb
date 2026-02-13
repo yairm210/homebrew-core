bff4f9d56ca92d14296da89de15b8c4a65916fea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-13 20:53:21 +0000

    ghq: update 1.9.2 bottle.

--- Formula/g/ghq.rb
+++ Formula/g/ghq.rb
@@ -8,12 +8,12 @@ class Ghq < Formula
   head "https://github.com/x-motemen/ghq.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8dc7ec8dbeca8a75d7b476d7e1684eadbbb414b2e144d1eed54897dcb82a3f42"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2eed23e7893962dc55436f9f579ed1a110d4eade031a59adffe1dedfd35181ad"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ef4581fd8456a9fd896be7d570a5bb53aa952741b1f59e5c10867fb55b28299"
-    sha256 cellar: :any_skip_relocation, sonoma:        "22e8afca66c3c9a02acf61f659d8339d525820fd654d8eb13e71ce7513d1b02c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2cbf891b5f8b79ea7993eecdc6e309b9d7a3dfbca9f833789594b07f9d679056"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "27fd098a7e4a1ef71226f62af24885974b94ccbf87f12e1b7e63b7cd04cefafa"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b14a44162f0831a85b39ff398c2e2c52f83a718f7549aff4bac1ae974f9a57ae"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fc136bf938c9e020cf81157f87ac5d5e8c48894df9ef8c897989bb84acbcfc7e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4b80fb9ca9d07d9ecb4a111d0d64dd5578dcf9352979372c847f1e1694f8e355"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5ec189cbed0824f35d29b8d32836abc98cff02eb57630e76320d27ec2740d177"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ba0bcaa8d76bdf5a9ab2ad4986ab773f2176e52a0ff549a7961df2b71c579b18"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "52dd69f3968793ee7f0f8380c4260c48ede78ffac5795ead5034b2eb4df4143a"
   end
 
   depends_on "go" => :build
