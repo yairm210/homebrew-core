13dabd880a42d756235446c2324043c7370fdec8
Michael Cho <michael@michaelcho.dev>
2026-02-13 00:50:00 -0500

    asymptote: use `zlib-ng-compat` on Linux

--- Formula/a/asymptote.rb
+++ Formula/a/asymptote.rb
@@ -32,13 +32,13 @@ class Asymptote < Formula
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "libtool" => :build
     depends_on "freeglut"
     depends_on "libtirpc"
     depends_on "mesa"
+    depends_on "zlib-ng-compat"
   end
 
   resource "manual" do
