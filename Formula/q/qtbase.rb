ff07576477a501b3af65dc5f53c3e291f56464a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-13 16:58:02 +0000

    qtbase: update 6.10.2 bottle.

--- Formula/q/qtbase.rb
+++ Formula/q/qtbase.rb
@@ -21,12 +21,12 @@ class Qtbase < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5845b81159deb556bb702328515a1214d0fe5a8db68a60b0779e4e2b8cf9d474"
-    sha256 cellar: :any,                 arm64_sequoia: "10d29790eed4a92b187a48c7a2f19c61fe5c89c5ee56ab62417494dde561262e"
-    sha256 cellar: :any,                 arm64_sonoma:  "1306662b99e96bfbf8d3cc0597171bc0d912287ccc10f7f84034fc178fd75134"
-    sha256 cellar: :any,                 sonoma:        "55472252982d1f461ecfa7ea402ed24c9835245c333e476b145ce440695b0de9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "db0429dc8098f4e96a239a5caf70a56d32222a4c5f08c76c6194ad796e790579"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "53d38c25cc681e95848c5ffd3b3981bec77d0a9073a22f39c8d907364f56bd63"
+    sha256 cellar: :any,                 arm64_tahoe:   "62b847653b08af0913e0c006497bb98b2015c7549cf954cd10b2c6851a670480"
+    sha256 cellar: :any,                 arm64_sequoia: "aa74bb3590fcf718fd54028dd1f6d41cac584d9c79c860f1f8dd904a2e3e1665"
+    sha256 cellar: :any,                 arm64_sonoma:  "d28e4d0bcae1b026b69ea2ddd21e996332813cf23e980f2b5970e6175837ce9f"
+    sha256 cellar: :any,                 sonoma:        "ac88078b92f4f028a0a51bf5ecc44b9113fb9ec7ceedc67ca0b382309c48b320"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "82d00be342ed225f61d4762da61a8c81c88211598b0f8b84c6240cd1e49f2eac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b81bb0eaef4e7080afbb06223b371dbfbae5f479fd3395354d3affdbeead3da1"
   end
 
   depends_on "cmake" => [:build, :test]
