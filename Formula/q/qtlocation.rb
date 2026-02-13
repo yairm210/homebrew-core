210c0b6668880323f87c8fff442a0711c08f15b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-13 16:58:04 +0000

    qtlocation: update 6.10.2 bottle.

--- Formula/q/qtlocation.rb
+++ Formula/q/qtlocation.rb
@@ -16,12 +16,12 @@ class Qtlocation < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "232fc7a036a87eae756a030593e3b5612bb92b059a161f4e4a5a45f66d3f9d7d"
-    sha256 cellar: :any,                 arm64_sequoia: "8e22d020d4f032eb20bd8c59a3e0a254f9c02a988456bad4e2e83657ce11759e"
-    sha256 cellar: :any,                 arm64_sonoma:  "4d57df2b86a3f52d6e13f6653e4f5a29994282064d1cb27c21422dac7abb769d"
-    sha256 cellar: :any,                 sonoma:        "c04dee7e96ced16cf7dc2830f50e7da821731a10ea7f211001ddce5740db2188"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c7983537520a5c73722a9965e044f06fa7ed2bff3a58d61c0c8cd3961e8ae17e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "68e342b5229f106167b8f229a6ca4eea578f51eed9100b7bb6b7faa9abcc6036"
+    sha256 cellar: :any,                 arm64_tahoe:   "8495d6034db0d2b568779b0a8bfa73c9386425dcb9a46b14123e662f0e036a54"
+    sha256 cellar: :any,                 arm64_sequoia: "e3e88f8d7529fc644dd57ecf6f918715f81767b2d0c744c51499d2dbe4e9ea87"
+    sha256 cellar: :any,                 arm64_sonoma:  "8013e0f81b65c9ec151b1647c39ec0f24bdb6e01f713823cde54ed97a96b09e0"
+    sha256 cellar: :any,                 sonoma:        "ecaf0b90635659bb22c7d2459381e579b6ad3b97d1cffc1050483f50abbf605d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "654baac9736673a8b0a256b84f1e10224e265857aefd76314cdefa144cb4c061"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "feedcf761093383a84ef3b25ff0cad9eba3c036180dc653b5b241c5e1e16711d"
   end
 
   depends_on "cmake" => [:build, :test]
