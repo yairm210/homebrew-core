9146c81a0922287b92d5dfafc3a9dd6c8876ab19
Michael Cho <michael@michaelcho.dev>
2026-02-12 23:00:28 -0500

    synfig: use `zlib-ng-compat` on Linux

--- Formula/s/synfig.rb
+++ Formula/s/synfig.rb
@@ -51,7 +51,6 @@ class Synfig < Formula
   depends_on "pango"
 
   uses_from_macos "perl" => :build
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "liblqr"
@@ -61,6 +60,7 @@ class Synfig < Formula
 
   on_linux do
     depends_on "perl-xml-parser" => :build
+    depends_on "zlib-ng-compat"
   end
 
   def install
