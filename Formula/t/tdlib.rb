dfc98750275759f83d5ff122bff6c9a7ed3f8036
Michael Cho <michael@michaelcho.dev>
2026-02-13 18:14:01 -0500

    tdlib: use `zlib-ng-compat` on Linux

--- Formula/t/tdlib.rb
+++ Formula/t/tdlib.rb
@@ -27,7 +27,9 @@ class Tdlib < Formula
   depends_on "openssl@3"
   depends_on "readline"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Workaround to build with CMake 4
