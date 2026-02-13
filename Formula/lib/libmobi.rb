cbffacadb2c77c91d8d4569b27caa48b1ea7a04b
Michael Cho <michael@michaelcho.dev>
2026-02-13 01:06:15 -0500

    libmobi: use `zlib-ng-compat` on Linux

--- Formula/lib/libmobi.rb
+++ Formula/lib/libmobi.rb
@@ -16,7 +16,10 @@ class Libmobi < Formula
   end
 
   uses_from_macos "libxml2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   # Fix -flat_namespace being used on Big Sur and later.
   patch do
