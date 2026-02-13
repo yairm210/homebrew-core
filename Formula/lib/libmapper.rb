255a161a1be97c5f4b15ca7020d5d920f013e4df
Michael Cho <michael@michaelcho.dev>
2026-02-13 01:02:26 -0500

    libmapper: use `zlib-ng-compat` on Linux

--- Formula/lib/libmapper.rb
+++ Formula/lib/libmapper.rb
@@ -20,7 +20,9 @@ class Libmapper < Formula
   depends_on "pkgconf" => :build
   depends_on "liblo"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./configure", *std_configure_args
