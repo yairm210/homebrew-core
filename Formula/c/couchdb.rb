146c83ff1d43a1c0392cb44f69405c5725a36e8e
Michael Cho <michael@michaelcho.dev>
2026-02-13 18:22:32 -0500

    couchdb: use `zlib-ng-compat` on Linux

--- Formula/c/couchdb.rb
+++ Formula/c/couchdb.rb
@@ -26,7 +26,10 @@ class Couchdb < Formula
   depends_on "openssl@3"
 
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with "ejabberd", because: "both install `jiffy` lib"
 
