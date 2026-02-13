ef885d25c2d40c6a44ddeb88dbb8665dfbbd4fae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-13 18:27:21 +0000

    ktexttemplate: update 6.23.0 bottle.

--- Formula/k/ktexttemplate.rb
+++ Formula/k/ktexttemplate.rb
@@ -7,12 +7,12 @@ class Ktexttemplate < Formula
   head "https://invent.kde.org/frameworks/ktexttemplate.git", branch: "master"
 
   bottle do
-    sha256 arm64_tahoe:   "431f74be8f93e2fa064bbc9abf518ae47b601ad161048c88433a8ca7f286b8fd"
-    sha256 arm64_sequoia: "be262be047d5dbb9233ed725a96337939935e4facd8a4d78dee467bbb3ccada7"
-    sha256 arm64_sonoma:  "ef8fc89ecda259ab5bf3e97b24d6bfe7c721c43d411d4d1ef2de6730584b6e30"
-    sha256 sonoma:        "348c52e0ad9b90d555180cad95e58650d18b9980fa0bdcad8eb2285814f48a31"
-    sha256 arm64_linux:   "bb13612529ab33e142893ab6affddf68e23235fcb06be4a0855d8cfb34cbdeb6"
-    sha256 x86_64_linux:  "5b6bc39cb311eab46799bce89981af8f503cf6cfef7259f6af65e029e5c329fb"
+    sha256 arm64_tahoe:   "5e7c2aa5252254da4d94af44fce70f617902c67b38f5634ecc13b534752d091b"
+    sha256 arm64_sequoia: "f3ef209d63872d8c72f3e3b74971d301c427798b1518e68508e25189eb93d174"
+    sha256 arm64_sonoma:  "819fcf5abe12dbd4037ee1fad001ab3ede42b285b7b5b9e825a4aca20061d1ad"
+    sha256 sonoma:        "32ebaee7e36ccf230c693a8866723afc95ba47f9b62e18f0aa0a567066f68c59"
+    sha256 arm64_linux:   "4a3d3f0d54a6e7038a4218275e8709b0081ca787ed872ab6152b40267b746245"
+    sha256 x86_64_linux:  "c7b2cd2098d1ccf732b54928743865dc8324d82afaf8f9aca37d430ba436d21f"
   end
 
   depends_on "cmake" => [:build, :test]
