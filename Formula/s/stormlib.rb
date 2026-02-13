b8500755a2bec3a3c8e7ee5fa128144c19875300
Michael Cho <michael@michaelcho.dev>
2026-02-12 22:55:31 -0500

    stormlib: use `zlib-ng-compat` on Linux

--- Formula/s/stormlib.rb
+++ Formula/s/stormlib.rb
@@ -18,7 +18,10 @@ class Stormlib < Formula
   depends_on "cmake" => :build
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   # prevents cmake from trying to write to /Library/Frameworks/
   patch :DATA
