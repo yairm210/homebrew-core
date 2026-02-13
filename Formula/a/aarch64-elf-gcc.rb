a11415894d87875c882a455610bbcd83978a3ab5
Michael Cho <michael@michaelcho.dev>
2026-02-13 00:46:28 -0500

    aarch64-elf-gcc: use `zlib-ng-compat` on Linux

--- Formula/a/aarch64-elf-gcc.rb
+++ Formula/a/aarch64-elf-gcc.rb
@@ -28,7 +28,9 @@ class Aarch64ElfGcc < Formula
   depends_on "mpfr"
   depends_on "zstd"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     target = "aarch64-elf"
