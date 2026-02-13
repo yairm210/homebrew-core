5a8687add646bae09fdebb24c92ce4072fd319d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-13 22:28:13 +0000

    umoci: add 0.6.0 bottle.

--- Formula/u/umoci.rb
+++ Formula/u/umoci.rb
@@ -5,6 +5,15 @@ class Umoci < Formula
   sha256 "400a26c5f7ac06e40af907255e0e23407237d950e78e8d7c9043a1ad46da9ae5"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "829bbd94a1b9c3964d348579e9a28529a4ead9ced1b404c4d4f622605ea62fb6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "829bbd94a1b9c3964d348579e9a28529a4ead9ced1b404c4d4f622605ea62fb6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "829bbd94a1b9c3964d348579e9a28529a4ead9ced1b404c4d4f622605ea62fb6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "72fe058ae9412ed0ce970924c427e99d277fca22c78d04da4b01902b62c3cce6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3377df8adcffb0ae679c980d628d88b6e48b72993777edff8c3910808c7d96df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8fcda2e6f0a1069ccc89935c14ab9e168f9b183fe99ede1878d444bb8c44db0c"
+  end
+
   depends_on "go" => :build
   depends_on "go-md2man" => :build
   depends_on "gpgme"
