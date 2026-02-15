adc595485ffb5856b33ed02bffc59f95a7abeaf6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-15 17:39:23 +0000

    ktea: update 0.7.0 bottle.

--- Formula/k/ktea.rb
+++ Formula/k/ktea.rb
@@ -7,12 +7,12 @@ class Ktea < Formula
   head "https://github.com/jonas-grgt/ktea.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d646661116ad573a62dcccf6522c27f9324bff4a0f9ead96bca25c6ce91b4ae1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d646661116ad573a62dcccf6522c27f9324bff4a0f9ead96bca25c6ce91b4ae1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d646661116ad573a62dcccf6522c27f9324bff4a0f9ead96bca25c6ce91b4ae1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2dd05e0ae89d02c245b926b6cc0a79369e2fb97edd48d77f345f9e119cc6f544"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "347662cd56f5a0675fde15ea028516095acb3722fececd1af8afbbc775366e5e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6bd45f8ca6c854f040024ad921ef5df07e19060b62b7ff2487ecc552dd6d384e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2c686cf4405c7e37429cb80d07c90d7c14dfcafe7b2aca85b772b8944b09a664"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2c686cf4405c7e37429cb80d07c90d7c14dfcafe7b2aca85b772b8944b09a664"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2c686cf4405c7e37429cb80d07c90d7c14dfcafe7b2aca85b772b8944b09a664"
+    sha256 cellar: :any_skip_relocation, sonoma:        "aa1629422d0be09cabe893bbd35716aaf60c646b4af6264a02dbe543cc7f1779"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "abecac10133b436d9cd5b6b7afc881311fe9958bfe9fbc3719089ec325a703b1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc7618264ddaffefdee60d2089683e02f8b41c546eecdc9c43efffa94f2ca8a3"
   end
 
   depends_on "go" => :build
