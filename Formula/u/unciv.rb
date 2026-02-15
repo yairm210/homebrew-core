62d478cdc623a0549e6075893238f11d489005dd
Yair Morgenstern <yairm210@hotmail.com>
2026-02-15 13:26:47 +0200

    unciv 4.19.13
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/u/unciv.rb
+++ Formula/u/unciv.rb
@@ -1,8 +1,8 @@
 class Unciv < Formula
   desc "Open-source Android/Desktop remake of Civ V"
   homepage "https://github.com/yairm210/Unciv"
-  url "https://github.com/yairm210/Unciv/releases/download/4.19.12/Unciv.jar"
-  sha256 "aecc1cd2cb08005e00006886946bb696cd991b5a0fce32677798f90906fe1ec7"
+  url "https://github.com/yairm210/Unciv/releases/download/4.19.13/Unciv.jar"
+  sha256 "f18ee86fbb82d615c4483d909e16c051940c0974b2ae807d6b7c4eda8e6ca207"
   license "MPL-2.0"
 
   livecheck do
