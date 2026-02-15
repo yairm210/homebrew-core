9a46639d4f52a49189acc9558fb2482867b33950
Michael Cho <michael@michaelcho.dev>
2026-02-14 21:42:21 -0500

    tokyo-cabinet: use `zlib-ng-compat` on Linux

--- Formula/t/tokyo-cabinet.rb
+++ Formula/t/tokyo-cabinet.rb
@@ -31,7 +31,10 @@ class TokyoCabinet < Formula
   end
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
