e3236e6371c257332cdc40477fffef6fcd2f6931
Michael Cho <michael@michaelcho.dev>
2026-02-14 22:55:51 -0500

    python@3.12: use `zlib-ng-compat` on Linux

--- Formula/p/python@3.12.rb
+++ Formula/p/python@3.12.rb
@@ -35,12 +35,12 @@ class PythonAT312 < Formula
   uses_from_macos "libxcrypt"
   uses_from_macos "ncurses"
   uses_from_macos "unzip"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "berkeley-db@5"
     depends_on "libnsl"
     depends_on "libtirpc"
+    depends_on "zlib-ng-compat"
   end
 
   link_overwrite "lib/python3.12/site-packages/pip*"
