7c4a29eb0863cc2325ab93334a449aef2756186e
Michael Cho <michael@michaelcho.dev>
2026-02-14 21:39:49 -0500

    libtar: use `zlib-ng-compat` on Linux

--- Formula/lib/libtar.rb
+++ Formula/lib/libtar.rb
@@ -29,7 +29,9 @@ class Libtar < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "autoreconf", "--force", "--install", "--verbose"
