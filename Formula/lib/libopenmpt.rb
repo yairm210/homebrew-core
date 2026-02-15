86e35fd624d9c957f0f39c6078d98ab0064ddfae
Rui Chen <rui@chenrui.dev>
2026-02-15 11:12:14 -0500

    libopenmpt: use `zlib-ng-compat` on linux
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libopenmpt.rb
+++ Formula/lib/libopenmpt.rb
@@ -29,10 +29,9 @@ class Libopenmpt < Formula
   depends_on "mpg123"
   depends_on "portaudio"
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "pulseaudio"
+    depends_on "zlib-ng-compat"
   end
 
   def install
