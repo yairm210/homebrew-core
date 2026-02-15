72fdf0e688e72429bc6884c65577a055085fb5a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-15 14:04:50 +0000

    docmd 0.4.5
    docmd: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/docmd.rb
+++ Formula/d/docmd.rb
@@ -1,20 +1,31 @@
 class Docmd < Formula
   desc "Minimal Markdown documentation generator"
   homepage "https://docmd.mgks.dev/"
-  url "https://registry.npmjs.org/@mgks/docmd/-/docmd-0.3.11.tgz"
-  sha256 "04ac749185914d5e2e067940b6164e6b211372b21f4752d7540a9f8d7c700a33"
+  url "https://registry.npmjs.org/@mgks/docmd/-/docmd-0.4.5.tgz"
+  sha256 "d73c88b53de2aa1e169472c3da582f482143fc630a60ca3b05e3c9d1d5c5e031"
   license "MIT"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "f0a3bc4f0ff6efeedcb5cc2b3c641328a84cebacebb13c47b2678bfeef3865b4"
-  end
 
   depends_on "esbuild" # for prebuilt binaries
   depends_on "node"
 
+  on_linux do
+    depends_on "xsel"
+  end
+
   def install
     system "npm", "install", *std_npm_args
     bin.install_symlink libexec.glob("bin/*")
+
+    node_modules = libexec/"lib/node_modules/@mgks/docmd/node_modules"
+    deuniversalize_machos node_modules/"fsevents/fsevents.node" if OS.mac?
+
+    clipboardy_fallbacks_dir = node_modules/"clipboardy/fallbacks"
+    rm_r(clipboardy_fallbacks_dir)
+    if OS.linux?
+      linux_dir = clipboardy_fallbacks_dir/"linux"
+      linux_dir.mkpath
+      ln_sf Formula["xsel"].opt_bin/"xsel", linux_dir/"xsel"
+    end
 
     # Remove pre-built binaries
     rm_r(libexec/"lib/node_modules/@mgks/docmd/node_modules/@esbuild")
