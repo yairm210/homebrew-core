4cad3fbfa75c4e7373fe21473e84e0fff243013e
Michael Cho <michael@michaelcho.dev>
2026-02-14 22:54:05 -0500

    libxfont: use `zlib-ng-compat` on Linux

--- Formula/lib/libxfont.rb
+++ Formula/lib/libxfont.rb
@@ -31,7 +31,10 @@ class Libxfont < Formula
   depends_on "xorgproto"
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = %W[
