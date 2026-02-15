e553a5aed94b5540c7046bbee0e03b17692c4a1a
Rui Chen <rui@chenrui.dev>
2026-02-15 11:12:13 -0500

    libmng: use `zlib-ng-compat` on linux
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libmng.rb
+++ Formula/lib/libmng.rb
@@ -27,7 +27,9 @@ class Libmng < Formula
   depends_on "jpeg-turbo"
   depends_on "little-cms2"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   resource "sample" do
     url "https://telparia.com/fileFormatSamples/image/mng/abydos.mng"
