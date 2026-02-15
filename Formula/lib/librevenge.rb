0fef17547183cda8202df057e2d0127181c1404c
Michael Cho <michael@michaelcho.dev>
2026-02-14 22:51:25 -0500

    librevenge: use `zlib-ng-compat` on Linux

--- Formula/lib/librevenge.rb
+++ Formula/lib/librevenge.rb
@@ -30,7 +30,9 @@ class Librevenge < Formula
   depends_on "boost" => :build
   depends_on "pkgconf" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./configure", "--without-docs",
