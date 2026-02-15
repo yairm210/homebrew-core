629af6ed274b0b307a3adc13848eb73dea5391d2
Michael Cho <michael@michaelcho.dev>
2026-02-14 22:55:04 -0500

    pgbackrest: use `zlib-ng-compat` on Linux

--- Formula/p/pgbackrest.rb
+++ Formula/p/pgbackrest.rb
@@ -27,7 +27,10 @@ class Pgbackrest < Formula
 
   uses_from_macos "bzip2"
   uses_from_macos "libxml2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV.append "LDFLAGS", "-Wl,-rpath,#{rpath(target: Formula["libpq"].opt_lib)}" if OS.linux?
