a5bb16afb15c24b8565780f06e22e4737ee0a8eb
Michael Cho <michael@michaelcho.dev>
2026-02-14 22:53:27 -0500

    libfreehand: use `zlib-ng-compat` on Linux

--- Formula/lib/libfreehand.rb
+++ Formula/lib/libfreehand.rb
@@ -36,7 +36,10 @@ class Libfreehand < Formula
   depends_on "little-cms2"
 
   uses_from_macos "gperf" => :build
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   # remove with version >=0.1.3
   patch do
