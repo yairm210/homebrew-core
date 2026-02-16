43db8299293f7df114c69bd46bfbb269aec21bff
Michael Cho <michael@michaelcho.dev>
2026-02-16 02:27:00 -0500

    xrootd: use `zlib-ng-compat` on Linux

--- Formula/x/xrootd.rb
+++ Formula/x/xrootd.rb
@@ -27,10 +27,10 @@ class Xrootd < Formula
   uses_from_macos "curl"
   uses_from_macos "libxcrypt"
   uses_from_macos "libxml2"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "util-linux" # for libuuid
+    depends_on "zlib-ng-compat"
   end
 
   def python3
