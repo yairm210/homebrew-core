8b09f152d05dc825c36bd5d3922f49ba05ad1ca7
Branch Vincent <branchevincent@gmail.com>
2026-02-16 14:35:08 -0800

    parliament: remove setuptools

--- Formula/p/parliament.rb
+++ Formula/p/parliament.rb
@@ -21,21 +21,19 @@ class Parliament < Formula
   depends_on "libyaml"
   depends_on "python@3.14"
 
-  pypi_packages extra_packages: "setuptools"
-
   resource "boto3" do
-    url "https://files.pythonhosted.org/packages/29/30/755a6c4b27ad4effefa9e407f84c6f0a69f75a21c0090beb25022dfcfd3f/boto3-1.42.25.tar.gz"
-    sha256 "ccb5e757dd62698d25766cc54cf5c47bea43287efa59c93cf1df8c8fbc26eeda"
+    url "https://files.pythonhosted.org/packages/59/41/7a7280875ec000e280b0392478a5d6247bc88e7ecf2ae6ec8f4ddb35b014/boto3-1.42.50.tar.gz"
+    sha256 "38545d7e6e855fefc8a11e899ccbd6d2c9f64671d6648c2acfb1c78c1057a480"
   end
 
   resource "botocore" do
-    url "https://files.pythonhosted.org/packages/2c/b5/8f961c65898deb5417c9e9e908ea6c4d2fe8bb52ff04e552f679c88ed2ce/botocore-1.42.25.tar.gz"
-    sha256 "7ae79d1f77d3771e83e4dd46bce43166a1ba85d58a49cffe4c4a721418616054"
+    url "https://files.pythonhosted.org/packages/93/fd/e63789133b2bf044c8550cd6766ec93628b0ac18a03f2aa0b80171f0697a/botocore-1.42.50.tar.gz"
+    sha256 "de1e128e4898f4e66877bfabbbb03c61f99366f27520442539339e8a74afe3a5"
   end
 
   resource "jmespath" do
-    url "https://files.pythonhosted.org/packages/00/2a/e867e8531cf3e36b41201936b7fa7ba7b5702dbef42922193f05c8976cd6/jmespath-1.0.1.tar.gz"
-    sha256 "90261b206d6defd58fdd5e85f478bf633a2901798906be2ad389150c5c60edbe"
+    url "https://files.pythonhosted.org/packages/d3/59/322338183ecda247fb5d1763a6cbe46eff7222eaeebafd9fa65d4bf5cb11/jmespath-1.1.0.tar.gz"
+    sha256 "472c87d80f36026ae83c6ddd0f1d05d4e510134ed462851fd5f754c8c3cbb88d"
   end
 
   resource "json-cfg" do
@@ -63,11 +61,6 @@ class Parliament < Formula
     sha256 "8e990f13268025792229cd52fa10cb7163744bf56e719e0b9cb925ab79abf920"
   end
 
-  resource "setuptools" do
-    url "https://files.pythonhosted.org/packages/18/5d/3bf57dcd21979b887f014ea83c24ae194cfcd12b9e0fda66b957c69d1fca/setuptools-80.9.0.tar.gz"
-    sha256 "f36b47402ecde768dbfafc46e8e4207b4360c654f1f3bb84475f0a28628fb19c"
-  end
-
   resource "six" do
     url "https://files.pythonhosted.org/packages/94/e7/b2c673351809dca68a0e064b6af791aa332cf192da575fd474ed7d6f16a2/six-1.17.0.tar.gz"
     sha256 "ff70335d468e7eb6ec65b95b99d3a2836546063f63acc5171de367e834932a81"
@@ -77,6 +70,9 @@ class Parliament < Formula
     url "https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3/urllib3-2.6.3.tar.gz"
     sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
   end
+
+  # Replace `pkg_resources` for Python 3.12+: https://github.com/duo-labs/parliament/pull/258
+  patch :DATA
 
   def install
     virtualenv_install_with_resources
@@ -89,3 +85,33 @@ class Parliament < Formula
                 "\"Action\": \"s3:GetObject\", \"Resource\": \"arn:aws:s3:::secretbucket\"}}'").strip
   end
 end
+
+__END__
+diff --git a/parliament/__init__.py b/parliament/__init__.py
+index ea32a6f..6254b71 100644
+--- a/parliament/__init__.py
++++ b/parliament/__init__.py
+@@ -10,16 +10,18 @@ import json
+ import jsoncfg
+ import re
+ 
+-import pkg_resources
++from importlib.resources import files, as_file
+ import yaml
+ 
+ # On initialization, load the IAM data
+-iam_definition_path = pkg_resources.resource_filename(__name__, "iam_definition.json")
+-iam_definition = json.load(open(iam_definition_path, "r"))
++iam_definition_path = files(__name__) / "iam_definition.json"
++with as_file(iam_definition_path) as path:
++    iam_definition = json.loads(path.read_text())
+ 
+ # And the config data
+-config_path = pkg_resources.resource_filename(__name__, "config.yaml")
+-config = yaml.safe_load(open(config_path, "r"))
++config_path = files(__name__) / "config.yaml"
++with as_file(config_path) as path:
++    config = yaml.safe_load(path.read_text())
+ 
+ 
+ def override_config(override_config_path):
