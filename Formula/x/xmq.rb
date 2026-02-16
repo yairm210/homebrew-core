8ce47ba9897ce126c3399df73032a4d5de06a74c
Michael Cho <michael@michaelcho.dev>
2026-02-16 02:27:36 -0500

    xmq: use `zlib-ng-compat` on Linux

--- Formula/x/xmq.rb
+++ Formula/x/xmq.rb
@@ -27,7 +27,10 @@ class Xmq < Formula
 
   uses_from_macos "libxml2"
   uses_from_macos "libxslt"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "autoreconf", "--force", "--install", "--verbose" if build.head?
