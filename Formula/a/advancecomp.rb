40353c2fc3d09c183c8fc7f2bced0fabddde11f4
Michael Cho <michael@michaelcho.dev>
2026-02-17 09:43:41 -0500

    advancecomp: use `zlib-ng-compat` on Linux

--- Formula/a/advancecomp.rb
+++ Formula/a/advancecomp.rb
@@ -23,7 +23,10 @@ class Advancecomp < Formula
   depends_on "libtool" => :build
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "autoreconf", "--force", "--install", "--verbose"
