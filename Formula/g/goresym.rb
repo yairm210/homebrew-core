5a937f885a907a29a7c384c14306c115da537657
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 17:52:07 +0000

    goresym: update 3.2 bottle.

--- Formula/g/goresym.rb
+++ Formula/g/goresym.rb
@@ -7,14 +7,12 @@ class Goresym < Formula
   head "https://github.com/mandiant/GoReSym.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "879de704173f2e066df717c6190b178f0a188b709a6c3f5e315c714ea74592ea"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "acf2b47031c6261e1d977bf293fb452485fea96cc206a6f3027ec7e18f86e04a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "acf2b47031c6261e1d977bf293fb452485fea96cc206a6f3027ec7e18f86e04a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "acf2b47031c6261e1d977bf293fb452485fea96cc206a6f3027ec7e18f86e04a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "91cbad4c26d47b16a46c27d48cc9798bca37915f72870ba29fb3a069bafa3295"
-    sha256 cellar: :any_skip_relocation, ventura:       "91cbad4c26d47b16a46c27d48cc9798bca37915f72870ba29fb3a069bafa3295"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e7e30fb975d2d32fa76af54e04edc2b138b8b878b9c5e13fd1bd74d0e3322f05"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f6414715ef12ade766c4e58fe223f6a341c4fbde88bf69495260bde6e100870b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "92a9cbd4568cf0c24b7efd08925d682b9d97979b979fd37df7b647df74dc32f5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92a9cbd4568cf0c24b7efd08925d682b9d97979b979fd37df7b647df74dc32f5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "92a9cbd4568cf0c24b7efd08925d682b9d97979b979fd37df7b647df74dc32f5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c58f83dcafdeb17f8674276892c7d236c17c7331781ea2d1137da341e67dc1f6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "93acb0e2901d05130ed6e59a711a69ea593f938511817e3b26e055bf29b4a6ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1f5ae6ccbca1ed390f82e8d67ccf1f8b4a2ca4ead1bf066dc275e6d38c1d14d9"
   end
 
   # Unpin Go when GoReSym supports Go 1.26, ref: https://github.com/mandiant/GoReSym/issues/80
