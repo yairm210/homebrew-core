cc3ad60fee023ef4e0cd385197ee8836f9166954
Michael Cho <michael@michaelcho.dev>
2026-02-17 14:55:25 -0500

    felinks: use `zlib-ng-compat` on Linux

--- Formula/f/felinks.rb
+++ Formula/f/felinks.rb
@@ -33,7 +33,10 @@ class Felinks < Formula
   uses_from_macos "curl"
   uses_from_macos "expat"
   uses_from_macos "python"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = %w[
