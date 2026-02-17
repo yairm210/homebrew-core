d4e545fe7bb0aa0b6e21bd3498be84878b5c5fd6
Michael Cho <michael@michaelcho.dev>
2026-02-17 15:52:49 -0500

    licensed: use `zlib-ng-compat` on Linux

--- Formula/l/licensed.rb
+++ Formula/l/licensed.rb
@@ -21,10 +21,9 @@ class Licensed < Formula
   depends_on "ruby"
   depends_on "xz"
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "openssl@3"
+    depends_on "zlib-ng-compat"
   end
 
   def install
