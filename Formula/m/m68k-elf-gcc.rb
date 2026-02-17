3e98f6f19d72dfbe2455cd21da70ae992651dee4
Michael Cho <michael@michaelcho.dev>
2026-02-17 15:53:44 -0500

    m68k-elf-gcc: use `zlib-ng-compat` on Linux

--- Formula/m/m68k-elf-gcc.rb
+++ Formula/m/m68k-elf-gcc.rb
@@ -28,7 +28,9 @@ class M68kElfGcc < Formula
   depends_on "mpfr"
   depends_on "zstd"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     target = "m68k-elf"
