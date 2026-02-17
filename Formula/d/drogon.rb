3cb194ffdb75b95e3c030b8ac60b63ee15f5ce60
Michael Cho <michael@michaelcho.dev>
2026-02-17 11:51:54 -0500

    drogon: use `zlib-ng-compat` on Linux

--- Formula/d/drogon.rb
+++ Formula/d/drogon.rb
@@ -24,10 +24,10 @@ class Drogon < Formula
   depends_on "openssl@3"
 
   uses_from_macos "sqlite"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "util-linux"
+    depends_on "zlib-ng-compat"
   end
 
   def install
