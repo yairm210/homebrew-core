6d254e25cdd2884499cd1d6158039128b6b5b7c7
Michael Cho <michael@michaelcho.dev>
2026-02-17 09:49:25 -0500

    bazarr: use `zlib-ng-compat` on Linux

--- Formula/b/bazarr.rb
+++ Formula/b/bazarr.rb
@@ -26,7 +26,10 @@ class Bazarr < Formula
 
   uses_from_macos "libxml2", since: :ventura
   uses_from_macos "libxslt"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   pypi_packages package_name:   "",
                 extra_packages: ["lxml", "setuptools", "webrtcvad-wheels"]
