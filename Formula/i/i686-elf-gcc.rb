4181f488bb2da1acefe4e8134036a6521286af99
Michael Cho <michael@michaelcho.dev>
2026-02-17 15:12:10 -0500

    i686-elf-gcc: use `zlib-ng-compat` on Linux

--- Formula/i/i686-elf-gcc.rb
+++ Formula/i/i686-elf-gcc.rb
@@ -27,7 +27,9 @@ class I686ElfGcc < Formula
   depends_on "mpfr"
   depends_on "zstd"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     target = "i686-elf"
