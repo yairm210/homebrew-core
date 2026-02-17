93f62ccfe50b75f83ea2e106a2e47888b006b947
Michael Cho <michael@michaelcho.dev>
2026-02-17 11:50:54 -0500

    deadfinder: use `zlib-ng-compat` on Linux

--- Formula/d/deadfinder.rb
+++ Formula/d/deadfinder.rb
@@ -21,7 +21,10 @@ class Deadfinder < Formula
   uses_from_macos "libffi"
   uses_from_macos "libxml2"
   uses_from_macos "libxslt"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     ENV["BUNDLE_FORCE_RUBY_PLATFORM"] = "1"
