c20a98e24b20173a2a08d19e5ca35528417f0ab0
Michael Cho <michael@michaelcho.dev>
2026-02-17 15:12:24 -0500

    intelli-shell: use `zlib-ng-compat` on Linux

--- Formula/i/intelli-shell.rb
+++ Formula/i/intelli-shell.rb
@@ -19,7 +19,9 @@ class IntelliShell < Formula
   depends_on "rust" => :build
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
