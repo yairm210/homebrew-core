c79c97e31720e5f9ff49cea33b5543c508bd5c10
Michael Cho <michael@michaelcho.dev>
2026-02-17 16:00:35 -0500

    mariadb@10.6: use `zlib-ng-compat` on Linux

--- Formula/m/mariadb@10.6.rb
+++ Formula/m/mariadb@10.6.rb
@@ -51,10 +51,10 @@ class MariadbAT106 < Formula
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
   uses_from_macos "xz"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "linux-pam"
+    depends_on "zlib-ng-compat"
   end
 
   def install
