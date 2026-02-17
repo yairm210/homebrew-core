a554539e927e1faecb178268b8603acc5f361265
Michael Cho <michael@michaelcho.dev>
2026-02-17 09:50:09 -0500

    boost@1.85: use `zlib-ng-compat` on Linux

--- Formula/b/boost@1.85.rb
+++ Formula/b/boost@1.85.rb
@@ -26,7 +26,10 @@ class BoostAT185 < Formula
   depends_on "zstd"
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Force boost to compile with the desired compiler
