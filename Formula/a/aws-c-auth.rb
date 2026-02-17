4623f3ad86046c02c4c12639a7dc2e0d9d240a83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 19:58:59 +0000

    aws-c-auth 0.9.6

--- Formula/a/aws-c-auth.rb
+++ Formula/a/aws-c-auth.rb
@@ -1,8 +1,8 @@
 class AwsCAuth < Formula
   desc "C99 library implementation of AWS client-side authentication"
   homepage "https://github.com/awslabs/aws-c-auth"
-  url "https://github.com/awslabs/aws-c-auth/archive/refs/tags/v0.9.5.tar.gz"
-  sha256 "39000bff55fe8c82265b9044a966ab37da5c192a775e1b68b6fcba7e7f9882fb"
+  url "https://github.com/awslabs/aws-c-auth/archive/refs/tags/v0.9.6.tar.gz"
+  sha256 "2448e939d924731a891bec34f6da764d3a34afd52b9f5a3e614bb1bf96e6452d"
   license "Apache-2.0"
 
   bottle do
