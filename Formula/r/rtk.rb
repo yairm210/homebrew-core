b986fba974f41ea3e0e68e57f8ecedb8e82e49dd
Rui Chen <rui@chenrui.dev>
2026-02-17 13:31:32 -0500

    rtk 0.20.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/r/rtk.rb
@@ -0,0 +1,22 @@
+class Rtk < Formula
+  desc "CLI proxy to minimize LLM token consumption"
+  homepage "https://www.rtk-ai.app/"
+  url "https://github.com/rtk-ai/rtk/archive/refs/tags/v0.20.1.tar.gz"
+  sha256 "6a6bc346061f29d117df76e8b619d20739ebeede5b7b7f6003f276e21faf5fdd"
+  license "MIT"
+  head "https://github.com/rtk-ai/rtk.git", branch: "master"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/rtk --version")
+
+    (testpath/"homebrew.txt").write "hello from homebrew\n"
+    output = shell_output("#{bin}/rtk ls #{testpath}")
+    assert_match "homebrew.txt", output
+  end
+end
