104e0c4ed202f77bd7d36a9fde210bb4851cb34d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 20:10:01 +0000

    gitlab-ci-local 4.66.1

--- Formula/g/gitlab-ci-local.rb
+++ Formula/g/gitlab-ci-local.rb
@@ -1,8 +1,8 @@
 class GitlabCiLocal < Formula
   desc "Run gitlab pipelines locally as shell executor or docker executor"
   homepage "https://github.com/firecow/gitlab-ci-local"
-  url "https://registry.npmjs.org/gitlab-ci-local/-/gitlab-ci-local-4.65.1.tgz"
-  sha256 "e6b1de5993ad55e68c59d3c1e4a35e99944f48e0712eddeab9b16abebf007f76"
+  url "https://registry.npmjs.org/gitlab-ci-local/-/gitlab-ci-local-4.66.1.tgz"
+  sha256 "f5d8efbd1520623c17ae021282a069f2139e7aa37c2b048e08dd2ac9df8c7054"
   license "MIT"
 
   bottle do
