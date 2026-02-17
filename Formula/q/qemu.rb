882a65dc67009fa41049c6a617ad990059ab9372
Michael Cho <michael@michaelcho.dev>
2026-02-17 15:47:32 -0500

    qemu: use `zlib-ng-compat` on Linux

--- Formula/q/qemu.rb
+++ Formula/q/qemu.rb
@@ -46,7 +46,6 @@ class Qemu < Formula
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "attr"
@@ -61,6 +60,7 @@ class Qemu < Formula
     depends_on "libxkbcommon"
     depends_on "mesa"
     depends_on "systemd"
+    depends_on "zlib-ng-compat"
   end
 
   def install
