54cd07c1739e54d0658b9e6e8a37541873e19efd
Michael Cho <michael@michaelcho.dev>
2026-02-17 09:58:08 -0500

    emqx: use `zlib-ng-compat` on Linux

--- Formula/e/emqx.rb
+++ Formula/e/emqx.rb
@@ -36,7 +36,10 @@ class Emqx < Formula
   uses_from_macos "cyrus-sasl"
   uses_from_macos "krb5"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with "cassandra", because: "both install `nodetool` binaries"
 
