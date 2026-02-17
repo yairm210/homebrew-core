d6ca626798b3060471d43e8c003bd12348614458
Michael Cho <michael@michaelcho.dev>
2026-02-17 16:01:27 -0500

    mkvtoolnix: use `zlib-ng-compat` on Linux

--- Formula/m/mkvtoolnix.rb
+++ Formula/m/mkvtoolnix.rb
@@ -45,10 +45,13 @@ class Mkvtoolnix < Formula
 
   uses_from_macos "libxslt" => :build
   uses_from_macos "ruby" => :build
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   conflicts_with cask: "mkvtoolnix-app"
