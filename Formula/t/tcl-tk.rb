20de82721875bac41f88a76eeea38ef9e56e747a
Michael Cho <michael@michaelcho.dev>
2026-02-17 15:20:02 -0500

    tcl-tk: use `zlib-ng-compat` on Linux

--- Formula/t/tcl-tk.rb
+++ Formula/t/tcl-tk.rb
@@ -23,13 +23,12 @@ class TclTk < Formula
   depends_on "libtommath"
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "freetype" => :build
     depends_on "pkgconf" => :build
     depends_on "libx11"
     depends_on "libxext"
+    depends_on "zlib-ng-compat"
   end
 
   conflicts_with "page", because: "both install `page` binaries"
