5f8216e8262dad9de4286b59c484018a29832db3
Michael Cho <michael@michaelcho.dev>
2026-02-17 11:51:35 -0500

    dotnet@9: use `zlib-ng-compat` on Linux

--- Formula/d/dotnet@9.rb
+++ Formula/d/dotnet@9.rb
@@ -33,7 +33,6 @@ class DotnetAT9 < Formula
 
   uses_from_macos "python" => :build
   uses_from_macos "krb5"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "grep" => :build # grep: invalid option -- P
@@ -42,6 +41,7 @@ class DotnetAT9 < Formula
   on_linux do
     depends_on "libunwind"
     depends_on "lttng-ust"
+    depends_on "zlib-ng-compat"
 
     on_intel do
       depends_on "llvm" => :build
