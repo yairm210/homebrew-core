d98413e2fc0923340bc6382f1c32115ca923565b
Michael Cho <michael@michaelcho.dev>
2026-02-17 16:09:56 -0500

    libmagic: use `zlib-ng-compat` on Linux

--- Formula/lib/libmagic.rb
+++ Formula/lib/libmagic.rb
@@ -23,7 +23,10 @@ class Libmagic < Formula
   end
 
   depends_on "pkgconf" => :test
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./configure", "--disable-silent-rules",
