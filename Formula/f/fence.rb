797c43b37ca099f5c616a70e7e41c240b23e96e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 02:52:39 +0000

    fence 0.1.27

--- Formula/f/fence.rb
+++ Formula/f/fence.rb
@@ -1,8 +1,8 @@
 class Fence < Formula
   desc "Lightweight sandbox for commands with network and filesystem restrictions"
   homepage "https://github.com/Use-Tusk/fence"
-  url "https://github.com/Use-Tusk/fence/archive/refs/tags/v0.1.26.tar.gz"
-  sha256 "d9360a76c3be2e946b390f3bbab3cdb37a5be9938324cbf949d3c8dc15da60f8"
+  url "https://github.com/Use-Tusk/fence/archive/refs/tags/v0.1.27.tar.gz"
+  sha256 "78a98ff08b1ec2589b261798750db4d48862beb845ba9f3fcdebc4275a8a21d3"
   license "Apache-2.0"
   head "https://github.com/Use-Tusk/fence.git", branch: "main"
 
