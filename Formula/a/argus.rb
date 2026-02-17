7d3070eea5e4a9a5d04444bb3505db6fc7a2fafe
Michael Cho <michael@michaelcho.dev>
2026-02-17 09:46:56 -0500

    argus: use `zlib-ng-compat` on Linux

--- Formula/a/argus.rb
+++ Formula/a/argus.rb
@@ -20,10 +20,10 @@ class Argus < Formula
   uses_from_macos "flex" => :build
   uses_from_macos "cyrus-sasl"
   uses_from_macos "libpcap"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "libtirpc"
+    depends_on "zlib-ng-compat"
   end
 
   def install
