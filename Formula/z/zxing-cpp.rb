72bf56e3fe509e508c0deeb29d724a5f56f98d8b
axxel <awagger@gmail.com>
2026-02-18 00:51:24 +0100

    zxing-cpp: bump to 3.0.2 and add cli tools

--- Formula/z/zxing-cpp.rb
+++ Formula/z/zxing-cpp.rb
@@ -1,10 +1,19 @@
 class ZxingCpp < Formula
   desc "Multi-format barcode image processing library written in C++"
   homepage "https://github.com/zxing-cpp/zxing-cpp"
-  url "https://github.com/zxing-cpp/zxing-cpp/releases/download/v3.0.1/zxing-cpp-3.0.1-homebrew.tar.gz"
-  sha256 "c0f210924955785004150c71d77306da8c6461b5db6abdb323fc331d6a213876"
   license "Apache-2.0"
   head "https://github.com/zxing-cpp/zxing-cpp.git", branch: "master"
+
+  stable do
+    url "https://github.com/zxing-cpp/zxing-cpp/releases/download/v3.0.2/zxing-cpp-3.0.2.tar.gz"
+    sha256 "e957f13e2ad4e31badb3d9af3f6ba8999a3ca3c9cc4d6bafc98032f9cce1a090"
+
+    # add support for homebrew specific STB_IMAGE_INCLUDE_DIR config option
+    patch do
+      url "https://github.com/zxing-cpp/zxing-cpp/commit/764f7ac3438f0e7638da27ad00cc2147312a2ea6.patch?full_index=1"
+      sha256 "2174f23e784b8fd68a5f0b3fdf467f9c22c448b73ca40133be711486dfe8d82b"
+    end
+  end
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "18ea1b16214084445c7e74b143d4985aba52702a4ebf0523b5e7b930271d7a52"
@@ -17,10 +26,28 @@ class ZxingCpp < Formula
 
   depends_on "cmake" => :build
 
+  resource "stb_image" do
+    url "https://raw.githubusercontent.com/nothings/stb/013ac3beddff3dbffafd5177e7972067cd2b5083/stb_image.h"
+    version "2.30"
+    sha256 "594c2fe35d49488b4382dbfaec8f98366defca819d916ac95becf3e75f4200b3"
+  end
+
+  resource "stb_image_write" do
+    url "https://raw.githubusercontent.com/nothings/stb/1ee679ca2ef753a528db5ba6801e1067b40481b8/stb_image_write.h"
+    version "1.16"
+    sha256 "cbd5f0ad7a9cf4468affb36354a1d2338034f2c12473cf1a8e32053cb6914a05"
+  end
+
   def install
-    args = %w[
-      -DZXING_C_API=OFF
-      -DZXING_EXAMPLES=OFF
+    resources.each do |r|
+      r.stage do
+        (include/"stb").install "#{r.name}.h"
+      end
+    end
+
+    args = %W[
+      -DZXING_DEPENDENCIES=LOCAL
+      -DSTB_IMAGE_INCLUDE_DIR=#{include}/stb
     ]
 
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
@@ -40,5 +67,7 @@ class ZxingCpp < Formula
 
     system ENV.cxx, "test.cpp", "-std=c++20", "-I#{include}", "-L#{lib}", "-lZXing", "-o", "test"
     system "./test"
+
+    assert_match version.to_s, shell_output("#{bin}/ZXingReader --version")
   end
 end
