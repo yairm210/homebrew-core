d9397e336225110eacf7ae6fbcdbff38eaf06895
Michael Cho <michael@michaelcho.dev>
2026-02-17 15:04:04 -0500

    gnupg: use `zlib-ng-compat` on Linux

--- Formula/g/gnupg.rb
+++ Formula/g/gnupg.rb
@@ -35,10 +35,13 @@ class Gnupg < Formula
   uses_from_macos "bzip2"
   uses_from_macos "openldap"
   uses_from_macos "sqlite"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   conflicts_with cask: "gpg-suite"
