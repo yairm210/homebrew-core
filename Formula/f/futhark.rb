ea98e270aa8d18ec856e7d44319a0b63adb0959a
Michael Cho <michael@michaelcho.dev>
2026-02-17 14:57:07 -0500

    futhark: use `zlib-ng-compat` on Linux

--- Formula/f/futhark.rb
+++ Formula/f/futhark.rb
@@ -22,7 +22,10 @@ class Futhark < Formula
 
   uses_from_macos "libffi"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cabal", "v2-update"
