4efba3870956eca9a75c47a209189ce8aa266108
Michael Cho <michael@michaelcho.dev>
2026-02-17 11:51:08 -0500

    dosbox-staging: use `zlib-ng-compat` on Linux

--- Formula/d/dosbox-staging.rb
+++ Formula/d/dosbox-staging.rb
@@ -39,12 +39,11 @@ class DosboxStaging < Formula
   depends_on "sdl2_net"
   depends_on "speexdsp"
 
-  uses_from_macos "zlib"
-
   on_linux do
     depends_on "alsa-lib"
     depends_on "mesa"
     depends_on "mesa-glu"
+    depends_on "zlib-ng-compat"
   end
 
   def install
