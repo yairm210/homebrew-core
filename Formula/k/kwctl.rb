90e326101853b6fbc1ffb363f34ea77b0da6cfe4
Rui Chen <rui@chenrui.dev>
2026-02-18 00:13:49 -0500

    kwctl 1.32.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/kwctl.rb
+++ Formula/k/kwctl.rb
@@ -1,10 +1,10 @@
 class Kwctl < Formula
   desc "CLI tool for the Kubewarden policy engine for Kubernetes"
   homepage "https://www.kubewarden.io/"
-  url "https://github.com/kubewarden/kwctl/archive/refs/tags/v1.31.0.tar.gz"
-  sha256 "251212ed21c1add9acbfce3695f0812b210dc71d5fd57ca246f1f1a052be4417"
+  url "https://github.com/kubewarden/kubewarden-controller/archive/refs/tags/v1.32.1.tar.gz"
+  sha256 "7683b958d19557462af93eb27a1ddb53bfd7579e3ab5b51e59e0db4ec6f28b76"
   license "Apache-2.0"
-  head "https://github.com/kubewarden/kwctl.git", branch: "main"
+  head "https://github.com/kubewarden/kubewarden-controller.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "26075ede58140d586ae0d4eea412f0e53d2cfe21bf14cecf1cf412fbe67f18da"
@@ -19,14 +19,15 @@ class Kwctl < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "install", *std_cargo_args(path: "crates/kwctl")
 
     generate_completions_from_executable(bin/"kwctl", "completions", "--shell")
   end
 
   test do
+    assert_match version.to_s, shell_output("#{bin}/kwctl --version")
+
     test_policy = "ghcr.io/kubewarden/policies/safe-labels:v0.1.7"
-    assert_equal "kwctl #{version}", shell_output("#{bin}/kwctl --version").strip.split("\n")[0]
     system bin/"kwctl", "pull", test_policy
     assert_match test_policy, shell_output("#{bin}/kwctl policies")
 
