c8994a0d427f3dd8481b458db5a41883b41853b2
Michael Cho <michael@michaelcho.dev>
2026-02-18 14:18:36 -0500

    jruby: source build and replace pre-builts

--- Formula/j/jruby.rb
+++ Formula/j/jruby.rb
@@ -1,8 +1,8 @@
 class Jruby < Formula
   desc "Ruby implementation in pure Java"
   homepage "https://www.jruby.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/jruby/jruby-dist/10.0.3.0/jruby-dist-10.0.3.0-bin.tar.gz"
-  sha256 "0edb5b02c3f482205d1cf8358f38e31d9e4c6d93a210039224750c72501e4717"
+  url "https://search.maven.org/remotecontent?filepath=org/jruby/jruby-dist/10.0.3.0/jruby-dist-10.0.3.0-src.zip"
+  sha256 "9209a9f0ed7d585e63e408e229cb629a3a18f04428373b8e8d9ed74c1e51abc2"
   license any_of: ["EPL-2.0", "GPL-2.0-only", "LGPL-2.1-only"]
 
   livecheck do
@@ -19,41 +19,89 @@ class Jruby < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "8f7f3b5501ef40ed646ad5ee00f0f2184765d2138fa3f483601b32de5aedc724"
   end
 
+  depends_on "ant" => :build # for jffi
+  depends_on "maven" => :build
+  depends_on "pkgconf" => :build # for jffi
+  depends_on "ruby" => :build # only used to detect conflicts
+
+  depends_on "libfixposix" => :no_linkage
   depends_on "openjdk"
 
-  def install
-    # Remove Windows files
-    rm Dir["bin/*.{bat,dll,exe}"]
+  uses_from_macos "libffi" # for jffi
 
-    cd "bin" do
-      # Prefix a 'j' on some commands to avoid clashing with other rubies
-      %w[ast erb bundle bundler rake rdoc ri racc].each { |f| mv f, "j#{f}" }
-      # Delete some unnecessary commands
-      rm "gem" # gem is a wrapper script for jgem
-      rm "irb" # irb is an identical copy of jirb
+  resource "jffi" do
+    url "https://github.com/jnr/jffi/archive/refs/tags/jffi-1.3.14.tar.gz"
+    sha256 "dfc120bc832cd81940fb785bef2987bd3f54199fddbed0e62145718d5a3d3b95"
+
+    livecheck do
+      url "https://raw.githubusercontent.com/jruby/jruby/refs/tags/#{LATEST_VERSION}/pom.xml"
+      strategy :xml do |xml|
+        xml.get_elements("//properties/jffi.version").map(&:text)
+      end
+    end
+  end
+
+  def install
+    jffi_version = Version.new(File.read("pom.xml")[/<jffi\.version>([\d.]+)</i, 1])
+    resource("jffi").stage do |r|
+      odie "Need jffi version #{jffi_version}!" if r.version != jffi_version
+
+      # Remove pre-built binaries and bundled libffi
+      rm(Dir["archive/*"])
+      rm_r("jni/libffi")
+      ENV["LIBFFI_LIBS"] = if OS.mac?
+        MacOS.sdk_for_formula(self).path/"usr/lib/libffi.tbd"
+      else
+        Formula["libffi"].opt_lib/shared_library("libffi")
+      end
+
+      # Avoid building universal binaries. Cannot use change_make_var! due to indentation
+      inreplace "jni/GNUmakefile", "ARCHES = x86_64 arm64", "ARCHES = #{Hardware::CPU.arch}"
+
+      system "ant", "-Duse.system.libffi=1", "jar"
+      system "ant", "-Duse.system.libffi=1", "archive-platform-jar"
+      system "mvn", "package"
+
+      # Install JARs into local repository to be used by Maven when building JRuby
+      system "mvn", "install:install-file", "-Dfile=target/jffi-#{r.version}.jar"
+      system "mvn", "install:install-file", "-Dfile=target/jffi-#{r.version}-native.jar",
+                                            "-DgroupId=com.github.jnr",
+                                            "-DartifactId=jffi",
+                                            "-Dpackaging=jar",
+                                            "-Dversion=#{r.version}",
+                                            "-Dclassifier=native"
     end
 
-    # Only keep the macOS native libraries
-    rm_r(Dir["lib/jni/*"] - ["lib/jni/Darwin"])
-    libexec.install Dir["*"]
+    system "mvn", "-Pdist"
+    libexec.mkpath
+    tarball = "maven/jruby-dist/target/jruby-dist-#{version}-bin.tar.gz"
+    system "tar", "--extract", "--file", tarball, "--directory", libexec, "--strip-components=1"
+
+    # Make sure locally built copy was used by checking there is a single library
+    jni_libs = libexec.glob("lib/jni/**/*jffi*").select(&:file?)
+    odie "Expected single jffi library but found:\n  #{jni_libs.join("\n  ")}." unless jni_libs.one?
+
+    # Remove Windows files and ffi-binary-libfixposix gem (pre-built libfixposix)
+    rm libexec.glob("bin/*.{bat,dll,exe}")
+    rm libexec/"lib/ruby/stdlib/libfixposix/binary.rb"
+    rm_r libexec/"lib/ruby/stdlib/libfixposix/binary"
+
+    # Expose commands on PATH but prefix a 'j' on any that conflict with Ruby
     bin.install libexec.glob("bin/*")
     bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
-
-    # Remove incompatible libfixposix library
-    os = OS.kernel_name.downcase
-    if OS.linux?
-      arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    (bin.children(false) & Formula["ruby"].bin.children(false)).each do |cmd|
+      if (bin/"j#{cmd}").exist?
+        rm(bin/cmd)
+      else
+        bin.install bin/cmd => "j#{cmd}"
+      end
     end
-    libfixposix_binary = libexec/"lib/ruby/stdlib/libfixposix/binary"
-    libfixposix_binary.children
-                      .each { |dir| rm_r(dir) if dir.basename.to_s != "#{arch}-#{os}" }
-
-    # Replace (prebuilt!) universal binaries with their native slices
-    # FIXME: Build libjffi-1.2.jnilib from source.
-    deuniversalize_machos
   end
 
   test do
-    assert_equal "hello\n", shell_output("#{bin}/jruby -e \"puts 'hello'\"")
+    assert_equal "hello\n", shell_output("#{bin}/jruby -e 'puts :hello'")
+
+    ENV["GEM_HOME"] = testpath
+    system bin/"jgem", "install", "json"
   end
 end
