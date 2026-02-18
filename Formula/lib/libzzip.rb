eb5f491d2fde6e924a7e88bfcfb3de4101ec4828
Michael Cho <michael@michaelcho.dev>
2026-02-17 22:03:57 -0500

    libzzip: use `zlib-ng-compat` on Linux

--- Formula/lib/libzzip.rb
+++ Formula/lib/libzzip.rb
@@ -21,7 +21,10 @@ class Libzzip < Formula
 
   uses_from_macos "python" => :build
   uses_from_macos "zip" => :test
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = %W[
