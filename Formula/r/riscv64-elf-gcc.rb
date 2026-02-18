2e77d92bf3345675cb258ccd7f959d35dc3480d4
Michael Cho <michael@michaelcho.dev>
2026-02-18 15:01:40 -0500

    riscv64-elf-gcc: use `zlib-ng-compat` on Linux

--- Formula/r/riscv64-elf-gcc.rb
+++ Formula/r/riscv64-elf-gcc.rb
@@ -27,7 +27,9 @@ class Riscv64ElfGcc < Formula
   depends_on "riscv64-elf-binutils"
   depends_on "zstd"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     target = "riscv64-elf"
