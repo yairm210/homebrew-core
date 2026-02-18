59b96ff1610e7c4b399bee95599e23d3c569f018
Michael Cho <michael@michaelcho.dev>
2026-02-18 15:04:24 -0500

    rye: use `zlib-ng-compat` on Linux

--- Formula/r/rye.rb
+++ Formula/r/rye.rb
@@ -25,10 +25,10 @@ class Rye < Formula
   depends_on "rust" => :build
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "openssl@3"
+    depends_on "zlib-ng-compat"
   end
 
   conflicts_with "ryelang", because: "both install `rye` binaries"
