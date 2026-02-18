489ff3d736b5ac672fe8736599483201ae56f81d
Michael Cho <michael@michaelcho.dev>
2026-02-17 22:09:39 -0500

    libmediainfo: use `zlib-ng-compat` on Linux

--- Formula/lib/libmediainfo.rb
+++ Formula/lib/libmediainfo.rb
@@ -27,7 +27,10 @@ class Libmediainfo < Formula
   depends_on "libzen"
 
   uses_from_macos "curl"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   # These files used to be distributed as part of the media-info formula
   link_overwrite "include/MediaInfo/*"
