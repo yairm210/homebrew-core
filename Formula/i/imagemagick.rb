9ce0545dd34e11ded569f6c1128c2e7a4de38d0c
Michael Cho <michael@michaelcho.dev>
2026-02-18 15:44:59 -0500

    imagemagick: use `zlib-ng-compat` on Linux

--- Formula/i/imagemagick.rb
+++ Formula/i/imagemagick.rb
@@ -39,11 +39,14 @@ class Imagemagick < Formula
 
   uses_from_macos "bzip2"
   uses_from_macos "libxml2"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gettext"
     depends_on "imath"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   skip_clean :la
