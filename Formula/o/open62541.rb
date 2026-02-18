8e571c76310736584ae9a3e27c214695fcc7cc8a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-18 20:26:59 +0000

    open62541 1.5.1

--- Formula/o/open62541.rb
+++ Formula/o/open62541.rb
@@ -1,8 +1,8 @@
 class Open62541 < Formula
   desc "Open source implementation of OPC UA"
   homepage "https://open62541.org/"
-  url "https://github.com/open62541/open62541/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "b7d92fe4b767aac2935ffb7bff9c88d528ce8a05a3767d093748f14388622636"
+  url "https://github.com/open62541/open62541/archive/refs/tags/v1.5.1.tar.gz"
+  sha256 "da029a0960be24b8f7c157a5d373b50ebf7d238a5a33eca0820c7c04f47946c9"
   license "MPL-2.0"
 
   livecheck do
