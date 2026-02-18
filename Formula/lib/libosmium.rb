c6f3d2f043ef2c1559f25e97c8a737b13d5bf908
Michael Cho <michael@michaelcho.dev>
2026-02-17 22:06:12 -0500

    libosmium: use `zlib-ng-compat` on Linux

--- Formula/lib/libosmium.rb
+++ Formula/lib/libosmium.rb
@@ -16,7 +16,10 @@ class Libosmium < Formula
 
   uses_from_macos "bzip2"
   uses_from_macos "expat"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = %W[
