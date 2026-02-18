64edb6aea674197e5ad23d61a8377bc2e9c13113
Michael Cho <michael@michaelcho.dev>
2026-02-18 15:08:29 -0500

    openssh: use `zlib-ng-compat` on Linux

--- Formula/o/openssh.rb
+++ Formula/o/openssh.rb
@@ -35,10 +35,10 @@ class Openssh < Formula
   uses_from_macos "krb5"
   uses_from_macos "libedit"
   uses_from_macos "libxcrypt"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "linux-pam"
+    depends_on "zlib-ng-compat"
   end
 
   resource "com.openssh.sshd.sb" do
