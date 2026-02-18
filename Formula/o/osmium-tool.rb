f7147298714e4959060d3fa3fb54b2fb560147bb
Michael Cho <michael@michaelcho.dev>
2026-02-18 15:09:12 -0500

    osmium-tool: use `zlib-ng-compat` on Linux

--- Formula/o/osmium-tool.rb
+++ Formula/o/osmium-tool.rb
@@ -24,7 +24,10 @@ class OsmiumTool < Formula
 
   uses_from_macos "bzip2"
   uses_from_macos "expat"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     protozero = Formula["protozero"].opt_include
