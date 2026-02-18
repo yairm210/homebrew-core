d217d6eb83d8ca13c797f7cbd4573e826888f52b
Michael Cho <michael@michaelcho.dev>
2026-02-17 22:00:01 -0500

    libre: use `zlib-ng-compat` on Linux

--- Formula/lib/libre.rb
+++ Formula/lib/libre.rb
@@ -17,7 +17,9 @@ class Libre < Formula
   depends_on "cmake" => :build
   depends_on "openssl@3"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cmake", "-B", "build", *std_cmake_args
