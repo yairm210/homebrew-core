4f6a13eb4cb8c50f814f99f2543738c751678037
Michael Cho <michael@michaelcho.dev>
2026-02-17 22:02:11 -0500

    libstrophe: use `zlib-ng-compat` on Linux

--- Formula/lib/libstrophe.rb
+++ Formula/lib/libstrophe.rb
@@ -24,7 +24,10 @@ class Libstrophe < Formula
   depends_on "openssl@3"
 
   uses_from_macos "expat"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "./bootstrap.sh"
