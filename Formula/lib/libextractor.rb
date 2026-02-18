9b9cbbf0209b789a6116d04a252cccd8d5c2f598
Michael Cho <michael@michaelcho.dev>
2026-02-17 21:50:28 -0500

    libextractor: use `zlib-ng-compat` on Linux

--- Formula/lib/libextractor.rb
+++ Formula/lib/libextractor.rb
@@ -24,7 +24,9 @@ class Libextractor < Formula
   depends_on "pkgconf" => :build
   depends_on "libtool"
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with "csound", because: "both install `extract` binaries"
 
