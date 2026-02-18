8163ed8937f904a9e2bb77fea37cb8cfee3f6802
Michael Cho <michael@michaelcho.dev>
2026-02-18 15:03:24 -0500

    ruby@3.2: use `zlib-ng-compat` on Linux

--- Formula/r/ruby@3.2.rb
+++ Formula/r/ruby@3.2.rb
@@ -33,7 +33,10 @@ class RubyAT32 < Formula
   uses_from_macos "libffi"
   uses_from_macos "libxcrypt"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   # Should be updated only when Ruby is updated (if an update is available).
   # The exception is Rubygem security fixes, which mandate updating this
