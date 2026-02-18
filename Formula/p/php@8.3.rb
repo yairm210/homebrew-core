30263619b663bb256593d889874dd1b0071204b1
Michael Cho <michael@michaelcho.dev>
2026-02-18 01:19:23 -0500

    php@8.3: use `zlib-ng-compat` on Linux

--- Formula/p/php@8.3.rb
+++ Formula/p/php@8.3.rb
@@ -79,13 +79,16 @@ class PhpAT83 < Formula
   uses_from_macos "libffi"
   uses_from_macos "libxml2"
   uses_from_macos "libxslt"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gettext"
     # PHP build system incorrectly links system libraries
     # see https://github.com/php/php-src/issues/10680
     patch :DATA
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   def install
