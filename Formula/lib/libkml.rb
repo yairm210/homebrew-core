76b1d64522694cd16ab989cafd80d84bbb246e9f
Michael Cho <michael@michaelcho.dev>
2026-02-18 00:55:47 -0500

    libkml: use `zlib-ng-compat` on Linux

--- Formula/lib/libkml.rb
+++ Formula/lib/libkml.rb
@@ -30,7 +30,10 @@ class Libkml < Formula
 
   uses_from_macos "curl"
   uses_from_macos "expat"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Workaround to build with CMake 4
