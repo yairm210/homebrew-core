9c96d68cc9dec2ae0b16840f3618f3094fff024d
Michael Cho <michael@michaelcho.dev>
2026-02-17 21:51:59 -0500

    libgsf: use `zlib-ng-compat` on Linux

--- Formula/lib/libgsf.rb
+++ Formula/lib/libgsf.rb
@@ -28,10 +28,13 @@ class Libgsf < Formula
 
   uses_from_macos "bzip2"
   uses_from_macos "libxml2"
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   def install
