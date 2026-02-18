d33ea049d45ff491ac15e9812dc833f1f8254a8c
Michael Cho <michael@michaelcho.dev>
2026-02-17 22:03:01 -0500

    libtorrent-rakshasa: use `zlib-ng-compat` on Linux

--- Formula/lib/libtorrent-rakshasa.rb
+++ Formula/lib/libtorrent-rakshasa.rb
@@ -26,7 +26,10 @@ class LibtorrentRakshasa < Formula
   depends_on "openssl@3"
 
   uses_from_macos "curl"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   conflicts_with "libtorrent-rasterbar", because: "both use the same libname"
 
