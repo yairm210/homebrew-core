01c9f746aeed00a5512940b9493d30f9e414be8c
Michael Cho <michael@michaelcho.dev>
2026-02-18 15:02:27 -0500

    rocksdb: use `zlib-ng-compat` on Linux

--- Formula/r/rocksdb.rb
+++ Formula/r/rocksdb.rb
@@ -22,7 +22,10 @@ class Rocksdb < Formula
   depends_on "zstd"
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     args = %W[
