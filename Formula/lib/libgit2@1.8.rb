7890aabd70d572bce7ba66ad2dbae195e6024cea
Michael Cho <michael@michaelcho.dev>
2026-02-18 00:55:06 -0500

    libgit2@1.8: use `zlib-ng-compat` on Linux

--- Formula/lib/libgit2@1.8.rb
+++ Formula/lib/libgit2@1.8.rb
@@ -24,10 +24,10 @@ class Libgit2AT18 < Formula
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
   depends_on "libssh2"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "openssl@3" # Uses SecureTransport on macOS.
+    depends_on "zlib-ng-compat"
   end
 
   def install
