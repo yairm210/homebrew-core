bf5a923f7409e51d777cc32a14c7e33c1e581ce8
Michael Cho <michael@michaelcho.dev>
2026-02-19 14:55:58 -0500

    texlive: use `zlib-ng-compat` on Linux

--- Formula/t/texlive.rb
+++ Formula/t/texlive.rb
@@ -74,7 +74,6 @@ class Texlive < Formula
 
   uses_from_macos "ncurses"
   uses_from_macos "ruby"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "libice"
@@ -87,6 +86,7 @@ class Texlive < Formula
     depends_on "libxpm"
     depends_on "libxt"
     depends_on "mesa"
+    depends_on "zlib-ng-compat"
   end
 
   conflicts_with "cweb", because: "both install `cweb` binaries"
