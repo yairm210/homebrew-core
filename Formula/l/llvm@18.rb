18ed49a76a9b8f5f81bcc326516c52c3ecfc1501
Michael Cho <michael@michaelcho.dev>
2026-02-19 11:27:57 -0500

    llvm@18: use `zlib-ng-compat` on Linux

--- Formula/l/llvm@18.rb
+++ Formula/l/llvm@18.rb
@@ -7,11 +7,8 @@ class LlvmAT18 < Formula
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   livecheck do
-    url :stable
-    regex(/^llvmorg[._-]v?(18(?:\.\d+)+)$/i)
+    skip "No longer developed or maintained"
   end
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     rebuild 1
@@ -37,12 +34,12 @@ class LlvmAT18 < Formula
   uses_from_macos "libedit"
   uses_from_macos "libffi"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "pkgconf" => :build
     depends_on "binutils" # needed for gold
     depends_on "elfutils" # openmp requires <gelf.h>
+    depends_on "zlib-ng-compat"
   end
 
   def python3
