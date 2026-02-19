51f144a214e951e5e99800a80c11a9e8ca74f525
Michael Cho <michael@michaelcho.dev>
2026-02-19 14:46:46 -0500

    git-annex: use `zlib-ng-compat` on Linux

--- Formula/g/git-annex.rb
+++ Formula/g/git-annex.rb
@@ -28,7 +28,10 @@ class GitAnnex < Formula
   depends_on "libmagic"
 
   uses_from_macos "libffi"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     # Workaround to build aeson with GHC 9.14, https://github.com/haskell/aeson/issues/1155
