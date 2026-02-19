7a095474ad947e75e14c71fff28ffbd841e5aaf2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 14:47:20 +0000

    zeroclaw: add 0.1.0 bottle.

--- Formula/z/zeroclaw.rb
+++ Formula/z/zeroclaw.rb
@@ -6,6 +6,15 @@ class Zeroclaw < Formula
   license "MIT"
   head "https://github.com/zeroclaw-labs/zeroclaw.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6db9835aef789e139089449e8686216132c7633b0f7222dae8bd392b79171940"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "486f3566bb1cbd004443bb89060cead705ae5a8c9d4c671bbd9beab4519aaa35"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "520491ae609b1242ff08e0a88b46c0f432ba159c32e4babdd171c1827cfcff60"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3e2693c1cb6090bc680b7b785c260c10ad0cd77d3bc752387c9767cab275661f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "505944b58eb1c4acd55a26317f3a91b0ca4bd15f988833b930f8559c14c028fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f507a02012dff8d2f60659ed860b6a6cf1b21d3471762c2a027d7b85fe8dd194"
+  end
+
   depends_on "rust" => :build
 
   def install
