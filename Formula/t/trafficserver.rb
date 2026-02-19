61d77518ce6e385b06842433cc1b19808405fe62
Michael Cho <michael@michaelcho.dev>
2026-02-19 15:08:20 -0500

    trafficserver: use `zlib-ng-compat` on Linux

--- Formula/t/trafficserver.rb
+++ Formula/t/trafficserver.rb
@@ -54,11 +54,11 @@ class Trafficserver < Formula
   uses_from_macos "flex" => :build
   uses_from_macos "curl"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "libcap"
     depends_on "libunwind"
+    depends_on "zlib-ng-compat"
   end
 
   def install
