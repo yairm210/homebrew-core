caab65ea2b154b1c9d45c984f1219835048e2e75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 12:18:36 +0000

    redocly-cli: update 2.19.1 bottle.

--- Formula/r/redocly-cli.rb
+++ Formula/r/redocly-cli.rb
@@ -6,7 +6,7 @@ class RedoclyCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e8eaadd1e91d9ccab16fa47fc59db96272a7efff14b833bd74c0af949ce7150c"
+    sha256 cellar: :any_skip_relocation, all: "91a8866991deead6fe1eaa0c66671437ba8472df28df3a949bd0c27292962cb3"
   end
 
   depends_on "node"
