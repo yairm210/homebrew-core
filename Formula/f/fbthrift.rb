1255546a9428ba0458b400872994366c4557977e
Michael Cho <michael@michaelcho.dev>
2026-02-18 22:28:10 -0500

    fbthrift: use `zlib-ng-compat` on Linux

--- Formula/f/fbthrift.rb
+++ Formula/f/fbthrift.rb
@@ -32,7 +32,6 @@ class Fbthrift < Formula
 
   uses_from_macos "flex" => :build
   uses_from_macos "python" => :build
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "llvm" if DevelopmentTools.clang_build_version <= 1100
@@ -40,6 +39,7 @@ class Fbthrift < Formula
 
   on_linux do
     depends_on "boost"
+    depends_on "zlib-ng-compat"
   end
 
   fails_with :clang do
