c926274dfbbafcaa957867d2b7991c7c27cec054
Michael Cho <michael@michaelcho.dev>
2026-02-19 15:08:43 -0500

    pypy: use `zlib-ng-compat` on Linux

--- Formula/p/pypy.rb
+++ Formula/p/pypy.rb
@@ -33,7 +33,10 @@ class Pypy < Formula
   uses_from_macos "libffi"
   uses_from_macos "ncurses"
   uses_from_macos "unzip"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   resource "bootstrap" do
     on_macos do
