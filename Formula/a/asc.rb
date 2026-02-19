403a2e229be6fdc2188589e8872c2555f59f8da0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 08:02:15 +0000

    asc 0.29.2

--- Formula/a/asc.rb
+++ Formula/a/asc.rb
@@ -1,8 +1,8 @@
 class Asc < Formula
   desc "Fast, lightweight CLI for App Store Connect"
   homepage "https://asccli.sh"
-  url "https://github.com/rudrankriyam/App-Store-Connect-CLI/archive/refs/tags/0.29.1.tar.gz"
-  sha256 "bdc78303d43d811b831f913984a01bf11e3138436a4725c8a6f7688486311d94"
+  url "https://github.com/rudrankriyam/App-Store-Connect-CLI/archive/refs/tags/0.29.2.tar.gz"
+  sha256 "dd2702177c30d19de3855279014af3bcf9b778bc34d18293c2166fb9bd762f4b"
   license "MIT"
   head "https://github.com/rudrankriyam/App-Store-Connect-CLI.git", branch: "main"
 
