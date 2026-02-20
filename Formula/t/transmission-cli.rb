c3bb696866506a18967540bc9d2a1e139ea9ffb6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 05:42:18 +0000

    transmission-cli 4.1.1

--- Formula/t/transmission-cli.rb
+++ Formula/t/transmission-cli.rb
@@ -1,8 +1,8 @@
 class TransmissionCli < Formula
   desc "Lightweight BitTorrent client"
   homepage "https://transmissionbt.com/"
-  url "https://github.com/transmission/transmission/releases/download/4.1.0/transmission-4.1.0.tar.xz"
-  sha256 "dcd28c1c9e6126229c4c17dbc9e95c9fd4aed7e76f4a1f2a74604c8cddec49d6"
+  url "https://github.com/transmission/transmission/releases/download/4.1.1/transmission-4.1.1.tar.xz"
+  sha256 "e743283ee03a42c4d0b08fed2bd52b554aa6c9f65b4d4d45b795c32d98762a79"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
 
   livecheck do
