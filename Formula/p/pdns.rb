95bf58ecf697399962f56d2c94cf4482c559021e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 08:22:23 +0000

    pdns 5.0.3

--- Formula/p/pdns.rb
+++ Formula/p/pdns.rb
@@ -1,10 +1,9 @@
 class Pdns < Formula
   desc "Authoritative nameserver"
   homepage "https://www.powerdns.com"
-  url "https://downloads.powerdns.com/releases/pdns-5.0.2.tar.bz2"
-  sha256 "d360e1fa127a562a4ad0ff648aef56af76b678311c6553a7f7034677438a085d"
+  url "https://downloads.powerdns.com/releases/pdns-5.0.3.tar.bz2"
+  sha256 "ec3120501950a772c785c600f599e8f4d711f703a02cbd1bec42edc1a05f81cc"
   license "GPL-2.0-or-later"
-  revision 1
 
   # The first-party download page (https://www.powerdns.com/downloads) isn't
   # always updated for newer versions, so for now we have to check the
