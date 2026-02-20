a4d3a42e943dbbedd6cde650d242fba42ccae5c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 18:28:35 +0000

    libgcrypt: update 1.12.1 bottle.

--- Formula/lib/libgcrypt.rb
+++ Formula/lib/libgcrypt.rb
@@ -11,12 +11,12 @@ class Libgcrypt < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "16322b7413313ab8d0153e7de12534f030c69993a540168773b37e3aa20a56ad"
-    sha256 cellar: :any,                 arm64_sequoia: "ecb9f091b6b819e7fa659d50edd7a8eebc46b5c00d486f91a0cb5ce91bbceef2"
-    sha256 cellar: :any,                 arm64_sonoma:  "0e03d89e6feff9b8bf59a40a35a1d45cbfa287bfc648ade1b591757ea1339afb"
-    sha256 cellar: :any,                 sonoma:        "ea6683e879f359926d7825da3063be268c030a2a6356a89099076148b6db1786"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1c76175d2eeceac7421eb666cdbc047dba5ab6df69a0ebfcee6e191ea0b35eff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b418fa5f73cb4b901b6b3b56e49708c36f499daa1998442c1523e8693c5bb580"
+    sha256 cellar: :any,                 arm64_tahoe:   "cff8b6a3c92ffd76aa26ccbcc13e21e10ca6b3b231ded3d35e81e38ed5b05a6d"
+    sha256 cellar: :any,                 arm64_sequoia: "1e90fbea3e8a54c2309a5f54db60159cbf4ac34264e60040e31e48b02c1c9d8c"
+    sha256 cellar: :any,                 arm64_sonoma:  "885a7de8758e4bd968dd91d1acbd4f090913dc7396aca96503c6fca2c7712001"
+    sha256 cellar: :any,                 sonoma:        "41423fc76f133a106758d801991bcd20fcb0dfdc2eed8f1ccbcbb1b324481a16"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9910126e491fb609f3671bcdc0ca9d9aeeb633361dc65fae56def5b5a3d4b7b7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec18f27e8109b7fb873ad1ead6acb4150af9ffb6c3a2f9f40c6e80c28c73e67e"
   end
 
   depends_on "libgpg-error"
