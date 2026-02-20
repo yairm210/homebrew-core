7c2386065fecae46a5f0eba40a7ce4f003a8acd9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 11:45:23 +0000

    vet 1.13.2

--- Formula/v/vet.rb
+++ Formula/v/vet.rb
@@ -1,8 +1,8 @@
 class Vet < Formula
   desc "Policy driven vetting of open source dependencies"
   homepage "https://safedep.io/"
-  url "https://github.com/safedep/vet/archive/refs/tags/v1.13.1.tar.gz"
-  sha256 "9ed03582f4eedc97e5442ba1dd0d575cab2cc1fca6b6bb2da043b79f02fb74e4"
+  url "https://github.com/safedep/vet/archive/refs/tags/v1.13.2.tar.gz"
+  sha256 "2735adc30a0f962dc7f78ef2c1ec4f3708a6454949848c6f2685d23fd1ca26d6"
   license "Apache-2.0"
 
   bottle do
