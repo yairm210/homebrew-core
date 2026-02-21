d484dd1e146f5d7eb26322f29b73799893efc643
Thierry Moisan <thierry.moisan@gmail.com>
2026-02-21 15:01:48 -0500

    oasis: add shell completions

--- Formula/o/oasis.rb
+++ Formula/o/oasis.rb
@@ -25,6 +25,7 @@ class Oasis < Formula
     ]
 
     system "go", "build", *std_go_args(ldflags:)
+    generate_completions_from_executable(bin/"oasis", shell_parameter_format: :cobra)
   end
 
   test do
