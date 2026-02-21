49065a7fd7d6a52b55be0dd6b1956e63218b8050
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 17:14:40 +0000

    nullclaw: add 2026.2.21 bottle.

--- Formula/n/nullclaw.rb
+++ Formula/n/nullclaw.rb
@@ -6,6 +6,15 @@ class Nullclaw < Formula
   license "MIT"
   head "https://github.com/nullclaw/nullclaw.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bbf3335b09949a724f5a9dbeb46981e0e07ba2e42794bc9f3d8f02d82cd0eae4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "37ce80189fde35537ac8b5749e3bebef54ba0a3be6614a406a4879b6a7938b3a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3217d5922322ffef6d9a503411190acc37da80d98d1468fb89e7e5a0cb888a72"
+    sha256 cellar: :any_skip_relocation, sonoma:        "22c34ae5b71b80cc70e0564ed1d5592bceafd770383beb990855f3f1c9aa4449"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "03a41960311ff4274a0423852a234aff185ba161c39341a23498088a25720db9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1516d7eff02d06d0e35420953f45e47195751d4fe76ed80047a7091d45669120"
+  end
+
   depends_on "zig" => :build
 
   def install
