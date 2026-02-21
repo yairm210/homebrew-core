5dc46395a957b468ee12e63c01bffbfbbc8c7a70
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 17:12:49 +0000

    clock-rs: add 0.1.31 bottle.

--- Formula/c/clock-rs.rb
+++ Formula/c/clock-rs.rb
@@ -5,6 +5,15 @@ class ClockRs < Formula
   sha256 "dc8670d9922ed7b4b5ceb79a9e329f3a194c2c26edbba01655279b0c503d39d5"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "002bf576942dd9b68666682b720ee3ff566b23c4d6283cdcb3acd6d2cd733af7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "83cd616af31d3557e81ce59788724ccc4abcfd467749ba584d37a8f1fa32b601"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e94146ab237c62d89a6b5cbc5e948bf456faa2c21b0c55ed365d5ebd73cc4c25"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0f33b06dc2f2d1e8353815959bcfc66b8262952552f5cd8153d24188154dba95"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b75baec67210c16e002c7703649ecc9186f7c2f3e89f1ddd808a35051d09b654"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e00217eca643f38a182029b3bdc57a9710a1b6d73e4af8c139de5f8a1c73d14"
+  end
+
   depends_on "rust" => :build
 
   def install
