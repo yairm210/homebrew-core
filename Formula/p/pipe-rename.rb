eb348446c9c0f96ddf6b6ad5dfe66ea87d215c15
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 11:07:17 +0000

    pipe-rename: update 1.6.7 bottle.

--- Formula/p/pipe-rename.rb
+++ Formula/p/pipe-rename.rb
@@ -7,12 +7,12 @@ class PipeRename < Formula
   head "https://github.com/marcusbuffett/pipe-rename.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c473246a77bec35a66a8a707873f1bc8649da9df73aa5d5fcae650403de7e921"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0891a2a485b0cab7fd7ce8a888f40f03988ac69ab96ea1524975ca1d5f722403"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "978ee728779ea0bb5f26ca997acf0983bb282d4151d793370f4b4e893a4d5cb1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "97ca54f1aaaeb7e1433e4dfc5bc10e1fde65d6689b4daba91e77474f2940be01"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "45d3ee6ac754a0c4b3150f3040c0bf5497e410e0339bb7948b65b60633a8333d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5ec3f113b225c212860f40fb65b0e97f03128c39a6107cc938debb301dc7d7c7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8886d9edd07b147fbf3ce064fc16e76dbf8e6743adcf51e430a96840a4ad8d37"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0d21af83ebdd3f7b5469630cf427f33bdab6dc981663a81eb16a0a326e8df434"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cec1a4fd195e350bdd24cf90802188a33b161c570dff6045683272021000efea"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a03aa9abc0ed27030089de75e0c528e2a556526441761668417be2e2cea9dcc7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "47fc5b5e6bb06cf1e0e152f47a4cf5676874206be8c6950dc92370274c300ef1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e9bb134cc3f9d6e85c6165c6f942e8ec532a96a1d1dd7f329fdcbd5fa1eae21"
   end
 
   depends_on "rust" => :build
