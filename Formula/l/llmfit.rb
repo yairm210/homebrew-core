20536c8ecef04e831cb9452dbac61f8b496a41f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 21:34:07 +0000

    llmfit: add 0.3.8 bottle.

--- Formula/l/llmfit.rb
+++ Formula/l/llmfit.rb
@@ -6,6 +6,15 @@ class Llmfit < Formula
   license "MIT"
   head "https://github.com/AlexsJones/llmfit.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "07f77c479c2620bc374ff74447b47bf078b1425b08142061201a6a6ebdebfbad"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c5469d37c5fd63d4629fe5608af82d4b37d87ff7232925398735a1274354f235"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eb6258b1565f0ab7671c0d1d053f13feffe1083e722016379a2db0779134c080"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c2c7e77aa61f3be66b0e23a1335566f6ab6a2aa634d411d7898db4bd9801741f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "afe169e97a568bb0025c9089f4962e954fbe42cbc7b008e52d0c53f9e6d4bcf0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5ddaea739bd90a7361513251d1e15575d84488e6776b0eec1b575006d8e5240e"
+  end
+
   depends_on "rust" => :build
 
   def install
