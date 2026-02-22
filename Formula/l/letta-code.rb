f840512105d10833e204bb4c42590d07047f3a31
Rui Chen <rui@chenrui.dev>
2026-02-22 16:00:19 -0500

    letta-code 0.16.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/l/letta-code.rb
@@ -0,0 +1,21 @@
+class LettaCode < Formula
+  desc "Memory-first coding agent"
+  homepage "https://docs.letta.com/letta-code"
+  url "https://registry.npmjs.org/@letta-ai/letta-code/-/letta-code-0.16.3.tgz"
+  sha256 "12fc60eed05337b06075b37ca295458ea10206bd0f8a8359ecb473bbda408009"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/letta --version")
+
+    output = shell_output("#{bin}/letta --info")
+    assert_match "Locally pinned agents: (none)", output
+  end
+end
