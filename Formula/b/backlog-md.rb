1e504de95bb859351859d808941b41cc9b514878
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 17:19:01 +0000

    backlog-md: update 1.39.1 bottle.

--- Formula/b/backlog-md.rb
+++ Formula/b/backlog-md.rb
@@ -6,12 +6,12 @@ class BacklogMd < Formula
   license "MIT"
 
   bottle do
-    sha256                               arm64_tahoe:   "73fcc518681ec6435eafa8c80c4e30be7d1624e3235530b2c3c606c5f5e90766"
-    sha256                               arm64_sequoia: "73fcc518681ec6435eafa8c80c4e30be7d1624e3235530b2c3c606c5f5e90766"
-    sha256                               arm64_sonoma:  "73fcc518681ec6435eafa8c80c4e30be7d1624e3235530b2c3c606c5f5e90766"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8cfc0d6dceb808c7de87818fee15c3f96fb920fa49059eee3bf8123c4985bec6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ee1f1e09a658a1ac0584ae152aa181b579acc29a257be7ec51cb31a4a6354a7e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "539a1ee78a8e9a40e0c7fa46beed66b2c18fa80f1624ab0ec2b6dad4de5f282f"
+    sha256                               arm64_tahoe:   "c3dab6481ba197e1526af67ee8d4a88b0e1717cba6fac205c54e1d2c5e6a2772"
+    sha256                               arm64_sequoia: "c3dab6481ba197e1526af67ee8d4a88b0e1717cba6fac205c54e1d2c5e6a2772"
+    sha256                               arm64_sonoma:  "c3dab6481ba197e1526af67ee8d4a88b0e1717cba6fac205c54e1d2c5e6a2772"
+    sha256 cellar: :any_skip_relocation, sonoma:        "adbdec723ba7f76ecb8a5248f8bb41cdff60e0fa5ae6c47c965aa5ef93485d94"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "fd920e2e882bda0a5874f8f5a4dbb6f005efdae3543897df826546fa81c10030"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8db3e1086925fed6fce549496b435e2ec82194b46d31b8c6dac6afbd4d46e617"
   end
 
   depends_on "node"
