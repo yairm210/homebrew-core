9509672e843dc6793a4906e024a8463a32c64f05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-23 08:12:26 +0000

    cronboard 0.4.3

--- Formula/c/cronboard.rb
+++ Formula/c/cronboard.rb
@@ -3,8 +3,8 @@ class Cronboard < Formula
 
   desc "Terminal-based dashboard for managing cron jobs locally and on servers"
   homepage "https://github.com/antoniorodr/cronboard"
-  url "https://github.com/antoniorodr/cronboard/archive/refs/tags/v0.4.2.tar.gz"
-  sha256 "c4b34e107488a02df21c6447062f452bb5f987866d721fd9e229cee3f49df079"
+  url "https://github.com/antoniorodr/cronboard/archive/refs/tags/v0.4.3.tar.gz"
+  sha256 "e2a05bea70a70f78f8a4a7023ed1f68ee456a129b31f0e86f8677bd0fe75103e"
   license "MIT"
 
   bottle do
@@ -194,8 +194,8 @@ class Cronboard < Formula
   end
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/74/99/a4cab2acbb884f80e558b0771e97e21e939c5dfb460f488d19df485e8298/rich-14.3.2.tar.gz"
-    sha256 "e712f11c1a562a11843306f5ed999475f09ac31ffb64281f73ab29ffdda8b3b8"
+    url "https://files.pythonhosted.org/packages/b3/c6/f3b320c27991c46f43ee9d856302c70dc2d0fb2dba4842ff739d5f46b393/rich-14.3.3.tar.gz"
+    sha256 "b8daa0b9e4eef54dd8cf7c86c03713f53241884e814f4e2f5fb342fe520f639b"
   end
 
   resource "six" do
