c333001b5c4f1e3f970e3075191e569ab7ce9635
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-23 21:42:05 +0000

    depot: update 2.101.10 bottle.

--- Formula/d/depot.rb
+++ Formula/d/depot.rb
@@ -14,12 +14,12 @@ class Depot < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "49d81e245eb83aac4d3e8f0a59aad2611c4af131a02a55198df264b668b5ea9e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "49d81e245eb83aac4d3e8f0a59aad2611c4af131a02a55198df264b668b5ea9e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "49d81e245eb83aac4d3e8f0a59aad2611c4af131a02a55198df264b668b5ea9e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7dbf8d50058a95b93ce2dd7a4ac49a453fa30f492e39fb6fd7043589c35fcde1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f28113b4fb335d13c3c733269ceb1e2a52ce78dc3d9f515ad4a7b74f327d8ae2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ee7f0c22bec6c53d3a7dc9fa9bc49442f8cf88a312bb411399a17c150071af17"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8f71fac4440820912303af08ed60eb0f9cbd5a7da9bc7b21f1b364dba71f87ce"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8f71fac4440820912303af08ed60eb0f9cbd5a7da9bc7b21f1b364dba71f87ce"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8f71fac4440820912303af08ed60eb0f9cbd5a7da9bc7b21f1b364dba71f87ce"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f79ececfbec976b73274e00701a35fd40300aa3f8f66994e40ddbea876a8879f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "af5571d672a354d176dcfa74eb7f931be47c4fdcf8c23f5f04ee78461527d8c1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "14bd1e8ea320e26369ecc7ce9f73401adfae925cd83a90fe55eb463c0f4f10a6"
   end
 
   depends_on "go" => :build
