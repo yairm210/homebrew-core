d740c3168f905473a32e2141bf38026202a9cd20
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-23 23:50:41 +0000

    ohdear-cli 5.0.0

--- Formula/o/ohdear-cli.rb
+++ Formula/o/ohdear-cli.rb
@@ -1,8 +1,8 @@
 class OhdearCli < Formula
   desc "Tool to manage your Oh Dear sites"
   homepage "https://github.com/ohdearapp/ohdear-cli"
-  url "https://github.com/ohdearapp/ohdear-cli/releases/download/v4.5.2/ohdear.phar"
-  sha256 "fa2e0e84d47adefea1a3f5e23274c245285957208e33d2e143f0ddb617b60112"
+  url "https://github.com/ohdearapp/ohdear-cli/releases/download/v5.0.0/ohdear.phar"
+  sha256 "3a20dba5890edeebb09a62addf654fc41180bcea525fc8eb82802f01ea33870e"
   license "MIT"
 
   bottle do
@@ -19,6 +19,6 @@ class OhdearCli < Formula
   end
 
   test do
-    assert_match "Unauthorised", shell_output("#{bin}/ohdear me", 1)
+    assert_match "Your API token is invalid or expired.", shell_output("#{bin}/ohdear get-me", 1)
   end
 end
