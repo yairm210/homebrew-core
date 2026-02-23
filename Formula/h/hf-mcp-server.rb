d26429ddd57e678ccf761e682d9caf84242eefe9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-23 21:42:48 +0000

    hf-mcp-server: update 0.3.4 bottle.

--- Formula/h/hf-mcp-server.rb
+++ Formula/h/hf-mcp-server.rb
@@ -6,12 +6,12 @@ class HfMcpServer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "864d9f0acbc0ce0fce001a6fe821428775bf7466b6efb38368ebd231f293bfd1"
-    sha256 cellar: :any,                 arm64_sequoia: "4be03d9c60265676b569d9e22813273706f08ad435f752815c7a4c7f1a4db3fa"
-    sha256 cellar: :any,                 arm64_sonoma:  "4be03d9c60265676b569d9e22813273706f08ad435f752815c7a4c7f1a4db3fa"
-    sha256 cellar: :any,                 sonoma:        "a257f545e722dd0f1d5b975d7c6f24d0e00c402d0392308cc96339facdb39f1c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "96121b0ffbb24f77ced29f06fb3089b7d95d34990d6def212c74716bf5950487"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9602340a5dd9ac81ffa5f18ceba915b2b288754816319d471baee6b1e521361c"
+    sha256 cellar: :any,                 arm64_tahoe:   "499e3453d48921c8d059420e09b7ae45bcf56b85ef5ee91284e04f0bbb87a848"
+    sha256 cellar: :any,                 arm64_sequoia: "389b69b265594f6a5992efd41051e95230c74e90d17990bff0848a153ac0f188"
+    sha256 cellar: :any,                 arm64_sonoma:  "389b69b265594f6a5992efd41051e95230c74e90d17990bff0848a153ac0f188"
+    sha256 cellar: :any,                 sonoma:        "1d5a97ede99656023e396b907dbb815e68fba72ded2636b62efe796688dade26"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ee2c1757c6ebcce03ed1c203a62933cb005d994f4e9a781e3ed22e28b6fecfa1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "70a4fb3fcfccb80f3d2ea255e460feab3d87a3e46fa164630b5248dd9fd75c90"
   end
 
   depends_on "node"
