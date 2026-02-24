a19be31217c69ef54b8d6ca020794c33c6568b1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-24 08:43:38 +0000

    protoc-gen-js 4.0.2
    protoc-gen-js: fix 4.0.2 build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/protoc-gen-js.rb
+++ Formula/p/protoc-gen-js.rb
@@ -1,10 +1,9 @@
 class ProtocGenJs < Formula
   desc "Protocol buffers JavaScript generator plugin"
   homepage "https://github.com/protocolbuffers/protobuf-javascript"
-  url "https://github.com/protocolbuffers/protobuf-javascript/archive/refs/tags/v4.0.1.tar.gz"
-  sha256 "123fac2e86109b24e80ccd356aa914e268bf5863ad1354d224d6ceaed6f5c45b"
+  url "https://github.com/protocolbuffers/protobuf-javascript/archive/refs/tags/v4.0.2.tar.gz"
+  sha256 "a08244115ed0535971ec894abf078da90ad2c0938700612f90dc550f218627ee"
   license "BSD-3-Clause"
-  revision 2
   head "https://github.com/protocolbuffers/protobuf-javascript.git", branch: "main"
 
   bottle do
@@ -16,7 +15,6 @@ class ProtocGenJs < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ab4081275b0bafb529632cb95698ecdddf7f912b4a57135d561c6e6f421dd04"
   end
 
-  depends_on "node" => :build
   depends_on "pkgconf" => :build
   depends_on "abseil"
   depends_on "protobuf"
@@ -25,9 +23,11 @@ class ProtocGenJs < Formula
   # and Protobuf that get statically linked into binary. Check for any upstream changes at
   # https://github.com/protocolbuffers/protobuf-javascript/blob/main/generator/BUILD.bazel
   def install
-    system "node", "generate-version-header.js", "generator/version.h"
+    system ENV.cxx, "-std=c++17", "generator/generate-version-header.cc", "-o", "generate-version-header"
+    system "./generate-version-header", "package.json", "generator/version.h"
     protobuf_flags = Utils.safe_popen_read("pkgconf", "--cflags", "--libs", "protobuf").chomp.split.uniq
-    system ENV.cxx, "-std=c++17", *Dir["generator/*.cc"], "-o", "protoc-gen-js", "-I.", *protobuf_flags, "-lprotoc"
+    system ENV.cxx, "-std=c++17", "generator/js_generator.cc", "generator/protoc-gen-js.cc",
+                    "generator/well_known_types_embed.cc", "-o", "protoc-gen-js", "-I.", *protobuf_flags, "-lprotoc"
     bin.install "protoc-gen-js"
   end
 
