495d4f63856a57752852c0b0cd7ea782233ba8a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-24 23:56:20 +0000

    skillshare 0.16.0

--- Formula/s/skillshare.rb
+++ Formula/s/skillshare.rb
@@ -1,8 +1,8 @@
 class Skillshare < Formula
   desc "Sync skills across AI CLI tools"
   homepage "https://skillshare.runkids.cc"
-  url "https://github.com/runkids/skillshare/archive/refs/tags/v0.15.5.tar.gz"
-  sha256 "0dc7fdfb691a65c099231a40bb396f87a68914f5c352e1f2fb8ebd765495f2f1"
+  url "https://github.com/runkids/skillshare/archive/refs/tags/v0.16.0.tar.gz"
+  sha256 "433d9fbff7b80f51991daaba0d0375cad4ba2bbf07101106bc031da7b7e678fe"
   license "MIT"
   head "https://github.com/runkids/skillshare.git", branch: "main"
 
