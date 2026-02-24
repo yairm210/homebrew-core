d03f3b95d9c5ab0b296db127feff98991b36d909
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-24 15:18:32 +0000

    tach 0.33.4

--- Formula/t/tach.rb
+++ Formula/t/tach.rb
@@ -3,8 +3,8 @@ class Tach < Formula
 
   desc "Tool to enforce dependencies using modular architecture"
   homepage "https://docs.gauge.sh/getting-started/introduction"
-  url "https://files.pythonhosted.org/packages/ae/e4/26ce2c1e56f43e2062c6056ad334ba6578c9722cca8885e00ff0a0d98bc8/tach-0.33.3.tar.gz"
-  sha256 "e5c049cb1f677fcbaaef020fb8a9bff150b9a13914a326c7686d0ea3ed9ebcea"
+  url "https://files.pythonhosted.org/packages/cd/3d/eb9f0ec5e0c26ff0dbaaab4d2fc5849b3525f00876b672f8e696b3c8591e/tach-0.33.4.tar.gz"
+  sha256 "633f2f3c2516cfa5ee5169a230bb4383644167c35acba831f2a787b0be5d05ea"
   license "MIT"
   head "https://github.com/gauge-sh/tach.git", branch: "main"
 
@@ -72,8 +72,8 @@ class Tach < Formula
   end
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/74/99/a4cab2acbb884f80e558b0771e97e21e939c5dfb460f488d19df485e8298/rich-14.3.2.tar.gz"
-    sha256 "e712f11c1a562a11843306f5ed999475f09ac31ffb64281f73ab29ffdda8b3b8"
+    url "https://files.pythonhosted.org/packages/b3/c6/f3b320c27991c46f43ee9d856302c70dc2d0fb2dba4842ff739d5f46b393/rich-14.3.3.tar.gz"
+    sha256 "b8daa0b9e4eef54dd8cf7c86c03713f53241884e814f4e2f5fb342fe520f639b"
   end
 
   resource "smmap" do
