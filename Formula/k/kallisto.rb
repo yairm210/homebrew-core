6dc879560e32735ead8305c7f22e0c925b97f730
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-25 17:34:08 +0000

    kallisto 0.52.0

--- Formula/k/kallisto.rb
+++ Formula/k/kallisto.rb
@@ -1,10 +1,9 @@
 class Kallisto < Formula
   desc "Quantify abundances of transcripts from RNA-Seq data"
   homepage "https://pachterlab.github.io/kallisto/"
-  url "https://github.com/pachterlab/kallisto/archive/refs/tags/v0.51.1.tar.gz"
-  sha256 "a8bcc23bca6ac758f15e30bb77e9e169e628beff2da3be2e34a53e1d42253516"
+  url "https://github.com/pachterlab/kallisto/archive/refs/tags/v0.52.0.tar.gz"
+  sha256 "68184e41706d77e409f05a598a87dacdf3cf227f18c028175e2bce8b284bdea4"
   license "BSD-2-Clause"
-  revision 2
 
   bottle do
     rebuild 1
@@ -23,12 +22,16 @@ class Kallisto < Formula
     depends_on "zlib-ng-compat"
   end
 
-  # Fix compilation error in Bifrost
-  # https://github.com/pachterlab/kallisto/issues/488
+  # PR ref: https://github.com/pachterlab/kallisto/pull/506
+  # Fix error: no member named 'sz_link' in 'DataStorage<Unitig_data_t>'
   patch do
-    url "https://github.com/pmelsted/bifrost/commit/d228b532a2cd5d3a598092ea4c46d1e997e50737.patch?full_index=1"
-    sha256 "75ae03575fc1ab9826504ec297d2b424fe779dae6e9edebdeaf78a8a8b4e55cd"
-    directory "ext/bifrost"
+    url "https://github.com/pachterlab/kallisto/commit/a5caefb608611c48e102d63e91eafd3660d3e569.patch?full_index=1"
+    sha256 "e29be49cc52a18f78b13b381f2d97cdf047ea45ba6b61b94caa54d46e195d0e2"
+  end
+  # Fix missing hdf5 libraries
+  patch do
+    url "https://github.com/pachterlab/kallisto/commit/e79245b1386d984849f2274fd2287a85682991bc.patch?full_index=1"
+    sha256 "0a2e28de1bbe247842f5f6082a00ac60158ffe3d3264b639131d16b9c8c2e1a5"
   end
 
   def install
