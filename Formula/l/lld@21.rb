8589b3df67cb61d6597214a1fc36f9396d301ed1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-25 20:56:30 +0000

    lld@21: add 21.1.8 bottle.

--- Formula/l/lld@21.rb
+++ Formula/l/lld@21.rb
@@ -10,6 +10,15 @@ class LldAT21 < Formula
     formula "llvm@21"
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "adbc47c9a735cd5c28c485a7bedbf9bef7af2652a6d89d1e8f78dab9367625c6"
+    sha256 cellar: :any,                 arm64_sequoia: "12509c3c0027577824b6d98a5e4265c4016f6f311b47975cb4d39ae7373620bf"
+    sha256 cellar: :any,                 arm64_sonoma:  "62db801951b3822a19dda273b12317654b03bfe56b9113f50cc4f69331a4ea06"
+    sha256 cellar: :any,                 sonoma:        "15fa1536fec1ead5d2f4e29b3ccc947dcba8285277bc00a6f312986e147b4535"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "020c5bb5b9a9d50f5aa1a38a6357f10b0d44f7ad4dc82f9fab3ae5c5fec54e6f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f28a6087b19e81177061fd9fe7dfb181216863e145731ebade85cfa37e9001bb"
+  end
+
   keg_only :versioned_formula
 
   depends_on "cmake" => :build
