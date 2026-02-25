18fb8b2e34107b1dea11c42422ae8b0657780089
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-25 09:23:32 +0000

    acronym: update 2.0.0_2 bottle.

--- Formula/a/acronym.rb
+++ Formula/a/acronym.rb
@@ -9,12 +9,12 @@ class Acronym < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ebb84ad2d1fae9d6de117408556b7990be205a17c751b9f665ebcd7bbeb43596"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1d028c58a59292034bc12757ea1755a041155133b453b246adc7f2cff3a8484b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3b331b39630d93ec486052914a2459c891610c39fee753b1896e6bfc9e0e2e4d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f82a512325d5810308944be03a4362bad0d7fa7ee609d77f05da52c5fc74174c"
-    sha256                               arm64_linux:   "f915549a828760078f642cb4547f6b954e7d566723e239bba8747cd38c89884b"
-    sha256                               x86_64_linux:  "b33a6e05b8991f839588dfcc98e9a5834a5d5f7c310050676a767684a5b8d744"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fe9d58a50861621cc1e5cc93adbe8079337051eed85a84a19b1890e34f627b3d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3248848b1b141ad55b49c0b932db4e95f74a6006087b02c1e6aba614e03ed66f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1c14f87e7d22b78a0acdfd37325733325242dc5f46f998d752491e89e9b4da53"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e58f08df88a9d4153f9fb31ea0b7d016935b5f65f56f16c60373cb4ca0c15fa4"
+    sha256                               arm64_linux:   "d6d9686539b292e0e0b80e80bb7431c8f60a3a769cf0deb0029ea50e29bf9426"
+    sha256                               x86_64_linux:  "e7a7706a8da238c0a30682d3c65565157ceb6d6f30b54792753ccc3aae0015b8"
   end
 
   depends_on "cmake" => :build
