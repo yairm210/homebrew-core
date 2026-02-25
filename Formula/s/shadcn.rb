99659ed57b1558e59e8f500d371d317a7b40973e
Leon Adomaitis <leon@adomaitis.de>
2026-02-25 02:45:18 +0100

    shadcn 3.8.5 (new formula)

---
+++ Formula/s/shadcn.rb
@@ -0,0 +1,22 @@
+class Shadcn < Formula
+  desc "CLI for adding components to your project"
+  homepage "https://ui.shadcn.com"
+  url "https://registry.npmjs.org/shadcn/-/shadcn-3.8.5.tgz"
+  sha256 "24b7af646c5afa01c7604dfe8bf3f28090594c7aef3ae5cdfc678ce040b5a55f"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/shadcn --version")
+
+    pipe_output = pipe_output("#{bin}/shadcn init -d 2>&1", "brew\n")
+    assert_match "Project initialization completed.", pipe_output
+    assert_path_exists "#{testpath}/brew/components.json"
+  end
+end
