34c5fe7c226c266355748e07e4d21a92aba52c56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-26 02:55:10 +0000

    fastly 14.0.3

--- Formula/f/fastly.rb
+++ Formula/f/fastly.rb
@@ -1,8 +1,8 @@
 class Fastly < Formula
   desc "Build, deploy and configure Fastly services"
   homepage "https://www.fastly.com/documentation/reference/cli/"
-  url "https://github.com/fastly/cli/archive/refs/tags/v14.0.2.tar.gz"
-  sha256 "5ddee8ef032d238ad0614a65f64b4af6e0f374cc4f311a4212f25f01288cdf01"
+  url "https://github.com/fastly/cli/archive/refs/tags/v14.0.3.tar.gz"
+  sha256 "382f6b55527bb0a7b9538a1e9ccac552de2419317538ab17be8b4618736eaa49"
   license "Apache-2.0"
   head "https://github.com/fastly/cli.git", branch: "main"
 
