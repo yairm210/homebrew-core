e0c3f1408dfc6f04bceb6b5a94e04af0d2a9c35e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-26 08:30:11 +0000

    asc: update 0.34.1 bottle.

--- Formula/a/asc.rb
+++ Formula/a/asc.rb
@@ -7,12 +7,12 @@ class Asc < Formula
   head "https://github.com/rudrankriyam/App-Store-Connect-CLI.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "732f1fc919718f3fef6c652b0068a49d4b436bdb0fb507da58dd2d222e2832ed"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "19dee228c40a695a04d53d165978685e1f8e0e83c7c443bb9c5da0c156522e10"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d8d7208b5cedc173a178c6645b257baa8d24dd9869a4187b00edc3ab79debdc"
-    sha256 cellar: :any_skip_relocation, sonoma:        "588ab08ffba9fddb9c15831d4ba495bbff7f7c4750211a586b30a9017a99d5b0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f0e0ce32804fb3348c84f641195e59a0f134701601c55794be61e91b8229de3a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "523bb4d5ad063f32cb4be3dc3287d0a499fda99e2fda09bf3e968a8cb8f7a8c4"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eafdb8249958e8eb66ba534a35f588444d4e6cfc24358d7734b62413f3f0d430"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dae51773af67b3364858def7928310750b2854c2aad2110953da7b1b2df10869"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3765afa72491d5986bc7473cd4c29e7a75653c979754ce9d749b38e27a9f4938"
+    sha256 cellar: :any_skip_relocation, sonoma:        "09632b2ca631ec5e61541cd3398d2d95b61428edf23b2341037c6de479cd298d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "67727ac87718074e5f59ad5d2af5e536761d2663793762798e87228cccb2b2e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "299db127a0ca1f82767f82dffe6ffab7abb4d27b1141451f96e04c25d8557e81"
   end
 
   depends_on "go" => :build
