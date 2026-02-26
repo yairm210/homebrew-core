534e8f8fd3be7765dda9b9c849e2ced97c7388ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-26 08:39:31 +0000

    koji: update 3.4.0 bottle.

--- Formula/k/koji.rb
+++ Formula/k/koji.rb
@@ -7,13 +7,12 @@ class Koji < Formula
   head "https://github.com/cococonscious/koji.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "2c2e65370ee4b66b797d400a6435446d2e23865f30f17d5b043981827071fe03"
-    sha256 cellar: :any,                 arm64_sequoia: "9dc61a06ca85e589e22edb4f49138080b8c465b4eb07fd786f570bfbe67484dc"
-    sha256 cellar: :any,                 arm64_sonoma:  "92223e83af0921779e26588f9a01b5ce030ef31dd5f0646a9fa25f8716b61541"
-    sha256 cellar: :any,                 sonoma:        "d48affcaca82ec955947353707efecb5c1587dbf2178c2f3e309637f6b2228fc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3222f067b938c91e5be8d5bf9f5f5a17689e46bddbf43be19eaee6fe498b7e9f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4d4ac52c1cf5615952acd1d35bd1b70060812a0f91ca55a04714e9674efc73b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4cc761a36b277b402f160acd6769d7d87e78a073d1238f70604d31480461e688"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6e1ab07ddde1a07445a7de66e374a75c6f10dcf3e02c1d32d606f10832b8c244"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ea25b8a31acdbc887b187dc57401bc0edaa2ac7268352f18f968d46651926988"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0f307d1adfa82da461d9e6823baed76753e7fe209720451037dfecd66fb99ef6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "43ab9f19dba5cac126033a2b3744090f954e40ba988436350691d5340d703ebf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "830f415428a6b7ae48b18baea0a4c4f0e846f23175712271baefa0353fee8741"
   end
 
   depends_on "pkgconf" => :build
