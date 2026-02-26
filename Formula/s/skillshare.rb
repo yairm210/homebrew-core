10c38eeb753aed13e270007a3f3a519d820a1e1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-26 15:29:15 +0000

    skillshare: update 0.16.2 bottle.

--- Formula/s/skillshare.rb
+++ Formula/s/skillshare.rb
@@ -7,12 +7,12 @@ class Skillshare < Formula
   head "https://github.com/runkids/skillshare.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6651e1a7d9b6a17de29a365394561d35c36246f53f02a5a7e00a8dcefb096b28"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6651e1a7d9b6a17de29a365394561d35c36246f53f02a5a7e00a8dcefb096b28"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6651e1a7d9b6a17de29a365394561d35c36246f53f02a5a7e00a8dcefb096b28"
-    sha256 cellar: :any_skip_relocation, sonoma:        "edef855cbf03353d5f44ba614f9526b6382e12f54b36bd8606bbfc8186716e63"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d6dce7ffda46879d145b1e00a38159f1f02ab3a8bec9cc9929767a07af683346"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "61a72c08f3895991335281480fa5dcedf3490f2768620f78c56fa6b41798a0b0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "47900bb3ef1e87ebcfb10a30e5d6f1972d2d33d8043416d1a86e31aec4ac2dde"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "47900bb3ef1e87ebcfb10a30e5d6f1972d2d33d8043416d1a86e31aec4ac2dde"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "47900bb3ef1e87ebcfb10a30e5d6f1972d2d33d8043416d1a86e31aec4ac2dde"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e8eb4e9e784af1ca9591c4a64c3a617162a8acc714839afc95946e3ce64dafc5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8760ab73266878e607e0c9f40d0a1ef1d359475f6cdf0ad9e16dc7b260abb88a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "feeaf1f4fdf172feeddaa26200c4d335673062d9002fb52a382aa809cf1e3a4d"
   end
 
   depends_on "go" => :build
