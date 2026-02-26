973fa97f4ec89da9e51d498ced94a0b5450698c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-26 20:08:03 +0000

    betterleaks: add 1.0.1 bottle.

--- Formula/b/betterleaks.rb
+++ Formula/b/betterleaks.rb
@@ -6,6 +6,15 @@ class Betterleaks < Formula
   license "MIT"
   head "https://github.com/betterleaks/betterleaks.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5fc277bb76141f3f9f08209a3378e02644825be13082b8fe7c6c737dcd139841"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5fc277bb76141f3f9f08209a3378e02644825be13082b8fe7c6c737dcd139841"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5fc277bb76141f3f9f08209a3378e02644825be13082b8fe7c6c737dcd139841"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6f0b5c92f94a99df2f90d45385f74a3c1336f07ae0fd49c7994ef39c10a2b0a7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7078bdf6ded471facde22152dd5f93da3e46b2f90a154ef6928f3bb4db989403"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ee7e19a41e5ef6b2dbbce40a7213db3b20d5d3d9dc79a2821fc7ad69a6116ecd"
+  end
+
   depends_on "go" => :build
 
   def install
