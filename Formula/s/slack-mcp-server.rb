a80de39583fd013aa260e8b286969aa0f055d09b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-26 05:19:45 +0000

    slack-mcp-server: update 1.2.2 bottle.

--- Formula/s/slack-mcp-server.rb
+++ Formula/s/slack-mcp-server.rb
@@ -7,12 +7,12 @@ class SlackMcpServer < Formula
   head "https://github.com/korotovsky/slack-mcp-server.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "78a87170e1df64c8b57a289c9fa2d69163f812ae37ce0927c409c9c3969ec81b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78a87170e1df64c8b57a289c9fa2d69163f812ae37ce0927c409c9c3969ec81b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "78a87170e1df64c8b57a289c9fa2d69163f812ae37ce0927c409c9c3969ec81b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "779bcdae54a60e8b029f5d2d08c5bbf79677064f73e3257a28e5a56669143f72"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dd9f99985af803e963d4e0bf1d066df16827429eb8918513ec57b7d9e3e51998"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e661e69d4fd69416c4854cf539f12006e1074d204e4b917688c399c112a5ce3e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0bb4eaa736ce2d4e036907b0e0c29e5106ddd8f96e3b078815348745bf448332"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0bb4eaa736ce2d4e036907b0e0c29e5106ddd8f96e3b078815348745bf448332"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0bb4eaa736ce2d4e036907b0e0c29e5106ddd8f96e3b078815348745bf448332"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e424325e147a2bb97d335e93ebfc653e25326b3096ed4c8653addc821909a1c9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3240e93fae2b81f8b7f52769663eee2b32f5e93364e30c1403697e08e01b7b27"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32a60e7a7e1d7aaa740f2b9922219971747f910e78d4413427e800ff66b91b3a"
   end
 
   depends_on "go" => :build
