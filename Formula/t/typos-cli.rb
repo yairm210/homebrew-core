8fd659b949e9d95c5f292a6cd26ebc50002e6e1f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-27 18:10:51 +0000

    typos-cli: update 1.44.0 bottle.

--- Formula/t/typos-cli.rb
+++ Formula/t/typos-cli.rb
@@ -11,12 +11,12 @@ class TyposCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8f9879f96580ccfef6199b7e8b9f63cc5274d3bb38f089706c029553b0661daa"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a87775ca10397a20177f7bf6c6f7224383571ad30a8653d934d1c7c13a09caa4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "547a4a8e3cfed581ddd8b1055217d14e2167aebcb175a63640d8e33566e246dd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "42c53ec9e2057494c24e5661076e6bd605546f3ecd11c9bb0b26095823321299"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "91b006878bae307d3ed704c3ce5959bdd39515d7293b314276df29a55ecb6867"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "64316cd207db2ae1fe36269d157c7d2cb78a8dc2ac3e5a6aa38497011dea168f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7b332c562e8b6ccb65a15e61707d8afe74d1329cfeff2cbc148f3320d626339d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "03fd45712216997f4466d43d12b8c70375cb566d36dbd9c72140c476d77cee62"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "abf5476c9151ec2d2e084c07e6c61ad27648f89603459e3cb6745008347e193a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "856f4f8cde97dc92d21a2c3f43b8cec86047173d5886a5285c5de9702c741417"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c1011959fe0ebc9d5603d5c4d0e60a61b030deae36b42ce0b87e53b63a3160a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1f912d6bd2965183f19ebbbccd40c87bc25f2097586cbe6c6f697301bf869e89"
   end
 
   depends_on "rust" => :build
