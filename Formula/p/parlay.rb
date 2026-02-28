eab7d833d844ac866fa61a53c345a814cf6c56c6
Thierry Moisan <thierry.moisan@gmail.com>
2026-02-28 09:01:34 -0500

    parlay: add shell completions

--- Formula/p/parlay.rb
+++ Formula/p/parlay.rb
@@ -20,6 +20,8 @@ class Parlay < Formula
   def install
     ldflags = "-s -w -X github.com/snyk/parlay/internal/commands.version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"parlay", shell_parameter_format: :cobra)
   end
 
   test do
