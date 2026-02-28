86e64016a801af238fe7fea79a5b405a984217eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-28 02:29:02 +0000

    protobuf@33: add 33.5 bottle.

--- Formula/p/protobuf@33.rb
+++ Formula/p/protobuf@33.rb
@@ -10,6 +10,15 @@ class ProtobufAT33 < Formula
     regex(/^v?(33(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any, arm64_tahoe:   "253be052458f64ff0e668bf07337288fc2de74423b82aa3f3213ec93664917d7"
+    sha256 cellar: :any, arm64_sequoia: "f6723219c9e3242d7c03fcf3e71b52206d49afe52f9de95c6c92a2199667c917"
+    sha256 cellar: :any, arm64_sonoma:  "c495e42cc1e2484c5b4d46af4b945892f279439ad955b7c1cd801b2d1bbb70b0"
+    sha256 cellar: :any, sonoma:        "eff25cf0e790a8f7ff30cecef656a3879b86c17e51df9825dca5ebde31ed03d2"
+    sha256               arm64_linux:   "8d947603561a14be3f947f16fd726cf143dbc2d410d6fdf308af6c1fdec67d0e"
+    sha256               x86_64_linux:  "a8d3efbaf407a35e6155876b71a89bdb4078864f6c3f6fbbc13a1d9993d70293"
+  end
+
   keg_only :versioned_formula
 
   # Support for protoc 33.x (protobuf C++ 6.33.x) will end on 2027-03-31
