ed155327f0591ef0f1110cda90efee42c41cf552
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-28 14:16:06 +0000

    lume 0.2.85

--- Formula/l/lume.rb
+++ Formula/l/lume.rb
@@ -1,8 +1,8 @@
 class Lume < Formula
   desc "Create and manage Apple Silicon-native virtual machines"
   homepage "https://github.com/trycua/cua"
-  url "https://github.com/trycua/cua/archive/refs/tags/lume-v0.2.84.tar.gz"
-  sha256 "d889792db5de5840258784c912a0e6810e9834b8289c6c5c086d1ea7dba98191"
+  url "https://github.com/trycua/cua/archive/refs/tags/lume-v0.2.85.tar.gz"
+  sha256 "2671acc92d7808e81e2bb0e86f6e32fc1bebb443fb45a5773beedcdadd8a9e63"
   license "MIT"
   head "https://github.com/trycua/cua.git", branch: "main"
 
