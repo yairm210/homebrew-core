858a08d56966e12225b7c307b164fa16bc006010
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-28 08:22:51 +0000

    sing-box 1.13.0

--- Formula/s/sing-box.rb
+++ Formula/s/sing-box.rb
@@ -1,8 +1,8 @@
 class SingBox < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org"
-  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.12.23.tar.gz"
-  sha256 "58b41bd8d6255c2ddc2ae5eace53171e550c9debaa8e16d89871299055b58b1a"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.13.0.tar.gz"
+  sha256 "6ddc71596dc937873c5aba15a4f2b395c5434265efdc1bd21f4c03d8c5b7f641"
   license "GPL-3.0-or-later"
   head "https://github.com/SagerNet/sing-box.git", branch: "dev-next"
 
