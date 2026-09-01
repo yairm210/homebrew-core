627b329f425643071c5730126fd9d2144be92a35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 17:54:28 +0000

    sabiql: add 3.0.0 bottle.

--- Formula/s/sabiql.rb
+++ Formula/s/sabiql.rb
@@ -5,6 +5,14 @@ class Sabiql < Formula
   sha256 "fa1a8edf3ec0b653d56c802a155dbb4b30be0f31f42b72b26f4ea1898dfa488a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8f81bd1a25632e8a702561833d39d0cb9d5fb54d293840be5bf9e7fca02ce1a7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7644b246b0652401c9932b924d0082afab598f5f7f8fcd476701f934635b4f4e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0c63aed69ad9accb5bc652db7d203ed23e0b9559f1776e2256a3267f67a94cb7"
+    sha256 cellar: :any,                 arm64_linux:   "85b2bf5700ad27f351587ea4903e7f3e3a609388077cf8fdaab2a35a38647659"
+    sha256 cellar: :any,                 x86_64_linux:  "82137759c7da8f9a7849efac2b19d76f1856682de159c9b0bad9230d28ae770e"
+  end
+
   depends_on "rust" => :build
   depends_on "graphviz"
 
