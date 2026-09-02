8edab32fd07cf39b1a1f1d5ea410e39c769af483
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-02 21:05:05 +0000

    zenith: update 0.15.1 bottle.

--- Formula/z/zenith.rb
+++ Formula/z/zenith.rb
@@ -13,12 +13,11 @@ class Zenith < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b7d5ef787ca0afe7d120eebf5ba5c694ec859968da494518ad999c2a28e1f5df"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b0c3ca47e1bb97224fb57defc71e9d08b6c5583fb9d44089b25e4848ac425af0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "50dacb91e7dd564d48126fd0d2812f62763c65f6ecfaa50b457033571e4c1aa8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c4fffebc7dd48c5f91beb8654388e3a47fb4833d3e235940011d6a77bbcd6e87"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b9752c5f88c7ca600c27476b08fcbfc7b4683c4331b3e7ebdc4c03c88c74d082"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a18a32e1d8f7c17b510fe207097779f64742597941b769daadd4404dd654e1d5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "448ce4fc97b127ff83fb720d14138cbd8cdf08a7b8b7a8442921ee0812ad2be9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "90fd0ebd769a609dcef73a6b33dbec3db75021e635031de00f46d57a0c504cc5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "75339d1afa6a66b06965cfcb9a33c75961ab5634a6adb3d2933ec5be2010f6c0"
+    sha256 cellar: :any,                 arm64_linux:   "f873db30d1df896ae03617e4c88686624e9cdf9b9a4e6ce21e176691aef8c87a"
+    sha256 cellar: :any,                 x86_64_linux:  "6b27201865e9774bfcb3f91122fe462ea4ff63fc0658121d952ec5ea981db79c"
   end
 
   depends_on "rust" => :build
