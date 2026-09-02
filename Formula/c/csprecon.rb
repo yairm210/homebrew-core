9cf0d399ce0a4ed98782df81a0f69a016cf27a5d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-02 11:48:18 +0000

    csprecon: update 0.4.7 bottle.

--- Formula/c/csprecon.rb
+++ Formula/c/csprecon.rb
@@ -7,12 +7,11 @@ class Csprecon < Formula
   head "https://github.com/edoardottt/csprecon.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b5c5d071902795993ac9ac7c907c9aa1e5df57f98e41fde922ce7a4df5cf28e6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b5c5d071902795993ac9ac7c907c9aa1e5df57f98e41fde922ce7a4df5cf28e6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b5c5d071902795993ac9ac7c907c9aa1e5df57f98e41fde922ce7a4df5cf28e6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "caa5d34e826837d065db28302463a92d9bd0ca678dcec9b8cd34f745cde84a67"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e69bbbc30582405a0881769b8f280bb3f4ceef96f2f8810e5ed8c5150a9627d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "acee474f9bc3d53e0229b40feacfdd11c988a95c53ffa0bfa1341a044767d14c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e5ba92dfd06ad1e654c7c67d6247ca151d8eac8339da43e40cdf64297e4fdc0d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dc2137259f0cf7ef65d465d9cab3317b7971e0bb888031c5f26ef2123a8a2ffb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b50c09be1d471d1ca93b698df46bffe9eec0acdabfd4c5f20b0f2e4da5527e67"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ad668b02c5b5c5c906fdad9b55c2d98fb29a77c339ca583fe9df2d9ad680c96d"
+    sha256 cellar: :any,                 x86_64_linux:  "d4db4557167efb9404980eea98b53ad21f038eee6901a977115a8c9a0daeddf3"
   end
 
   depends_on "go" => :build
