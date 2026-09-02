76cf91654161ca9e82938e129662cf7ab00d06e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-02 18:05:14 +0000

    vacuum 0.30.2

--- Formula/v/vacuum.rb
+++ Formula/v/vacuum.rb
@@ -1,8 +1,8 @@
 class Vacuum < Formula
   desc "World's fastest OpenAPI & Swagger linter"
   homepage "https://quobix.com/vacuum/"
-  url "https://github.com/daveshanley/vacuum/archive/refs/tags/v0.30.1.tar.gz"
-  sha256 "183c732d48d8156508a13169185caa7dd1103f71e9cedc479c161cb60e2ea240"
+  url "https://github.com/daveshanley/vacuum/archive/refs/tags/v0.30.2.tar.gz"
+  sha256 "fb8c0f1504dafd3e13641e124bee1dbc58088684d02e5d0dfada15856561988e"
   license "MIT"
   head "https://github.com/daveshanley/vacuum.git", branch: "main"
 
