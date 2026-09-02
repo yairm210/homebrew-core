768ffea7508626ecc39433a549c37ec3950128a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-02 14:39:58 +0000

    nginx 1.31.5

--- Formula/n/nginx.rb
+++ Formula/n/nginx.rb
@@ -3,10 +3,10 @@ class Nginx < Formula
   homepage "https://nginx.org/"
   # Use "mainline" releases only (odd minor version number), not "stable"
   # See https://www.nginx.com/blog/nginx-1-12-1-13-released/ for why
-  url "https://nginx.org/download/nginx-1.31.4.tar.gz"
-  sha256 "e6f20b644a17a643f059ae6467a1971fe2811587d025e071068753a1f1e3b3c3"
+  url "https://nginx.org/download/nginx-1.31.5.tar.gz"
+  sha256 "e951607d534836624bd36b6b45a71dbfb055237deae3738da6bbf3270dada279"
   license "BSD-2-Clause"
-  compatibility_version 9
+  compatibility_version 10
   head "https://github.com/nginx/nginx.git", branch: "master"
 
   livecheck do
