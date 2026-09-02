98a982169fca24c8ce4536d91d391603e32da168
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-02 08:02:16 +0000

    dcd 0.17.1

--- Formula/d/dcd.rb
+++ Formula/d/dcd.rb
@@ -2,8 +2,8 @@ class Dcd < Formula
   desc "Auto-complete program for the D programming language"
   homepage "https://github.com/dlang-community/DCD"
   url "https://github.com/dlang-community/DCD.git",
-      tag:      "v0.17.0",
-      revision: "10be6833e8f0ed8d1fc541fd65a3b5ca67b2a158"
+      tag:      "v0.17.1",
+      revision: "b601fdd743bd7690efe479fd3f0dd674b66e6c52"
   license "GPL-3.0-or-later"
   head "https://github.com/dlang-community/dcd.git", branch: "master"
 
