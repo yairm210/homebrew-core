6857908b89acc2ad69ad5f61413a0ae0bcc26ac3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 18:06:52 +0000

    scala 3.9.0

--- Formula/s/scala.rb
+++ Formula/s/scala.rb
@@ -1,8 +1,8 @@
 class Scala < Formula
   desc "JVM-based programming language"
   homepage "https://scala-lang.org/"
-  url "https://github.com/scala/scala3/releases/download/3.8.4/scala3-3.8.4.tar.gz"
-  sha256 "cb2f9a358ec449eec417d63efd9b6fc6bd66a13b1347d49c25571eca284857d3"
+  url "https://github.com/scala/scala3/releases/download/3.9.0/scala3-3.9.0.tar.gz"
+  sha256 "8ecddee33ecda620256e4b744b928d3ad6de64653bd41e4b6a308992b3b6bb31"
   license "Apache-2.0"
 
   livecheck do
