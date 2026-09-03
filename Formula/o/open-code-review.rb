0d5689b9be4e4c69ee1b54d01b09d15b83682a18
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 06:28:24 +0000

    open-code-review: update 1.11.3 bottle.

--- Formula/o/open-code-review.rb
+++ Formula/o/open-code-review.rb
@@ -7,11 +7,11 @@ class OpenCodeReview < Formula
   head "https://github.com/alibaba/open-code-review.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a68dd02f4ecd03965af859a88d9231138444c7f61c938c8cddcdc67e4a504ae1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a68dd02f4ecd03965af859a88d9231138444c7f61c938c8cddcdc67e4a504ae1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a68dd02f4ecd03965af859a88d9231138444c7f61c938c8cddcdc67e4a504ae1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3ee8c6d6f9ebf86ab2d9c1158f9d2071f0bb167387a5ea498d2aca19048ef994"
-    sha256 cellar: :any,                 x86_64_linux:  "4ea4202fe2fcbb7699c9acf721edff5f76c298ea05c78d14d8e1c4e3116ed326"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2ad431571a5309e6c478b5286fbd1740ec805a4fec7b01060878f7e4e156e7a1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2ad431571a5309e6c478b5286fbd1740ec805a4fec7b01060878f7e4e156e7a1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ad431571a5309e6c478b5286fbd1740ec805a4fec7b01060878f7e4e156e7a1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0d9d6e8f1e42869ec7dcfdd428264a73e48605f016361b654acccdf4a46b2451"
+    sha256 cellar: :any,                 x86_64_linux:  "210c3357c7393a702379c28e65435c2d19c7ba15af6d8e1297642e17e1f5ca04"
   end
 
   depends_on "go" => :build
