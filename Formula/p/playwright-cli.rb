286d0ec19db6b8f41672daa026cc5351974409c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 00:08:38 +0000

    playwright-cli: update 0.1.19 bottle.

--- Formula/p/playwright-cli.rb
+++ Formula/p/playwright-cli.rb
@@ -6,12 +6,7 @@ class PlaywrightCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "99f69f0be29a043f05338b6e5da2b1fb94c49b83f12781a7af0f44e1a6db8ca8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "99f69f0be29a043f05338b6e5da2b1fb94c49b83f12781a7af0f44e1a6db8ca8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "99f69f0be29a043f05338b6e5da2b1fb94c49b83f12781a7af0f44e1a6db8ca8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "461c2fc73bd39557b263ef73e03ddce76639295656eaa19ead957124daf4b0bf"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1b17758e4add247ab7b9796d0796d5fa03429dfe11bf3d59144f77646ddce18d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b17758e4add247ab7b9796d0796d5fa03429dfe11bf3d59144f77646ddce18d"
+    sha256 cellar: :any_skip_relocation, all: "0418afa65ea41cf4eceaad8ec91ff1528fee9030e658d8dcc54891e0c3090137"
   end
 
   depends_on "node"
