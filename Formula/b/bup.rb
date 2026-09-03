788141fc018b448636a1901d736b4bffd0aea2eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 00:44:51 +0000

    bup: update 0.34 bottle.

--- Formula/b/bup.rb
+++ Formula/b/bup.rb
@@ -7,12 +7,11 @@ class Bup < Formula
   head "https://github.com/bup/bup.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "e8dd1764fdfdcb4e0a3be1c0187a5c9fae165c38130282620f4683d47bc4886f"
-    sha256 cellar: :any,                 arm64_sequoia: "9aad3e7860b09e030da32a5800505ebfe0b5fe7a647879db0cff40d919e9767f"
-    sha256 cellar: :any,                 arm64_sonoma:  "a4536482d2a0d6d13ec56e1574fb7a6913a561d4548ab7f3428cb289b2b28225"
-    sha256 cellar: :any,                 sonoma:        "a132b47d549095e472be6cb7703c0c8cf78f8850b0aa8dadc76210a00120e055"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1916cb6c7346701861a8d1206d68eaf8e19bd79ff1e93a9cd60f7c6de26b7a27"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1419ad06f33c6f879c55e6e40596f58ffd7d83a903c801433ee1f5b993adb5cd"
+    sha256 cellar: :any, arm64_tahoe:   "8f71679b731e4a7bfbcc382dd7f911d6f634ed37c9eee02143971226d2d451be"
+    sha256 cellar: :any, arm64_sequoia: "d8d437801b8ec655f19801fd9d83b80ff4f6fcd69a7d4de5b30bdb1c1d5b7e00"
+    sha256 cellar: :any, arm64_sonoma:  "1fcf0c6c6101d4e067e7718305a9d57c5f00824ae0e08a72091794b2062638d9"
+    sha256 cellar: :any, arm64_linux:   "0fe6a15cf51268dc23e3105263d170c93c23ff823b01911ac4c7e8c85bc295ec"
+    sha256 cellar: :any, x86_64_linux:  "6fe2954a65d5c41aa658850b16ee0cda1fc1c0b6b615a30f6414c10127f94bd2"
   end
 
   depends_on "pandoc" => :build
