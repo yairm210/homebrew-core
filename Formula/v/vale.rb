3a4e47061c2bc1a70c843d9d08d932eb53b0363f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 00:18:12 +0000

    vale: update 3.20.0 bottle.

--- Formula/v/vale.rb
+++ Formula/v/vale.rb
@@ -6,12 +6,11 @@ class Vale < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7b706086a93207b2ad2a4670d40e2084829fc1c0d2625bc2cac59205392fa8c6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7c75bb70fc7c5e72902dd30fb8e3e6007ce5053bed39b74dbdf0655147145b86"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "af699b4dc2bb3fc25b8ebc5d08280a834403e0002d66235a15bddb3d4b99e1b0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b4cb6efc240990eede6e94f2bd4bde1f76da8a45e21ad44928d26a0fe8d7776b"
-    sha256 cellar: :any,                 arm64_linux:   "8ab8f41f7d79a1be2244c4384d5941aabd51df5f263efa2b321b3a0a53a05728"
-    sha256 cellar: :any,                 x86_64_linux:  "5d0a687402ea7a7a675e1de06e19a7a638dc6e6dc626142d1936d525540fa249"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "922d004b1f8393855b3db4a7b823ae700e22f205f4b49c1e398fb39d2fed8553"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c8204de4f23fa69f704b12c78e3df944dac15d17b8731a3d24f8a1239a1fb733"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6497d96903080c2c2064333cd97b1e5c57c86448bd01f1fd2bc2d1565927316d"
+    sha256 cellar: :any,                 arm64_linux:   "b78e3a672bd95678fe50dd924c7d280d22ec198aa392517d45c537fcc6a02beb"
+    sha256 cellar: :any,                 x86_64_linux:  "a6f923858ac76f3759dab0d3fd21b434f4f8435d03b0f947b0f147c50daf1f72"
   end
 
   depends_on "go" => :build
