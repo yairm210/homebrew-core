36d979ec1571f9a4bd4228ac45e5e25a612e8ae8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 09:05:43 +0000

    rocq-elpi 3.5.1

--- Formula/r/rocq-elpi.rb
+++ Formula/r/rocq-elpi.rb
@@ -2,10 +2,10 @@ class RocqElpi < Formula
   desc "Elpi extension language for Rocq"
   homepage "https://github.com/LPCIC/coq-elpi"
   # Update resources based on https://github.com/LPCIC/coq-elpi/blob/v#{version}/rocq-elpi.opam#L18-L26
-  url "https://github.com/LPCIC/coq-elpi/releases/download/v3.5.0/rocq-elpi-3.5.0.tar.gz"
-  sha256 "fd052f6389ba0b648b63388b7e60a99e7832564b09ce1551c82e2d69250d9068"
+  url "https://github.com/LPCIC/coq-elpi/releases/download/v3.5.1/rocq-elpi-3.5.1.tar.gz"
+  sha256 "08975c8b094c380049dfa31a1d8b32d4ea033b6d926bfa40d0164bc6d613fa46"
   license "LGPL-2.1-or-later"
-  compatibility_version 3
+  compatibility_version 4
 
   livecheck do
     url :stable
@@ -32,8 +32,8 @@ class RocqElpi < Formula
   # The result is similar to using `--deps-only` in other formulae. We can't
   # run that here as it installs a duplicate copy of `rocq`.
   resource "elpi" do
-    url "https://raw.githubusercontent.com/LPCIC/elpi/refs/tags/v3.7.2/elpi.opam"
-    sha256 "24e253b1cd5afb678f0f1e0d7f340ac3c549cf974a5c029a402c2fab5d582635"
+    url "https://raw.githubusercontent.com/LPCIC/elpi/refs/tags/v3.7.3/elpi.opam"
+    sha256 "c3ce914dde7fbfba6bd94ab872d65307a4d55a90c5b9fa1361573f07de8a2405"
   end
 
   resource "ppx_optcomp" do
@@ -56,10 +56,17 @@ class RocqElpi < Formula
     # Add symlinks to reduce subdirectories in path needed to use rocq-elpi
     libexec.install_symlink libexec.glob("ocaml-system/*")
 
+    # `ocamlfind` 1.9.9 writes relative paths, which resolve against the working
+    # directory rather than the switch.
+    inreplace libexec/"ocaml-system/lib/findlib.conf" do |s|
+      s.gsub!(/^destdir=.*/, "destdir=\"#{libexec}/lib\"")
+      s.gsub!(/^path=.*/, "path=\"#{HOMEBREW_PREFIX}/lib/ocaml:#{libexec}/lib\"")
+    end
+
     ENV["OCAMLFIND_CONF"] = libexec/"lib/findlib.conf"
 
     # dune 3.24 replaced the Coq build language with the Rocq build language.
-    dune_files = buildpath.glob("**/dune") << (buildpath/"dune-project")
+    dune_files = buildpath.glob("**/{dune,dune-project}")
     {
       "(lang dune 3.13)" => "(lang dune 3.24)",
       "(using coq 0.8)"  => "(using rocq 0.11)",
