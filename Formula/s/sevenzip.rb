441a02df20a7cb74a963e7f669743066b06f6280
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 18:25:45 +0000

    sevenzip: update 26.03 bottle.

--- Formula/s/sevenzip.rb
+++ Formula/s/sevenzip.rb
@@ -10,12 +10,11 @@ class Sevenzip < Formula
   no_autobump! because: :incompatible_version_format
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "fc1db5be9d1adf000e15fc2d3331a4e29fda08df5bd8fa76c20f8d07fefae500"
-    sha256 cellar: :any, arm64_sequoia: "6d224639f3d00aa2704cc0d5a9ce2c8859ea578ddfb9be8985120f7c61fbb2b2"
-    sha256 cellar: :any, arm64_sonoma:  "69f82a5c1c3c036a2606576db6d2fe1fcb8ff3842ea8ad98a9c79ac346e73902"
-    sha256 cellar: :any, sonoma:        "3dcc8bc8baae8201e908ccaf34a08bd406b7bcc29f6adf798f8c0c2a45c2cdb4"
-    sha256 cellar: :any, arm64_linux:   "bce2705f799b771b174bb0bac71347f4c8eb89ff46d53e16350efb92637404ee"
-    sha256 cellar: :any, x86_64_linux:  "7022469dddd4a89573800d91b6e9a6fcb2143b686271d764ad5f1e457065bdf3"
+    sha256 cellar: :any, arm64_tahoe:   "253b07aca4d56ccf849cd025beeaee2db98815571195c4027c84b1a91690d274"
+    sha256 cellar: :any, arm64_sequoia: "8e14d4dbe8fb93a9b08a0c4be9aae22d561b28d1eaf423f7a0ed0ddd94b5b1b2"
+    sha256 cellar: :any, arm64_sonoma:  "5833969c107401708c0ff136d79031831ae606e3d57962487ff85a71aa1bca50"
+    sha256 cellar: :any, arm64_linux:   "2b45a5c63c107874244eda5175db8b2f32053fed9f58f1ba71959308a431a70e"
+    sha256 cellar: :any, x86_64_linux:  "dbf1f812d853a75b2df4bdf2642094509d12dc05db52f3810687566866cef0ad"
   end
 
   def install
