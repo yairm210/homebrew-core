afb3277b9d30d3abb8b3a67e3caea9d8090e598f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 23:52:34 +0000

    rumdl 0.2.65

--- Formula/r/rumdl.rb
+++ Formula/r/rumdl.rb
@@ -1,8 +1,8 @@
 class Rumdl < Formula
   desc "Markdown Linter and Formatter written in Rust"
   homepage "https://github.com/rvben/rumdl"
-  url "https://github.com/rvben/rumdl/archive/refs/tags/v0.2.64.tar.gz"
-  sha256 "f4ef4b48fff8bdb09ba0de89f9735e2a5afc326bcacb0120592183550b733631"
+  url "https://github.com/rvben/rumdl/archive/refs/tags/v0.2.65.tar.gz"
+  sha256 "8c3fdbafbdcc6fde28a0507a0a9f29f42ebf9d26f8427729906c34d895b13036"
   license "MIT"
 
   livecheck do
