f262eccaaca195fbdd7c2a1858b9ec6bd54827ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 20:42:43 +0000

    crit: update 0.20.0 bottle.

--- Formula/c/crit.rb
+++ Formula/c/crit.rb
@@ -7,11 +7,11 @@ class Crit < Formula
   head "https://github.com/tomasz-tomczyk/crit.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4275aaf86d99957834d89d4a65ab7f5261d879706050e110c5331e308734df97"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4275aaf86d99957834d89d4a65ab7f5261d879706050e110c5331e308734df97"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4275aaf86d99957834d89d4a65ab7f5261d879706050e110c5331e308734df97"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6143071fd41f1f3463d22cf0f0b2d68dfabaf45443a395df2f5cca755692b8dc"
-    sha256 cellar: :any,                 x86_64_linux:  "71facd1f15dddf19dfa6cf96b1352aa059976ebd2f63f629a801dd4ee2b044da"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6261d662487892fcefbee89622c61bde9936219c685bd8d3605efafcb708f170"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6261d662487892fcefbee89622c61bde9936219c685bd8d3605efafcb708f170"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6261d662487892fcefbee89622c61bde9936219c685bd8d3605efafcb708f170"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d44e725bdad46afea52df2440103097bb669989bdc950b0da0fe658b9f5a53b3"
+    sha256 cellar: :any,                 x86_64_linux:  "b0091afdd89b9bb128f0ebcc7e6bf93d41afca144137fc12e6bcb21d885899f3"
   end
 
   depends_on "go" => :build
