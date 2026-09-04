94bc3f17222bf5537e103879bb3fe948fd9202d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 14:44:58 +0000

    fish: update 4.9.1 bottle.

--- Formula/f/fish.rb
+++ Formula/f/fish.rb
@@ -15,11 +15,11 @@ class Fish < Formula
   pour_bottle? only_if: :default_prefix
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "c4a4ff2040c86f669407db14e0bbe0ffc523f2b3b3e44618f8238868a8174e23"
-    sha256 cellar: :any, arm64_sequoia: "d80377e13e5d24d60fed70f4c39db27226b35b97a06ba14c7764ccaaaa3ce553"
-    sha256 cellar: :any, arm64_sonoma:  "dcfb86133198d8b857d2f9b5a581c4d2f21f437ba35f40de3b2481c09a8cef1a"
-    sha256 cellar: :any, arm64_linux:   "979ec9bc4dd832eb924c14c690fdb3ea8b2ba46631acc0dddf62de9edeb43833"
-    sha256 cellar: :any, x86_64_linux:  "45b74ac5dc439dc5af2030d28d1121eddb54c55f8f1e4d15cdaf22bad73fa211"
+    sha256 cellar: :any, arm64_tahoe:   "781ad2db83ff66b3b6e5151a86bf7bf64601879f4d949b025b2af68c0efbab17"
+    sha256 cellar: :any, arm64_sequoia: "444b3b520089122e0eef85dc39f96f390be6a8729a94fc84c6342bcf1eb5fe93"
+    sha256 cellar: :any, arm64_sonoma:  "8cb010bab387290f2e05715ce5f0664c6d5c8482e154a1664014182e76d0c0f1"
+    sha256 cellar: :any, arm64_linux:   "ca3034132d32b877874cb922bd466bab8fec2b274070e4ca19eeafea2b92d9e1"
+    sha256 cellar: :any, x86_64_linux:  "a395de95d0e41b75364d8d19d87c43ea34b6a018f951ccb7d9a9f931579cadf2"
   end
 
   depends_on "cmake" => :build
