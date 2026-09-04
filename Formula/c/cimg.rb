c7441ce75e7c09998f63d33be89634723e1c5b9a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 14:22:38 +0000

    cimg 4.0.5

--- Formula/c/cimg.rb
+++ Formula/c/cimg.rb
@@ -1,8 +1,8 @@
 class Cimg < Formula
   desc "C++ toolkit for image processing"
   homepage "https://cimg.eu/"
-  url "https://cimg.eu/files/CImg_4.0.4.zip"
-  sha256 "fcad751d6b9ba8890ef2729f7a7d744af478b6b7f058711cf2762fbcfd986200"
+  url "https://cimg.eu/files/CImg_4.0.5.zip"
+  sha256 "0051e6f42013109a3514e617264a3021582eb569352ae297a949034e9f840881"
   license "CECILL-2.0"
 
   livecheck do
