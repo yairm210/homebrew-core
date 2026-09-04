73955f6ad09cccda8a1237f59d89bfa02e7ef638
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 08:36:03 +0000

    knot 3.5.8

--- Formula/k/knot.rb
+++ Formula/k/knot.rb
@@ -1,8 +1,8 @@
 class Knot < Formula
   desc "High-performance authoritative-only DNS server"
   homepage "https://www.knot-dns.cz/"
-  url "https://knot-dns.nic.cz/release/knot-3.5.7.tar.xz"
-  sha256 "0e363e9160895e2b83d02b0a7488c3a566b605b92edac85d03be3ebce94b8214"
+  url "https://knot-dns.nic.cz/release/knot-3.5.8.tar.xz"
+  sha256 "4197c902feccf32475b254c7ddaa1ac123700e3895f50b80103ffeb8352a2807"
   license all_of: ["GPL-3.0-or-later", "0BSD", "BSD-3-Clause", "LGPL-2.0-or-later", "MIT"]
 
   livecheck do
