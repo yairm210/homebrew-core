580c88e258df579dd5f167781bbd259882d08c17
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 12:03:30 +0000

    parquet-cli 1.18.1

--- Formula/p/parquet-cli.rb
+++ Formula/p/parquet-cli.rb
@@ -1,8 +1,8 @@
 class ParquetCli < Formula
   desc "Apache Parquet command-line tools and utilities"
   homepage "https://parquet.apache.org/"
-  url "https://github.com/apache/parquet-java/archive/refs/tags/apache-parquet-1.18.0.tar.gz"
-  sha256 "2c1f8931177a01beeac7e0b87c53e693598a3157595ca6bf3fbb3bf4db9de2a8"
+  url "https://github.com/apache/parquet-java/archive/refs/tags/apache-parquet-1.18.1.tar.gz"
+  sha256 "8c93ac92bd76f2167154ededcdb79b32d4725d9d940f9fedc84bef5103912546"
   license "Apache-2.0"
   head "https://github.com/apache/parquet-java.git", branch: "master"
 
