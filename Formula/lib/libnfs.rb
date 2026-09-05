ab92bfed8c786b3e720645e453d64d4b0f595077
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-05 08:40:53 +0000

    libnfs: update 7.0.2 bottle.

--- Formula/lib/libnfs.rb
+++ Formula/lib/libnfs.rb
@@ -7,11 +7,11 @@ class Libnfs < Formula
   compatibility_version 3
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "f659050963b83166026d6543e11d45ca28926801682b037b1d8206a03c386011"
-    sha256 cellar: :any, arm64_sequoia: "0bb7c6ef70ed90dd251fd9f77f272ae96d866c2da8eca6eba9c54d5aeec01205"
-    sha256 cellar: :any, arm64_sonoma:  "3f885ad22d5f1bb1dc8f6cbdbe09d07d860af37fee65c9b6433e5d258171b8b8"
-    sha256 cellar: :any, arm64_linux:   "9a5bd3b6e56e80168975d4c82b01029e6773162650ab6b90c394bbfc88664bb2"
-    sha256 cellar: :any, x86_64_linux:  "6d786f910b87ea409598ca7666d10f41b5e2f5582f2389c2df6fe0a6a0ceab00"
+    sha256 cellar: :any, arm64_tahoe:   "0fce96881163a186518162e96e7023514a1e9000bc75e54292270296eb6126dc"
+    sha256 cellar: :any, arm64_sequoia: "fee9ca4fa5fdafaa7ffb979fa4250612449d24772cff84f6ef34b95e5cadc43a"
+    sha256 cellar: :any, arm64_sonoma:  "c42f5db155f1878ae94aaede1fa5a158a38724226b75c936e91ca73c677b0575"
+    sha256 cellar: :any, arm64_linux:   "49f0493854e6ae3edd5866ba744e8dd73f89e7447675e0038627aa7734dcc21a"
+    sha256 cellar: :any, x86_64_linux:  "357d2921e88f4db420bcf7cc1c42bcc40d054f2c14439e8aa6b61d21e4cf4ffe"
   end
 
   depends_on "cmake" => :build
