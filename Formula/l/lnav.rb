280b00919f3a4587a3e37be04e36bc9c3cfd329f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-05 05:30:19 +0000

    lnav 0.14.1

--- Formula/l/lnav.rb
+++ Formula/l/lnav.rb
@@ -1,8 +1,8 @@
 class Lnav < Formula
   desc "Curses-based tool for viewing and analyzing log files"
   homepage "https://lnav.org/"
-  url "https://github.com/tstack/lnav/releases/download/v0.14.0/lnav-0.14.0.tar.gz"
-  sha256 "0fd591a2e0488a06b3b44d7b384d3d7c6852d68607efc16ef4dec7a6ed054eea"
+  url "https://github.com/tstack/lnav/releases/download/v0.14.1/lnav-0.14.1.tar.gz"
+  sha256 "870c896a6973e1e973007be1d66bba3aef23b2aa68411fb5cc85bb7dd21a7fb0"
   license "BSD-2-Clause"
 
   livecheck do
