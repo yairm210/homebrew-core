3a7913e06a245d60ba6c25a2b7b42c3d3e94be60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-05 09:24:52 +0000

    stockfish: update 19 bottle.

--- Formula/s/stockfish.rb
+++ Formula/s/stockfish.rb
@@ -12,12 +12,11 @@ class Stockfish < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9a5b9fefb7c4839d8c27bade2699cc10a567224b1c8eed3138cec4a8baa6766d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e0378e13eeb77a7f96d73e52043cd76cd7b17f9b4a1066257165dbb3128a6be2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ed8c211d3e313b17d1fcf588c0e370b34acff7c76dcd3cb5b0ab43357088759"
-    sha256 cellar: :any_skip_relocation, sonoma:        "013a1a1baf3128725386ad3828dfd47f1d5daf24ca923ea71b863a40c367b169"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "70c600097da4cf8be54ce859cf65b060c8acb50f9b8d5e470e4fa99263352744"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9bfa9ba2618027d9e2a95148008f77151521014fcc992e28799eda4299b9f63d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cff11c467e56926956e5042db854523ea3a8dae6625db4fe58215b911bf2cb6b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8fa31b8efc0156a64877eeb79c93386bf063520c27b5e51bf97eb28ff6846e05"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9af19bcd8571d9d28b878191839a147914933f82195f27c053444645f05d5046"
+    sha256 cellar: :any,                 arm64_linux:   "ef719fa41ee73e2f5ae3f556791745218543e53be25242a7e66e7a4cfc7307c5"
+    sha256 cellar: :any,                 x86_64_linux:  "2f2fa24960bb944c41f4b09503c36dffd7495206201cf1dabe7606691a96d31d"
   end
 
   def install
