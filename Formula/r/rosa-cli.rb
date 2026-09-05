7005ba7f114d8a9811784ee5654674da45aeefdc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-05 08:44:16 +0000

    rosa-cli: update 1.2.65 bottle.

--- Formula/r/rosa-cli.rb
+++ Formula/r/rosa-cli.rb
@@ -12,12 +12,11 @@ class RosaCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a0320732b42e9fb1fa458c8eb47eac71ef6d25f337518e11f9225d67cf47997a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c8e380a7fefffd2a72b739915a2bc161ad56e6fd918730a746372fbd0459c583"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "caeca34d34a0392cd927bd3e27f9bc2a04984f08ed35f0092c21019fa0f05a69"
-    sha256 cellar: :any_skip_relocation, sonoma:        "524ad574f94ee1cbd5c2a42d2cab358fc008d8faa0343942573107ce765de7f6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0b448ea9c7c525ea97cb32d83a4918962061a6728584324a0bb5af1f4485a0eb"
-    sha256 cellar: :any,                 x86_64_linux:  "d488dbcea38c1c017c29fd7570b2e17b043bccc6063469282ced08976de3ad06"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c2f518eb0a062dfd4c7972060e466c0383a1bba97d58d0904759310e51e9963b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ef9fda09ffaa58099e389b79d29984586639dd806663d209c7d2d07c3199fbc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "751eac3a27d4e44f4fb1a2c940914df698bcdb7636225a1ca5d6113ce918eaae"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2e8c28f08327c384acb78885da265c879addd34756f9cc1ecf40c443dfaf27c7"
+    sha256 cellar: :any,                 x86_64_linux:  "c5025e84940f1331c304724eb41223063066ae327185d59d0b0f46fb5b18b3f7"
   end
 
   depends_on "go" => :build
